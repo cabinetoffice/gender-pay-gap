071b595cb262f4d23c307dcd4730910057230525
James Griffiths <james.griffiths@softwire.com>
2020-01-23 19:04:05 +0000

    #37820 Invalid link causes error (actually a bug in AnchorTagHelper)

---
+++ GenderPayGap.WebUI/Helpers/UriSanitiser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenderPayGap.WebUI.Helpers
+{
+    public static class UriSanitiser
+    {
+
+        public static bool IsValidHttpOrHttpsLink(string uri)
+        {
+            bool uriIsValid = Uri.TryCreate(uri, UriKind.Absolute, out Uri uriResult)
+                              && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            return uriIsValid;
+        }
+
+    }
+}
