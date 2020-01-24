8ff5f456adf7b8f2b61306cba4b3716d7b30d871
Raam Chauhan <raam.chauhan@softwire.com>
2020-01-24 14:49:19 +0000

    #37486 Add ReminderEmails table

---
+++ GenderPayGap.Database.Models/ReminderEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GenderPayGap.Core;
+
+namespace GenderPayGap.Database.Models
+{
+    public class ReminderEmail
+    {
+        public long ReminderEmailId { get; set; }
+        
+        public long UserId { get; set; }
+        
+        public SectorTypes SectorType { get; set; }
+        
+        public DateTime DateSent { get; set; }
+    }
+}
