6131c5f3cb23173088dc8b46adab43c0a25b79e3
Aida <aida.younesi@softwire.com>
2020-01-24 17:21:11 +0000

    #38793 Use csv feeder to make requests dynamic

--- LoadTests/src/test/scala/default/RecordingSimulation.scala
+++ LoadTests/src/test/scala/default/RecordingSimulation.scala
@@ -4,7 +4,6 @@ import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
 
 class RecordingSimulation extends Simulation {
 
@@ -24,39 +23,38 @@ class RecordingSimulation extends Simulation {
 
 	val headers_2 = Map("X-Requested-With" -> "XMLHttpRequest")
 
+	val feeder = csv("search.csv").random
+
 	object HomePage {
 		val visit = exec(http("Visit home page")
 			.get("/")
 			.headers(headers_0)
-			.resources(http("request_1")
+			.resources(http("Load logo")
 				.get("/public/govuk_template/assets/stylesheets/images/gov.uk_logotype_crown.png")
 				.headers(headers_1),
-				http("request_2")
+				http("Load search button")
 					.get("/public/images/search-button.png")
 					.headers(headers_1),
-				http("request_3")
+				http("Load licence image")
 					.get("/public/govuk_template/assets/stylesheets/images/open-government-licence_2x.png")
 					.headers(headers_1),
-				http("request_4")
+				http("Load crest")
 					.get("/public/govuk_template/assets/stylesheets/images/govuk-crest-2x.png")
 					.headers(headers_1)))
 			.pause(1)
 
-		val search = exec(http("request_5")
-			.get("/viewing/suggest-employer-name-js?search=Te")
-			.headers(headers_2)
-			.resources(http("request_6")
-				.get("/viewing/suggest-employer-name-js?search=Tes")
-				.headers(headers_2)))
+		val search = feed(feeder)
+			.exec(http("Search a word")
+			.get("/viewing/suggest-employer-name-js?search=${SearchCriteria}")
+			.headers(headers_2))
 			.pause(1)
-
-		val click = exec(http("request_7")
-			.get("/employer/d4JZhnna")
+			.exec(http("Select an organisation")
+			.get("/employer/${EmployerIdentifier}")
 			.headers(headers_0))
 			.pause(1)
 	}
 
 	val scn = scenario("Viewing").exec(HomePage.visit, HomePage.search)
 
-	setUp(scn.inject(rampUsers(5) during (30 seconds))).protocols(httpProtocol)
+	setUp(scn.inject(rampUsers(10) during (30 seconds))).protocols(httpProtocol)
 }
