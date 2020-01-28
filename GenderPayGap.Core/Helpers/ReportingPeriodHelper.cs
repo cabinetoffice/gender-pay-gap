97343d151bd052c88bdb812d7970af16b2b9d96b
James Griffiths <james.griffiths@softwire.com>
2020-01-28 11:49:46 +0000

    #39224 Pull out date formatting into new helper method

---
+++ GenderPayGap.Core/Helpers/ReportingPeriodHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace GenderPayGap.Core.Helpers
+{
+    public static class ReportingPeriodHelper
+    {
+
+        public static string FormatReportingPeriod(int reportingPeriodStartYear)
+        {
+            int fourDigitStartYear = reportingPeriodStartYear;
+
+            int fourDigitEndYear = reportingPeriodStartYear + 1;
+            int twoDigitEndYear = fourDigitEndYear % 100;
+
+            string formattedYear = $"{fourDigitStartYear}-{twoDigitEndYear}";
+            return formattedYear;
+        }
+
+    }
+}
