764dcb2cd54fc21f2a011b6564bcc118ae130511
James Griffiths <james.griffiths@softwire.com>
2020-01-28 08:46:10 +0000

    #39223 Add GovUkCheckboxItemFor to Design System library

---
+++ GovUkDesignSystem/HtmlGenerators/CheckboxItemHtmlGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using GovUkDesignSystem.GovUkDesignSystemComponents;
+using GovUkDesignSystem.Helpers;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace GovUkDesignSystem.HtmlGenerators
+{
+    public class CheckboxItemHtmlGenerator
+    {
+
+        public static IHtmlContent GenerateHtml<TModel>(
+            IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, bool>> propertyLambdaExpression,
+            LabelViewModel labelOptions = null,
+            HintViewModel hintOptions = null,
+            Conditional conditional = null,
+            bool disabled = false)
+            where TModel : GovUkViewModel
+        {
+            PropertyInfo property = ExpressionHelpers.GetPropertyFromExpression(propertyLambdaExpression);
+            string propertyName = property.Name;
+
+            TModel model = htmlHelper.ViewData.Model;
+            bool isChecked = ExpressionHelpers.GetPropertyValueFromModelAndExpression(model, propertyLambdaExpression);
+
+            if (labelOptions != null)
+            {
+                labelOptions.For = propertyName;
+            }
+
+            var checkboxItemViewModel = new CheckboxItemViewModel
+            {
+                Id = propertyName,
+                Name = propertyName,
+                Value = true.ToString(),
+                Label = labelOptions,
+                Hint = hintOptions,
+                Conditional = conditional,
+                Disabled = disabled,
+                Checked = isChecked
+            };
+
+            return htmlHelper.Partial("/GovUkDesignSystemComponents/CheckboxItem.cshtml", checkboxItemViewModel);
+        }
+
+    }
+}
