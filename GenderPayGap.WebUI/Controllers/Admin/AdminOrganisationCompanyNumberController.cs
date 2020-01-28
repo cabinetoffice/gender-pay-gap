1656aa16304b9b674583f64036c5665f9fedb2a1
James Griffiths <james.griffiths@softwire.com>
2020-01-28 11:12:31 +0000

    GPG-165 Creates Controller, ViewModel and View for changing company no.

---
+++ GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationCompanyNumberController.cs
@@ -0,0 +1,61 @@
+﻿using GenderPayGap.Core.API;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.WebUI.Classes;
+using GenderPayGap.WebUI.Models.Admin;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Admin
+{
+    [Authorize(Roles = "GPGadmin")]
+    [Route("admin")]
+    public class AdminOrganisationCompanyNumberController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+        private readonly ICompaniesHouseAPI companiesHouseApi;
+        private readonly AuditLogger auditLogger;
+
+        public AdminOrganisationCompanyNumberController(
+            IDataRepository dataRepository,
+            ICompaniesHouseAPI companiesHouseApi,
+            AuditLogger auditLogger)
+        {
+            this.dataRepository = dataRepository;
+            this.companiesHouseApi = companiesHouseApi;
+            this.auditLogger = auditLogger;
+        }
+
+        [HttpGet("organisation/{id}/change-company-number")]
+        public IActionResult ChangeCompanyNumberGet(long id)
+        {
+            Organisation organisation = dataRepository.Get<Organisation>(id);
+
+            var viewModel = new AdminOrganisationCompanyNumberViewModel
+            {
+                Organisation = organisation
+            };
+
+            if (!string.IsNullOrEmpty(organisation.CompanyNumber))
+            {
+                return View("ViewOrganisationCompanyNumber", viewModel);
+            }
+            else
+            {
+                return View("ViewOrganisationCompanyNumber", viewModel);
+            }
+        }
+
+        [HttpPost("organisation/{id}/change-company-number")]
+        public IActionResult ChangeCompanyNumberPost(long id, AdminOrganisationCompanyNumberViewModel viewModel)
+        {
+            Organisation organisation = dataRepository.Get<Organisation>(id);
+
+            viewModel.Organisation = organisation;
+
+            return View("ViewOrganisationCompanyNumber", viewModel);
+        }
+
+    }
+}
