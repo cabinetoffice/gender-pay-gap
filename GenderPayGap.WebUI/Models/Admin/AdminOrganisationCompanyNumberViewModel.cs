8b0c0e702a44dce1d868c0da66861f04826e16a1
James Griffiths <james.griffiths@softwire.com>
2020-01-28 11:12:31 +0000

    GPG-165 Creates Controller, ViewModel and View for changing company no.

---
+++ GenderPayGap.WebUI/Models/Admin/AdminOrganisationCompanyNumberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GovUkDesignSystem;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class AdminOrganisationCompanyNumberViewModel : GovUkViewModel
+    {
+
+        public Database.Organisation Organisation { get; set; }
+        
+
+
+    }
+
+    public enum AdminOrganisationCompanyNumberChangeOrRemove
+    {
+        Change,
+        Remove
+    }
+}
