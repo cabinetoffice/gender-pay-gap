9dfa09198da0afc426710f69b57a45933ec350cb
Raam <raam.chauhan@softwire.com>
2020-02-03 16:30:32 +0000

    #39114 Create controller, model and view

---
+++ GenderPayGap.WebUI/Models/Admin/DatabaseIntegrityChecksViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Database;
+using GovUkDesignSystem;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class DatabaseIntegrityChecksViewModel : GovUkViewModel
+    {
+
+        public List<Database.Organisation> OrganisationsWithMultipleActiveAddresses { get; set; } = new List<Database.Organisation>();
+
+        public List<Database.Organisation> OrganisationsWhereLatestAddressIsNotActive { get; } = new List<Database.Organisation>();
+
+        public List<Database.Organisation> OrganisationsWithMultipleActiveScopesForASingleYear { get; set; } = new List<Database.Organisation>();
+        
+        public List<Database.Organisation> OrganisationsWithNoActiveScopeForEveryYear { get; set; } = new List<Database.Organisation>();
+        
+        public List<Database.Organisation> OrganisationsWithMultipleActiveReturnsForASingleYear { get; set; } = new List<Database.Organisation>();
+        
+        public List<Database.Organisation> PublicSectorOrganisationsWithoutAPublicSectorType { get; set; } = new List<Database.Organisation>();
+    }
+}
