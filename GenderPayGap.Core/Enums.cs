5ea0a01348825c76d6dba928af795e1a2851560c
James Griffiths <james.griffiths@softwire.com>
2020-02-14 17:06:20 +0000

    #39439 Move "Late submissions" to new download page

--- GenderPayGap.Core/Enums.cs
+++ GenderPayGap.Core/Enums.cs
@@ -276,7 +276,6 @@ namespace GenderPayGap.Core
         public const string UnfinishedOrganisations = "GPG-UnfinishedOrgs.csv";
         public const string OrganisationScopes = "GPG-Scopes.csv";
         public const string OrganisationSubmissions = "GPG-Submissions.csv";
-        public const string OrganisationLateSubmissions = "GPG-LateSubmissions.csv";
         public const string ShortCodes = "GPG-ShortCodes.csv";
         public const string SicCodes = "SicCodes.csv";
         public const string SicSections = "SicSections.csv";
--- GenderPayGap.WebUI/Controllers/Admin/AdminDownloadsController.cs
+++ GenderPayGap.WebUI/Controllers/Admin/AdminDownloadsController.cs
@@ -10,6 +10,7 @@ using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GenderPayGap.WebUI.Controllers
 {
@@ -50,18 +51,53 @@ namespace GenderPayGap.WebUI.Controllers
                 .ToList();
 
             var records = orphanOrganisations.Select(
-                    org => new {
+                    org => new
+                    {
                         org.OrganisationId,
                         org.OrganisationName,
                         Address = org.GetAddressString(),
                         Sector = org.SectorType,
-                        ReportingDeadline = org.SectorType.GetAccountingStartDate().AddYears(1).AddDays(-1).ToString("d MMMM yyyy"),
+                        ReportingDeadline = org.SectorType.GetAccountingStartDate().AddYears(1).AddDays(-1),
                         ScopeStatus = org.GetCurrentScope()?.ScopeStatus,
                         HasSubmitted = org.LatestReturn?.Status == ReturnStatuses.Submitted
                     })
                 .ToList();
 
-            string fileDownloadName = $"Gpg-UserConsent-SendUpdates-{DateTime.Now:yyyy-MM-dd HH:mm}.csv";
+            string fileDownloadName = $"Gpg-OrphanOrganisations-{DateTime.Now:yyyy-MM-dd HH:mm}.csv";
+            FileContentResult fileContentResult = CreateCsvDownload(records, fileDownloadName);
+
+            return fileContentResult;
+        }
+
+        [HttpGet("downloads-new/late-submissions")]
+        public FileContentResult DownloadLateSubmissions()
+        {
+            List<Organisation> organisationsWithLateReturns = dataRepository.GetAll<Organisation>()
+                .Where(org => org.Status == OrganisationStatuses.Active)
+                .Where(
+                    org => org.LatestScope == null
+                           || org.LatestScope.ScopeStatus == ScopeStatuses.InScope
+                           || org.LatestScope.ScopeStatus == ScopeStatuses.PresumedInScope)
+                .Where(org => org.LatestReturn == null || org.LatestReturn.IsLateSubmission)
+                .Include(org => org.Returns)
+                .ToList();
+
+            var records = organisationsWithLateReturns.Select(
+                    org => new
+                    {
+                        org.OrganisationId,
+                        org.OrganisationName,
+                        org.SectorType,
+                        Submitted = org.LatestReturn != null,
+                        ReportingDeadline = org.SectorType.GetAccountingStartDate().AddYears(1).AddDays(-1).ToString("d MMMM yyyy"),
+                        SubmittedDate = org.LatestReturn?.Created,
+                        ModifiedDate = org.LatestReturn?.Modified,
+                        org.LatestReturn?.LateReason
+
+                    })
+                .ToList();
+
+            string fileDownloadName = $"Gpg-LateSubmissions-{DateTime.Now:yyyy-MM-dd HH:mm}.csv";
             FileContentResult fileContentResult = CreateCsvDownload(records, fileDownloadName);
 
             return fileContentResult;
@@ -76,7 +112,8 @@ namespace GenderPayGap.WebUI.Controllers
                 .ToList();
 
             var records = users.Select(
-                    u => new {
+                    u => new
+                    {
                         u.Firstname,
                         u.Lastname,
                         u.JobTitle,
@@ -105,7 +142,8 @@ namespace GenderPayGap.WebUI.Controllers
                 .ToList();
 
             var records = users.Select(
-                    u => new {
+                    u => new
+                    {
                         u.Firstname,
                         u.Lastname,
                         u.JobTitle,
