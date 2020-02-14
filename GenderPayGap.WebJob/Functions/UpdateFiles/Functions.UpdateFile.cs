203ebcfe1ed6f0b5eeb0fc059f7182d595b0b2b0
James Griffiths <james.griffiths@softwire.com>
2020-02-14 18:43:43 +0000

    #40577 Remove some unused code

--- GenderPayGap.WebJob/Functions/UpdateFiles/Functions.UpdateFile.cs
+++ GenderPayGap.WebJob/Functions/UpdateFiles/Functions.UpdateFile.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using GenderPayGap.Core;
 using GenderPayGap.Core.Classes;
@@ -29,65 +27,6 @@ namespace GenderPayGap.WebJob
             }
         }
 
-
-        public async Task WriteRecordsPerYearAsync<T>(string filePath, Func<int, Task<List<T>>> fillRecordsAsync)
-        {
-            string path = Path.GetDirectoryName(filePath);
-            string fileName = Path.GetFileNameWithoutExtension(filePath);
-            string extension = Path.GetExtension(filePath);
-            string prefix = fileName.BeforeFirst("_");
-            string datePart = fileName.AfterLast("_", includeWhenNoSeparator: false);
-
-            int endYear = SectorTypes.Private.GetAccountingStartDate().Year;
-            int startYear = Global.FirstReportingYear;
-            if (!string.IsNullOrWhiteSpace(datePart))
-            {
-                int start = datePart.BeforeFirst("-").ToInt32().ToFourDigitYear();
-                if (start > startYear && start <= endYear)
-                {
-                    startYear = start;
-                }
-
-                endYear = startYear;
-            }
-
-            //Make sure start and end are in correct order
-            if (startYear > endYear)
-            {
-                (startYear, endYear) = (endYear, startYear);
-            }
-
-            for (int year = endYear; year >= startYear; year--)
-            {
-                List<T> records = await fillRecordsAsync(year);
-
-                filePath = $"{prefix}_{year}-{(year + 1).ToTwoDigitYear()}{extension}";
-                if (!string.IsNullOrWhiteSpace(path))
-                {
-                    filePath = Path.Combine(path, filePath);
-                }
-
-                await Global.FileRepository.SaveCSVAsync(records, filePath);
-            }
-        }
-
-        public async Task WriteRecordsForYearAsync<T>(string filePath, int year, Func<Task<List<T>>> fillRecordsAsync)
-        {
-            string path = Path.GetDirectoryName(filePath);
-            string fileName = Path.GetFileNameWithoutExtension(filePath);
-            string extension = Path.GetExtension(filePath);
-            string prefix = fileName.BeforeFirst("_");
-
-            List<T> records = await fillRecordsAsync();
-
-            filePath = $"{prefix}_{year}-{(year + 1).ToTwoDigitYear()}{extension}";
-            if (!string.IsNullOrWhiteSpace(path))
-            {
-                filePath = Path.Combine(path, filePath);
-            }
-
-            await Global.FileRepository.SaveCSVAsync(records, filePath);
-        }
 
     }
 }
