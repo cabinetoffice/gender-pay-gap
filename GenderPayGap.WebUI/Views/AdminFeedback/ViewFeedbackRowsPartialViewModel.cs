f163e848bc100c6a55948d05eda4689562a3d716
James Griffiths <james.griffiths@softwire.com>
2020-02-17 17:02:45 +0000

    #38720 Code review changes

---
+++ GenderPayGap.WebUI/Views/AdminFeedback/ViewFeedbackRowsPartialViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Database.Models;
+
+namespace GenderPayGap.WebUI.Views.AdminFeedback
+{
+    public class ViewFeedbackRowsPartialViewModel
+    {
+        public List<Feedback> FeedbackRows { get; set; }
+        public string ButtonClasses { get; set; }
+        public string FeedbackTitle { get; set; }
+        public bool Open { get; set; }
+
+    }
+}
