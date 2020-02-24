0784de83dcf995aef017cbe50be7914c88bb3398
James Griffiths <james.griffiths@softwire.com>
2020-02-24 11:50:38 +0000

    #39721 Change ReminderEmail (DateSent -> DateChecked) and a link to User

--- GenderPayGap.Database.Models/ReminderEmail.cs
+++ GenderPayGap.Database.Models/ReminderEmail.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using GenderPayGap.Core;
 
 namespace GenderPayGap.Database.Models
@@ -6,11 +7,15 @@ namespace GenderPayGap.Database.Models
     public class ReminderEmail
     {
         public long ReminderEmailId { get; set; }
-        
+
+        public virtual User User { get; set; }
+        [ForeignKey("User")]
         public long UserId { get; set; }
-        
+
         public SectorTypes SectorType { get; set; }
-        
-        public DateTime DateSent { get; set; }
+
+        public DateTime DateChecked { get; set; }
+
+        public bool EmailSent { get; set; }
     }
 }
