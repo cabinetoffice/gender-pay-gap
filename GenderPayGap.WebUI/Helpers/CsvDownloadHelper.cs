a77001e40a016e86040f7d62023d826083776aba
James Griffiths <james.griffiths@softwire.com>
2020-02-28 17:58:20 +0000

    #38814 Move CSV file creation to helper file

---
+++ GenderPayGap.WebUI/Helpers/CsvDownloadHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.IO;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Helpers
+{
+    public static class CsvDownloadHelper
+    {
+
+        public static FileContentResult CreateCsvDownload(IEnumerable rows, string fileDownloadName)
+        {
+            var memoryStream = new MemoryStream();
+            using (var writer = new StreamWriter(memoryStream))
+            {
+                using (var csv = new CsvWriter(writer))
+                {
+                    csv.WriteRecords(rows);
+                }
+            }
+
+            var fileContentResult = new FileContentResult(memoryStream.GetBuffer(), "text/csv")
+            {
+                FileDownloadName = fileDownloadName
+            };
+            return fileContentResult;
+        }
+
+    }
+}
