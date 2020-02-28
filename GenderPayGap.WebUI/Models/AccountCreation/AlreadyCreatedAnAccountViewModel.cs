6f8b775a95f5a08a5b13b78e18ff2eafaa66cf7b
Raam <raam.chauhan@softwire.com>
2020-02-28 14:46:08 +0000

    #39722 Create new pages and viewmodels

---
+++ GenderPayGap.WebUI/Models/AccountCreation/AlreadyCreatedAnAccountViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes;
+
+namespace GenderPayGap.WebUI.Models.AccountCreation
+{
+
+    public class AlreadyCreatedAnAccountViewModel : GovUkViewModel
+    {
+
+        public HaveYouAlreadyCreatedYourUserAccount? HaveYouAlreadyCreatedYourUserAccount { get; set; }
+
+    }
+
+    public enum HaveYouAlreadyCreatedYourUserAccount
+    {
+
+        [GovUkRadioCheckboxLabelText(Text = "Yes")]
+        Yes = 0,
+
+        [GovUkRadioCheckboxLabelText(Text = "No")]
+        No = 1,
+
+        [GovUkRadioCheckboxLabelText(Text = "Not sure")]
+        NotSure = 2,
+
+        [GovUkRadioCheckboxLabelText(Text = "Unspecified")]
+        Unspecified = 3
+
+    }
+
+}
---
+++ GenderPayGap.WebUI/Models/AccountCreation/ConfirmEmailAddressViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GovUkDesignSystem;
+
+namespace GenderPayGap.WebUI.Models.AccountCreation
+{
+    public class ConfirmEmailAddressViewModel : GovUkViewModel 
+    {
+        public string EmailAddress { get; set; }
+        
+    }
+}
