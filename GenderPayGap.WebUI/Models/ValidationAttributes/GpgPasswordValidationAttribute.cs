2f5ceeaad8d92508088eea99b0b9a0b666b2234b
Raam <raam.chauhan@softwire.com>
2020-02-28 14:55:25 +0000

    #39722 Update DesignSystem to handle custom validation attributes

---
+++ GenderPayGap.WebUI/Models/ValidationAttributes/GpgPasswordValidationAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace GovUkDesignSystem.Attributes.ValidationAttributes
+{
+    public class GpgPasswordValidationAttribute : GovUkValidationAttribute
+    {
+
+        public override bool CheckForValidationErrors<TProperty>(
+            GovUkViewModel model,
+            PropertyInfo property,
+            TProperty parameterValue)
+        {
+            if (typeof(TProperty) != typeof(string))
+            {
+                throw new Exception("Paramater value has the wrong type");
+            }
+
+            var value = parameterValue as string;
+            
+            if (value.Length < 8)
+            {
+                model.AddErrorFor(property, "Password must be at least 8 characters long");
+                return false;
+            }
+
+            if (!value.Any(char.IsLower) || !value.Any(char.IsUpper) || !value.Any(char.IsNumber))
+            {
+                model.AddErrorFor(property, "Password must have at least one lower case letter, one capital letter and one number");
+                return false;
+            }
+
+            if (value.Contains("password"))
+            {
+                model.AddErrorFor(property, "Enter a password that doesn't contain 'password'");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
