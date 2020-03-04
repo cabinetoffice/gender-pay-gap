3560d130ea5c0d2577068230e6bdf96af9fc9760
Raam <raam.chauhan@softwire.com>
2020-03-04 09:53:39 +0000

    #39722 Review markups

--- GenderPayGap.WebUI/Models/AccountCreation/CreateUserAccountViewModel.cs
+++ GenderPayGap.WebUI/Models/AccountCreation/CreateUserAccountViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
-using GovUkDesignSystem;
+﻿using GovUkDesignSystem;
 using GovUkDesignSystem.Attributes;
 using GovUkDesignSystem.Attributes.ValidationAttributes;
 
@@ -8,50 +6,37 @@ namespace GenderPayGap.WebUI.Models.AccountCreation
 {
     public class CreateUserAccountViewModel : GovUkViewModel
     {
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please enter an email address."
-        )]
-        public string EmailAddress { get; set; }   
-        
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please confirm your email address."
-        )]
-        public string ConfirmEmailAddress { get; set; }   
-        
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please enter your first name."
-        )]
-        public string FirstName { get; set; }  
-        
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please enter your last name."
-        )]
-        public string LastName { get; set; }  
-        
-        [GovUkDisplayNameForErrors(
-            NameAtStartOfSentence = "Job title",
-            NameWithinSentence = "job title"
-        )]
-        [GovUkValidateCharacterCount(
-            MaxCharacters = 50
-        )]
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please enter your job title."
-        )]
-        public string JobTitle { get; set; }  
-        
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter an email address.")]
+        public string EmailAddress { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please confirm your email address.")]
+        public string ConfirmEmailAddress { get; set; }
+
+        [GovUkDisplayNameForErrors(NameAtStartOfSentence = "First name", NameWithinSentence = "first name")]
+        [GovUkValidateCharacterCount(MaxCharacters = 50)]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter your first name.")]
+        public string FirstName { get; set; }
+
+        [GovUkDisplayNameForErrors(NameAtStartOfSentence = "Last name", NameWithinSentence = "last name")]
+        [GovUkValidateCharacterCount(MaxCharacters = 50)]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter your last name.")]
+        public string LastName { get; set; }
+
+        [GovUkDisplayNameForErrors(NameAtStartOfSentence = "Job title", NameWithinSentence = "job title")]
+        [GovUkValidateCharacterCount(MaxCharacters = 50)]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter your job title.")]
+        public string JobTitle { get; set; }
+
         [GpgPasswordValidationAttribute]
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please enter a password."
-        )]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a password.")]
         public string Password { get; set; }
-        [GovUkValidateRequired(
-            ErrorMessageIfMissing = "Please confirm your password."
-        )]
-        public string ConfirmPassword { get; set; }  
-        
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please confirm your password.")]
+        public string ConfirmPassword { get; set; }
+
         public bool SendUpdates { get; set; }
-        
+
         public bool AllowContact { get; set; }
 
     }
