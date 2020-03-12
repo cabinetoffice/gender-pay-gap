17077d735be7f396ff254576fccceb7943e3811b
Raam <raam.chauhan@softwire.com>
2020-03-12 09:49:05 +0000

    #39785 Review markups

--- GenderPayGap.WebUI/wwwroot/scripts/accessible-autocomplete-gpg.js
+++ GenderPayGap.WebUI/wwwroot/scripts/accessible-autocomplete-gpg.js
@@ -12,47 +12,49 @@
             suggestion: organisationSuggestionTemplate
         }
     });
-}
 
-function selectOrganisation(suggestion) {
-    if (suggestion) {
-        var url = '/employer/' + suggestion.Id;
-        window.location.href = url;
+    function selectOrganisation(suggestion) {
+        if (suggestion) {
+            var url = '/employer/' + suggestion.Id;
+            window.location.href = url;
+        }
+    }
+
+    function organisationList (query, syncResults) {
+        var organisationListUrl = '/viewing/suggest-employer-name-js?search=';
+        return getSuggestions(organisationListUrl, query, syncResults);
+    }
+
+    function getSuggestions(url, keywords, callback) {
+        return $.get(encodeURI(url + keywords), function (response) {
+            return callback(response.Matches);
+        });
+    }
+
+    function organisationInputTemplate (suggestion) {
+        return suggestion && suggestion.Text;
+    }
+
+    function organisationSuggestionTemplate (suggestion) {
+        var suggestionElement = document.createElement("span");
+        suggestionElement.textContent = suggestion.Text;
+
+        if (suggestion.PreviousName.length > 0) {
+            var previousNameElement = document.createElement("div");
+            previousNameElement.textContent = "previously ";
+            previousNameElement.classList.add("previous-name-marker", "govuk-body-s");
+
+            var previousNameChildElement = document.createElement("span");
+            previousNameChildElement.textContent = suggestion.PreviousName;
+            previousNameChildElement.classList.add("previous-name");
+
+            previousNameElement.appendChild(previousNameChildElement);
+            suggestionElement.appendChild(previousNameElement)
+
+        }
+        return suggestionElement.innerHTML;
     }
 }
 
-function organisationList (query, syncResults) {
-    var organisationListUrl = '/viewing/suggest-employer-name-js?search=';
-    return getSuggestions(organisationListUrl, query, syncResults);
-}
 
-function getSuggestions(url, keywords, callback) {
-    return $.get(encodeURI(url + keywords), function (response) {
-        return callback(response.Matches);
-    });
-}
-
-function organisationInputTemplate (suggestion) {
-    return suggestion && suggestion.Text;
-}
-
-function organisationSuggestionTemplate (suggestion) {
-    var suggestionElement = document.createElement("span");
-    suggestionElement.textContent = suggestion.Text;
-
-    if (suggestion.PreviousName.length > 0) {
-        var previousNameElement = document.createElement("div");
-        previousNameElement.textContent = "previously ";
-        previousNameElement.classList.add("previous-name-marker", "govuk-body-s");
-
-        var previousNameChildElement = document.createElement("span");
-        previousNameChildElement.textContent = suggestion.PreviousName;
-        previousNameChildElement.classList.add("previous-name");
-
-        previousNameElement.appendChild(previousNameChildElement);
-        suggestionElement.appendChild(previousNameElement)
-
-    }
-    return suggestionElement.innerHTML;
-}
 
