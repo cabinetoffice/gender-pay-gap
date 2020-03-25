ae917005fe7335f81ae90ab81d5e394ede6105de
Aida <aida.younesi@softwire.com>
2020-03-25 02:03:49 +0000

    #40122 Add the controller and views
    to be amended with controller commit

---
+++ GenderPayGap.WebUI/Models/Admin/AdminRemoveUserViewModel.cs
@@ -0,0 +1,20 @@
+using GenderPayGap.Core;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class AdminRemoveUserViewModel : GovUkViewModel
+    {
+        public long OrganisationId { get; set; }
+
+        public string OrganisationName { get; set; }
+
+        public long UserId { get; set; }
+
+        public string UserFullName { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
+        public string Reason { get; set; }
+    }
+}
