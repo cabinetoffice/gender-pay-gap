a0ccb7078e8d28c64340b7a76e887beb8aa3b0af
Aida <aida.younesi@softwire.com>
2020-03-27 22:45:42 +0000

    #39970 Add structure for transition between three statuses

--- GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationStatusController.cs
+++ GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationStatusController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using GenderPayGap.BusinessLogic.Services;
+using GenderPayGap.Core;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.WebUI.Classes;
@@ -61,8 +63,75 @@ namespace GenderPayGap.WebUI.Controllers
                 return View("ChangeStatus", viewModel);
             }
 
+            if (viewModel.CurrentStatus == OrganisationStatuses.Active)
+            {
+                ChangeStatusFromActive(viewModel);
+            }
+            else if (viewModel.CurrentStatus == OrganisationStatuses.Retired)
+            {
+                ChangeStatusFromRetired(viewModel);
+            }
+            else if (viewModel.CurrentStatus == OrganisationStatuses.Deleted)
+            {
+                ChangeStatusFromDeleted(viewModel);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Organisation current status is not accepted");
+            }
+
             return RedirectToAction("ViewStatusHistory", "AdminOrganisationStatus", new {id});
         }
+
+        private void ChangeStatusFromActive(AdminChangeStatusViewModel viewModel)
+        {
+            if (viewModel.NewStatusFromActive == NewStatusesFromActive.Retired)
+            {
+                RetireOrganisation(viewModel);
+            }
+            else if (viewModel.NewStatusFromActive == NewStatusesFromActive.Deleted)
+            {
+                DeleteOrganisation(viewModel);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Organisation new status is not accepted");
+            }
+        }
+
+        private void ChangeStatusFromRetired(AdminChangeStatusViewModel viewModel)
+        {
+            if (viewModel.NewStatusFromRetired == NewStatusesFromRetired.Active)
+            {
+                UnRetireOrganisation(viewModel);
+            }
+            else if (viewModel.NewStatusFromRetired == NewStatusesFromRetired.Deleted) { }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Organisation new status is not accepted");
+            }
+        }
+
+        private void ChangeStatusFromDeleted(AdminChangeStatusViewModel viewModel)
+        {
+            if (viewModel.NewStatusFromDeleted == NewStatusesFromDeleted.Active)
+            {
+                UnDeleteOrganisation(viewModel);
+            }
+            else if (viewModel.NewStatusFromDeleted == NewStatusesFromDeleted.Retired) { }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Organisation new status is not accepted");
+            }
+        }
+
+        private void RetireOrganisation(AdminChangeStatusViewModel viewModel) { }
+
+        private void UnRetireOrganisation(AdminChangeStatusViewModel viewModel) { }
+
+        private void DeleteOrganisation(AdminChangeStatusViewModel viewModel) { }
+
+        private void UnDeleteOrganisation(AdminChangeStatusViewModel viwModel) { }
 
     }
 }
