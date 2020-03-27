4f7f28b8cfbda3d6361bcae54bc8a6d5b866cb9b
Raam <raam.chauhan@softwire.com>
2020-03-27 16:20:15 +0000

    #40564 Create initial files for guidance pages

---
+++ GenderPayGap.WebUI/Controllers/GuidanceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    [Route("guidance")]
+    public class GuidanceController : Controller
+    {
+
+        [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap")]
+        public IActionResult EightWaysToUnderstandYourOrganisationsGpg()
+        {
+            return View("EightWaysToUnderstandYourOrganisationsGpg");
+        }
+
+    }
+}
