85a69bb9bea51ec4ac95f87e8029b3cc0bac5a10
Aida <aida.younesi@softwire.com>
2020-03-27 11:48:11 +0000

    #39970 Add a page for organisation status history

---
+++ GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationStatusController.cs
@@ -0,0 +1,33 @@
+﻿using GenderPayGap.BusinessLogic.Services;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    [Authorize(Roles = "GPGadmin")]
+    [Route("admin")]
+    public class AdminOrganisationStatusController : Controller
+    {
+
+        private readonly AuditLogger auditLogger;
+
+        private readonly IDataRepository dataRepository;
+
+        public AdminOrganisationStatusController(IDataRepository dataRepository, AuditLogger auditLogger)
+        {
+            this.dataRepository = dataRepository;
+            this.auditLogger = auditLogger;
+        }
+
+        [HttpGet("organisation/{id}/status")]
+        public IActionResult ViewStatusHistory(long id)
+        {
+            var organisation = dataRepository.Get<Organisation>(id);
+
+            return View("ViewOrganisationStatus", organisation);
+        }
+
+    }
+}
