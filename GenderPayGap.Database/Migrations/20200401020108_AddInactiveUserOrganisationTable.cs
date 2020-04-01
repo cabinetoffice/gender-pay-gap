846bef49ee699b5757336bc9df4c99082b50b3b4
Aida <aida.younesi@softwire.com>
2020-04-01 03:10:03 +0100

    #39970 Make InactiveUserOrganisation fairly disconnected from other tables

--- GenderPayGap.Database/Migrations/20200401020108_AddInactiveUserOrganisationTable.cs
+++ GenderPayGap.Database/Migrations/20200401020108_AddInactiveUserOrganisationTable.cs
@@ -7,16 +7,6 @@ namespace GenderPayGap.Database.Core21.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<long>(
-                name: "InactiveUserOrganisationOrganisationId",
-                table: "Organisations",
-                nullable: true);
-
-            migrationBuilder.AddColumn<long>(
-                name: "InactiveUserOrganisationUserId",
-                table: "Organisations",
-                nullable: true);
-
             migrationBuilder.CreateTable(
                 name: "InactiveUserOrganisations",
                 columns: table => new
@@ -38,29 +28,24 @@ namespace GenderPayGap.Database.Core21.Migrations
                 {
                     table.PrimaryKey("PK_dbo.InactiveUserOrganisations", x => new { x.UserId, x.OrganisationId });
                     table.ForeignKey(
-                        name: "FK_dbo.InactiveUserOrganisations_dbo.OrganisationAddresses_AddressId",
+                        name: "FK_InactiveUserOrganisations_OrganisationAddresses_AddressId",
                         column: x => x.AddressId,
                         principalTable: "OrganisationAddresses",
                         principalColumn: "AddressId",
                         onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
-                        name: "FK_dbo.InactiveUserOrganisations_dbo.Organisations_OrganisationId",
+                        name: "FK_InactiveUserOrganisations_Organisations_OrganisationId",
                         column: x => x.OrganisationId,
                         principalTable: "Organisations",
                         principalColumn: "OrganisationId",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_dbo.InactiveUserOrganisations_dbo.Users_UserId",
+                        name: "FK_InactiveUserOrganisations_Users_UserId",
                         column: x => x.UserId,
                         principalTable: "Users",
                         principalColumn: "UserId",
                         onDelete: ReferentialAction.Cascade);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Organisations_InactiveUserOrganisationUserId_InactiveUserOrganisationOrganisationId",
-                table: "Organisations",
-                columns: new[] { "InactiveUserOrganisationUserId", "InactiveUserOrganisationOrganisationId" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_AddressId",
@@ -76,36 +61,12 @@ namespace GenderPayGap.Database.Core21.Migrations
                 name: "IX_UserId",
                 table: "InactiveUserOrganisations",
                 column: "UserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Organisations_InactiveUserOrganisations_InactiveUserOrganisationUserId_InactiveUserOrganisationOrganisationId",
-                table: "Organisations",
-                columns: new[] { "InactiveUserOrganisationUserId", "InactiveUserOrganisationOrganisationId" },
-                principalTable: "InactiveUserOrganisations",
-                principalColumns: new[] { "UserId", "OrganisationId" },
-                onDelete: ReferentialAction.Restrict);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Organisations_InactiveUserOrganisations_InactiveUserOrganisationUserId_InactiveUserOrganisationOrganisationId",
-                table: "Organisations");
-
             migrationBuilder.DropTable(
                 name: "InactiveUserOrganisations");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Organisations_InactiveUserOrganisationUserId_InactiveUserOrganisationOrganisationId",
-                table: "Organisations");
-
-            migrationBuilder.DropColumn(
-                name: "InactiveUserOrganisationOrganisationId",
-                table: "Organisations");
-
-            migrationBuilder.DropColumn(
-                name: "InactiveUserOrganisationUserId",
-                table: "Organisations");
         }
     }
 }
