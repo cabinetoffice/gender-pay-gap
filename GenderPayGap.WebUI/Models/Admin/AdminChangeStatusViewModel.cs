a84d1e049721fd63880062786c2573794ce273ae
Aida <aida.younesi@softwire.com>
2020-04-01 14:54:19 +0100

    #39970 Add confirmation page

--- GenderPayGap.WebUI/Models/Admin/AdminChangeStatusViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/AdminChangeStatusViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using GenderPayGap.Core;
+using GenderPayGap.Database;
 using GovUkDesignSystem;
 using GovUkDesignSystem.Attributes.ValidationAttributes;
 
@@ -7,15 +9,25 @@ namespace GenderPayGap.WebUI.Models.Admin
     public class AdminChangeStatusViewModel : GovUkViewModel
     {
 
-        public long OrganisationId { get; set; }
+        public ChangeOrganisationStatusViewModelActions Action { get; set; }
 
-        public string OrganisationName { get; set; }
-        public OrganisationStatuses CurrentStatus { get; set; }
+        public OrganisationStatuses? NewStatus { get; set; }
 
         [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
         public string Reason { get; set; }
 
-        public OrganisationStatuses? NewStatus { get; set; }
+        // Not mapped, only used for displaying information in the views
+        public Database.Organisation Organisation { get; set; }
+        public List<InactiveUserOrganisation> InactiveUserOrganisations { get; set; }
+
+    }
+
+    public enum ChangeOrganisationStatusViewModelActions
+    {
+
+        Unknown,
+        OfferNewStatusAndReason,
+        ConfirmStatusChange
 
     }
 }
