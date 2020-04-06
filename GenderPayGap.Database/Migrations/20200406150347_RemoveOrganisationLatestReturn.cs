d9fb4dd47e53e6bf315c808dec401fa0a19b4bd4
James Griffiths <james.griffiths@softwire.com>
2020-04-06 17:22:31 +0100

    #40691 Remove LatestReturn: Remove column from database (RELEASE LATER)

---
+++ GenderPayGap.Database/Migrations/20200406150347_RemoveOrganisationLatestReturn.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GenderPayGap.Database.Core21.Migrations
+{
+    public partial class RemoveOrganisationLatestReturn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_dbo.Organisations_dbo.Returns_LatestReturnId",
+                table: "Organisations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LatestReturnId",
+                table: "Organisations");
+
+            migrationBuilder.DropColumn(
+                name: "LatestReturnId",
+                table: "Organisations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "LatestReturnId",
+                table: "Organisations",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                "IX_LatestReturnId",
+                "Organisations",
+                "LatestReturnId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_dbo.Organisations_dbo.Returns_LatestReturnId",
+                table: "Organisations",
+                column: "LatestReturnId",
+                principalTable: "Returns",
+                principalColumn: "ReturnId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
