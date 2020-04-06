bbc68101504f49273a91bf8fe885cb712aa88446
James Griffiths <james.griffiths@softwire.com>
2020-04-06 19:29:38 +0100

    #40691 Remove LatestRegistration: Remove code that resets LatestRegistration when something is deleted

--- GenderPayGap.BusinessLogic/Repositories/RegistrationRepository.cs
+++ GenderPayGap.BusinessLogic/Repositories/RegistrationRepository.cs
@@ -22,32 +22,17 @@ namespace GenderPayGap.BusinessLogic.Repositories
 
         public async Task RemoveRetiredUserRegistrationsAsync(User userToRetire, User actionByUser)
         {
-            // remove all the user registrations associated with the organisation
-            userToRetire.UserOrganisations.ForEach(uo => uo.Organisation.UserOrganisations.Remove(uo));
+            foreach (UserOrganisation userOrgToUnregister in userToRetire.UserOrganisations)
+            {
+                // remove all the user registrations associated with the organisation
+                userOrgToUnregister.Organisation.UserOrganisations.Remove(userOrgToUnregister);
 
-            // get all latest registrations assigned to this user or latest registration is null
-            IEnumerable<UserOrganisation> latestRegistrationsByUser = userToRetire.UserOrganisations
-                .Where(
-                    uo => uo.Organisation.LatestRegistration == null || uo.Organisation.LatestRegistration.UserId == userToRetire.UserId);
+                // log unregistered via closed account
+                await RegistrationLog.LogUserAccountClosedAsync(userOrgToUnregister, actionByUser.EmailAddress);
 
-            // update those latest registrations
-            latestRegistrationsByUser.ForEach(
-                async userOrgToUnregister => {
-                    Organisation sourceOrg = userOrgToUnregister.Organisation;
-
-                    // update latest registration (if one exists)
-                    UserOrganisation newLatestReg = sourceOrg.GetLatestRegistration();
-                    if (newLatestReg != null)
-                    {
-                        sourceOrg.LatestRegistration = newLatestReg;
-                    }
-
-                    // log unregistered via closed account
-                    await RegistrationLog.LogUserAccountClosedAsync(userOrgToUnregister, actionByUser.EmailAddress);
-
-                    // Remove user organisation
-                    DataRepository.Delete(userOrgToUnregister);
-                });
+                // Remove user organisation
+                DataRepository.Delete(userOrgToUnregister);
+            }
 
             // save changes to database
             await DataRepository.SaveChangesAsync();
@@ -69,16 +54,6 @@ namespace GenderPayGap.BusinessLogic.Repositories
 
             // Remove the user registration from the organisation
             sourceOrg.UserOrganisations.Remove(userOrgToUnregister);
-
-            // update latest registration (if one exists)
-            if (sourceOrg.LatestRegistration == null || sourceOrg.LatestRegistration.UserId == userOrgToUnregister.UserId)
-            {
-                UserOrganisation newLatestReg = sourceOrg.GetLatestRegistration();
-                if (newLatestReg != null)
-                {
-                    sourceOrg.LatestRegistration = newLatestReg;
-                }
-            }
 
             // log record
             if (userOrgToUnregister.UserId == actionByUser.UserId)
