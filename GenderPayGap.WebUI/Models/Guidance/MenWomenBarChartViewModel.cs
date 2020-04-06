22ac75d4681c7d49e288c400dd4d28c13eaebba3
Raam <raam.chauhan@softwire.com>
2020-04-06 15:56:36 +0100

    #40564 Create bar chart component

---
+++ GenderPayGap.WebUI/Models/Guidance/MenWomenBarChartViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GenderPayGap.WebUI.Models.Guidance
+{
+    public class MenWomenBarChartViewModel
+    {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string SecondaryTitle { get; set; }
+        public decimal Male { get; set; }
+        public decimal Female { get; set; }
+        public string MaleFormatted => string.Format("{0:0.#}", Male);
+        public string FemaleFormatted => string.Format("{0:0.#}", Female);
+    }
+}
