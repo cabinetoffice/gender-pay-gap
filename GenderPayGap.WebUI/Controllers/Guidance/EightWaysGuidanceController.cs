a96439f9dd68fb152c2744c37c435942349bc499
James Griffiths <james.griffiths@softwire.com>
2020-04-07 22:52:27 +0100

    #40564 Rename Controller and move into Guidance folder

--- GenderPayGap.WebUI/Controllers/Guidance/EightWaysGuidanceController.cs
+++ GenderPayGap.WebUI/Controllers/Guidance/EightWaysGuidanceController.cs
@@ -3,64 +3,64 @@ using GenderPayGap.WebUI.Views.Components.PaginationLinks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GenderPayGap.WebUI.Controllers
+namespace GenderPayGap.WebUI.Controllers.Guidance
 {
     [Route("guidance")]
-    public class GuidanceController : Controller
+    public class EightWaysGuidanceController : Controller
     {
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/1")]
         public IActionResult EightWaysOverview()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/Overview");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/Overview");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/2")]
         public IActionResult EightWaysDoPeopleGetStuck()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/DoPeopleGetStuck");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/DoPeopleGetStuck");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/3")]
         public IActionResult EightWaysIsThereGenderImbalance()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/IsThereGenderImbalance");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/IsThereGenderImbalance");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/4")]
         public IActionResult EightWaysAreWomenMoreLikely()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/AreWomenMoreLikely");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/AreWomenMoreLikely");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/5")]
         public IActionResult EightWaysDoMenAndWomenLeave()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/DoMenAndWomenLeave");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/DoMenAndWomenLeave");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/6")]
         public IActionResult EightWaysDoParticularAspectsOfPay()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/DoParticularAspectsOfPay");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/DoParticularAspectsOfPay");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/7")]
         public IActionResult EightWaysDoMenAndWomenReceive()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/DoMenAndWomenReceive");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/DoMenAndWomenReceive");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/8")]
         public IActionResult EightWaysAreYouDoingAllThatYouCan()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/AreYouDoingAllThatYouCan");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/AreYouDoingAllThatYouCan");
         }
 
         [HttpGet("eight-ways-to-understand-your-organisations-gender-pay-gap/9")]
         public IActionResult EightWaysAreYouSupportingBoth()
         {
-            return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/AreYouSupportingBoth");
+            return View("../Guidance/EightWaysToUnderstandYourOrganisationsGpg/Pages/AreYouSupportingBoth");
         }
 
         public static PaginationPages GetPagesInThisGuidance(IUrlHelper helper, HttpRequest Request)
@@ -70,47 +70,47 @@ namespace GenderPayGap.WebUI.Controllers
                 new PaginationPage
                 {
                     Title = "Overview",
-                    Url = helper.Action("EightWaysOverview", "Guidance")
+                    Url = helper.Action("EightWaysOverview", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Do people get ‘stuck’ at certain levels within your organisation?",
-                    Url = helper.Action("EightWaysDoPeopleGetStuck", "Guidance")
+                    Url = helper.Action("EightWaysDoPeopleGetStuck", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Is there gender imbalance in your promotions?",
-                    Url = helper.Action("EightWaysIsThereGenderImbalance", "Guidance")
+                    Url = helper.Action("EightWaysIsThereGenderImbalance", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Are women more likely to be recruited into lower paid roles in your organisation?",
-                    Url = helper.Action("EightWaysAreWomenMoreLikely", "Guidance")
+                    Url = helper.Action("EightWaysAreWomenMoreLikely", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Do men and women leave your organisation at different rates?",
-                    Url = helper.Action("EightWaysDoMenAndWomenLeave", "Guidance")
+                    Url = helper.Action("EightWaysDoMenAndWomenLeave", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Do particular aspects of pay (such as starting salaries and bonuses) differ by gender?",
-                    Url = helper.Action("EightWaysDoParticularAspectsOfPay", "Guidance")
+                    Url = helper.Action("EightWaysDoParticularAspectsOfPay", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Do men and women receive different performance scores on average?",
-                    Url = helper.Action("EightWaysDoMenAndWomenReceive", "Guidance")
+                    Url = helper.Action("EightWaysDoMenAndWomenReceive", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Are you doing all that you can to support part-time employees to progress?",
-                    Url = helper.Action("EightWaysAreYouDoingAllThatYouCan", "Guidance")
+                    Url = helper.Action("EightWaysAreYouDoingAllThatYouCan", "EightWaysGuidance")
                 },
                 new PaginationPage
                 {
                     Title = "Are you supporting both men and women to take on caring responsibilities?",
-                    Url = helper.Action("EightWaysAreYouSupportingBoth", "Guidance")
+                    Url = helper.Action("EightWaysAreYouSupportingBoth", "EightWaysGuidance")
                 },
             };
 
