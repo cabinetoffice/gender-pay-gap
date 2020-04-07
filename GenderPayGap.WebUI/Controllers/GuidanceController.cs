d0897d036c4b931438ffaadf20b98117cd452b9b
James Griffiths <james.griffiths@softwire.com>
2020-04-07 21:35:33 +0100

    #40564 Auto-generate Table of Contents

--- GenderPayGap.WebUI/Controllers/GuidanceController.cs
+++ GenderPayGap.WebUI/Controllers/GuidanceController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using GenderPayGap.WebUI.Views.Components.PaginationLinks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GenderPayGap.WebUI.Controllers
@@ -58,6 +60,61 @@ namespace GenderPayGap.WebUI.Controllers
         public IActionResult EightWaysAreYouSupportingBoth()
         {
             return View("EightWaysToUnderstandYourOrganisationsGpg/Pages/AreYouSupportingBoth");
+        }
+
+        public static PaginationPages GetPagesInThisGuidance(IUrlHelper helper)
+        {
+            return new PaginationPages
+            {
+                Pages = new List<PaginationPage>
+                {
+                    new PaginationPage
+                    {
+                        Title = "Overview",
+                        Url = helper.Action("EightWaysOverview", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Do people get ‘stuck’ at certain levels within your organisation?",
+                        Url = helper.Action("EightWaysDoPeopleGetStuck", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Is there gender imbalance in your promotions?",
+                        Url = helper.Action("EightWaysIsThereGenderImbalance", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Are women more likely to be recruited into lower paid roles in your organisation?",
+                        Url = helper.Action("EightWaysAreWomenMoreLikely", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Do men and women leave your organisation at different rates?",
+                        Url = helper.Action("EightWaysDoMenAndWomenLeave", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Do particular aspects of pay (such as starting salaries and bonuses) differ by gender?",
+                        Url = helper.Action("EightWaysDoParticularAspectsOfPay", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Do men and women receive different performance scores on average?",
+                        Url = helper.Action("EightWaysDoMenAndWomenReceive", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Are you doing all that you can to support part-time employees to progress?",
+                        Url = helper.Action("EightWaysAreYouDoingAllThatYouCan", "Guidance")
+                    },
+                    new PaginationPage
+                    {
+                        Title = "Are you supporting both men and women to take on caring responsibilities?",
+                        Url = helper.Action("EightWaysAreYouSupportingBoth", "Guidance")
+                    },
+                }
+            };
         }
 
     }
