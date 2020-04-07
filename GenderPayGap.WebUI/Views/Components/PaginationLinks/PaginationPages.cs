c96670cef15ca0e6371b6cf445813ec247e03332
James Griffiths <james.griffiths@softwire.com>
2020-04-07 21:35:33 +0100

    #40564 Auto-generate Table of Contents

---
+++ GenderPayGap.WebUI/Views/Components/PaginationLinks/PaginationPages.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GenderPayGap.WebUI.Views.Components.PaginationLinks
+{
+    public class PaginationPages
+    {
+        public List<PaginationPage> Pages { get; set; }
+    }
+
+    public class PaginationPage
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
