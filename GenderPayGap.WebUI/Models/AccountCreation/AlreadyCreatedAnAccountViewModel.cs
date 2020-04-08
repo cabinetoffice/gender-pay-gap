9b16291224394357cc083c11aaf6cfd7a8696108
Raam <raam.chauhan@softwire.com>
2020-04-08 14:46:25 +0100

    #40587 Copy changes and add feature flag for step by step

--- GenderPayGap.WebUI/Models/AccountCreation/AlreadyCreatedAnAccountViewModel.cs
+++ GenderPayGap.WebUI/Models/AccountCreation/AlreadyCreatedAnAccountViewModel.cs
@@ -14,13 +14,13 @@ namespace GenderPayGap.WebUI.Models.AccountCreation
     public enum HaveYouAlreadyCreatedYourUserAccount
     {
 
-        [GovUkRadioCheckboxLabelText(Text = "Yes")]
+        [GovUkRadioCheckboxLabelText(Text = "Yes, I have a user account")]
         Yes = 0,
 
-        [GovUkRadioCheckboxLabelText(Text = "No")]
+        [GovUkRadioCheckboxLabelText(Text = "No, I don't have a user account")]
         No = 1,
 
-        [GovUkRadioCheckboxLabelText(Text = "Not sure")]
+        [GovUkRadioCheckboxLabelText(Text = "My organisation has used this service before, but I haven't")]
         NotSure = 2,
 
         [GovUkRadioCheckboxLabelText(Text = "Unspecified")]
