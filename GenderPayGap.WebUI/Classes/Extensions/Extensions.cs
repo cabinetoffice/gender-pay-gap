d4dfbabac6689c8b6a014eab98962ac1bea10b49
Raam <raam.chauhan@softwire.com>
2020-04-08 16:19:29 +0100

    #40587 Remove verify-email controller and view

--- GenderPayGap.WebUI/Classes/Extensions/Extensions.cs
+++ GenderPayGap.WebUI/Classes/Extensions/Extensions.cs
@@ -128,6 +128,16 @@ namespace GenderPayGap.WebUI.Classes
         }
 
         #endregion
+
+        #region Registration Helpers
+
+        public static async Task<bool> SendPasswordResetAsync(this Controller controller, string emailAddress, string resetCode)
+        {
+            string resetUrl = controller.Url.Action("NewPassword", "Register", new {code = resetCode}, "https");
+            return await Emails.SendResetPasswordNotificationAsync(resetUrl, emailAddress);
+        }
+
+        #endregion
         
         #region Session Handling
 
