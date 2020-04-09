a4b57799538f8139009ef4063c9c96137fd765fa
James Griffiths <james.griffiths@softwire.com>
2020-04-09 11:17:20 +0100

    #40576 More code that can be removed

--- GenderPayGap.Extensions/Reflect.cs
+++ GenderPayGap.Extensions/Reflect.cs
@@ -8,18 +8,6 @@ namespace GenderPayGap.Extensions
     public static class Reflect
     {
 
-        public static Dictionary<string, object> GetPropertiesDictionary(this object source,
-            BindingFlags filterFlags = BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance)
-        {
-            return source.GetType()
-                .GetProperties(filterFlags)
-                .Select(prop => new {Key = prop.Name, Value = prop.GetValue(source, null)})
-                // ignore null values
-                .Where(prop => prop.Value != null)
-                // convert to dictionary
-                .ToDictionary(prop => prop.Key, prop => prop.Value);
-        }
-
         public static R GetProperty<R>(this object obj, string property)
         {
             PropertyInfo value = obj.GetType().GetProperty(property);
