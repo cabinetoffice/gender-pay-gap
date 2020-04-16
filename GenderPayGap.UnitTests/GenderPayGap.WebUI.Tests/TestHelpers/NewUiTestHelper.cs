5c8f7513c029ceb09b20d4ed3aef4175efbe0f16
Raam <raam.chauhan@softwire.com>
2020-04-16 12:14:00 +0100

    #40587b Tidy up NewUiTestHelper

--- GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/TestHelpers/NewUiTestHelper.cs
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/TestHelpers/NewUiTestHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Net;
 using System.Reflection;
 using System.Security.Claims;
 using Autofac;
@@ -16,31 +14,24 @@ using GenderPayGap.BusinessLogic.Repositories;
 using GenderPayGap.BusinessLogic.Services;
 using GenderPayGap.Core;
 using GenderPayGap.Core.Classes;
-using GenderPayGap.Core.Classes.Queues;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Core.Models;
 using GenderPayGap.Core.Models.Cookies;
 using GenderPayGap.Database;
 using GenderPayGap.Extensions;
 using GenderPayGap.Extensions.AspNetCore;
-using GenderPayGap.Tests;
 using GenderPayGap.Tests.Common.Classes;
 using GenderPayGap.Tests.Common.Mocks;
 using GenderPayGap.Tests.Common.TestHelpers;
 using GenderPayGap.WebUI.Areas.Account.Abstractions;
 using GenderPayGap.WebUI.Areas.Account.ViewServices;
-using GenderPayGap.WebUI.Classes;
 using GenderPayGap.WebUI.Classes.Presentation;
 using GenderPayGap.WebUI.Classes.Services;
 using GenderPayGap.WebUI.Controllers.Account;
-using GenderPayGap.WebUI.Controllers.Admin;
-using GenderPayGap.WebUI.Models.AccountCreation;
 using GenderPayGap.WebUI.Options;
-using GenderPayGap.WebUI.Services;
 using GenderPayGap.WebUI.Tests.Mocks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -76,56 +67,13 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             //Create Inversion of Control container
             MvcApplication.ContainerIoC = DIContainer;
             Program.MvcApplication = DIContainer.Resolve<IMvcApplication>();
-            
-            //Mock UserId as claim
-            var claims = new List<Claim>();
-            if (userId < 0)
-            {
-                User user = dbObjects.OfType<User>().FirstOrDefault();
-                if (user == null)
-                {
-                    foreach (object item in dbObjects)
-                    {
-                        var enumerable = item as IEnumerable<object>;
-                        if (enumerable != null)
-                        {
-                            user = enumerable.OfType<User>().FirstOrDefault();
-                            if (user != null)
-                            {
-                                break;
-                            }
-                        }
-                    }
-                }
 
-                userId = user.UserId;
-            }
-
-
-            if (userId != 0)
-            {
-                claims.Add(new Claim("sub", userId.ToString()));
-            }
-
-            var mockPrincipal = new Mock<ClaimsPrincipal>();
-            mockPrincipal.Setup(m => m.Claims).Returns(claims);
-            mockPrincipal.Setup(m => m.Identity.IsAuthenticated).Returns(userId > 0);
-            if (userId > 0)
-            {
-                mockPrincipal.Setup(m => m.Identity.Name).Returns(userId.ToString());
-            }
-            
-            
-            
-//            //Set the Remote IP Address
-//            var features = new FeatureCollection();
-//            features.Set<IHttpConnectionFeature>(new HttpConnectionFeature {RemoteIpAddress = IPAddress.Parse("127.0.0.1")});
+            var mockPrincipal = CreateMockPrincipal(userId, dbObjects);
 
             //Mock HttpRequest
             var requestMock = new Mock<HttpRequest>();
-
+            var requestForm = new FormCollection(requestFormValues);
             var requestHeaders = new HeaderDictionary();
-            //requestMock.Setup(x => x.GetUri()).Returns(Uri);
             var requestCookies = new RequestCookieCollection(
                 new Dictionary<string, string> {
                     {
@@ -138,18 +86,8 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
                 });
             requestMock.SetupGet(x => x.Cookies).Returns(requestCookies);
             requestMock.SetupGet(x => x.Headers).Returns(requestHeaders);
+            requestMock.SetupGet(req => req.Form).Returns(requestForm);
             
-            
-            var requestForm = new FormCollection(requestFormValues);
-            requestMock.Setup(req => req.Form).Returns(requestForm);
-
-
-            //Done:Added the queryString Dictionary property for mock object
-            var queryString = new QueryString("?code=abcdefg");
-            requestMock.SetupGet(x => x.QueryString).Returns(queryString);
-
-            var query = new QueryCollection();
-
             //Mock HttpResponse
             var responseMock = new Mock<HttpResponse>();
             var responseHeaders = new HeaderDictionary();
@@ -161,26 +99,18 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             responseMock.SetupGet(x => x.Headers).Returns(responseHeaders);
             responseMock.SetupGet(x => x.Cookies).Returns(responseCookies.Object);
             
-            
             //Mock session
             var uri = new UriBuilder(Uri);
 
             //Mock HttpContext
             var httpContextMock = new Mock<HttpContext>();
-            //contextMock.Setup(m => m.Cache).Returns(HttpRuntime.Cache);
             httpContextMock.Setup(ctx => ctx.User).Returns(mockPrincipal.Object);
             httpContextMock.SetupGet(ctx => ctx.Request.Headers).Returns(requestHeaders);
             httpContextMock.SetupGet(ctx => ctx.Request).Returns(requestMock.Object);
             httpContextMock.SetupGet(ctx => ctx.Request.Cookies).Returns(requestCookies);
-            httpContextMock.SetupGet(ctx => ctx.Request.QueryString).Returns(queryString);
-            httpContextMock.SetupGet(ctx => ctx.Request.Query).Returns(query);
             httpContextMock.SetupGet(ctx => ctx.Request.HttpContext).Returns(httpContextMock.Object);
-            
             httpContextMock.SetupGet(ctx => ctx.Request.Form).Returns(requestForm);
             
-            
-            //contextMock.SetupGet(ctx => ctx.Request.GetUri()).Returns(Uri);
-            //contextMock.Setup(ctx => ctx.GetUserHostAddress()).Returns("127.0.0.1");
             httpContextMock.SetupGet(ctx => ctx.Request.Scheme).Returns(uri.Scheme);
             httpContextMock.SetupGet(ctx => ctx.Request.Host).Returns(new HostString(uri.Host, uri.Port));
             httpContextMock.SetupGet(ctx => ctx.Request.Path).Returns(uri.Path);
@@ -191,18 +121,11 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             httpContextMock.SetupGet(ctx => ctx.Response.Headers).Returns(responseHeaders);
             httpContextMock.SetupGet(ctx => ctx.Response.HttpContext).Returns(httpContextMock.Object);
             httpContextMock.SetupGet(ctx => ctx.Session).Returns(new MockHttpSession());
-//            httpContextMock.SetupGet(ctx => ctx.Features).Returns(features);
 
             //Mock the httpcontext to the controllercontext
-
             var routeData = new RouteData();
-            //var controllerContextMock = new Mock<ControllerContext>();
             var controllerContextMock = new ControllerContext {HttpContext = httpContextMock.Object, RouteData = routeData};
             
-            Global.FileRepository = DIContainer.Resolve<IFileRepository>();
-            Global.SearchRepository = DIContainer.Resolve<ISearchRepository<EmployerSearchModel>>();
-//            Global.SicCodeSearchRepository = DIContainer.Resolve<ISearchRepository<SicCodeSearchModel>>();
-
             //Mock IHttpContextAccessor
             Mock<IHttpContextAccessor> mockHttpContextAccessor = DIContainer.Resolve<IHttpContextAccessor>().GetMockFromObject();
             mockHttpContextAccessor.SetupGet(a => a.HttpContext).Returns(httpContextMock.Object);
@@ -212,13 +135,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
 
             //Create and return the controller
             var controller = DIContainer.Resolve<T>();
-            
             controller.ControllerContext = controllerContextMock;
-            
-            //Mock session
-            
-//            controller.AddMockUriHelperNew(new UriBuilder(Uri).ToString());
-
             return controller;
             
         }
@@ -247,7 +164,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             builder.RegisterType<UserRepository>().As<IUserRepository>().SingleInstance();
             builder.RegisterType<RegistrationRepository>().As<IRegistrationRepository>().SingleInstance();
 
-//             BL Services
+            // BL Services
             builder.RegisterInstance(Config.Configuration);
             builder.RegisterType<CommonBusinessLogic>().As<ICommonBusinessLogic>().InstancePerLifetimeScope();
             builder.RegisterType<SearchBusinessLogic>().As<ISearchBusinessLogic>().InstancePerLifetimeScope();
@@ -271,25 +188,11 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
                 .As<ISubmissionBusinessLogic>()
                 .InstancePerLifetimeScope();
             builder.RegisterType<OrganisationBusinessLogic>().As<IOrganisationBusinessLogic>().InstancePerLifetimeScope();
-
             
-            builder.RegisterType<MockPrivateEmployerRepository>()
-                .As<IPagedRepository<EmployerRecord>>()
-                .Keyed<IPagedRepository<EmployerRecord>>("Private")
-                .SingleInstance();
-            builder.RegisterType<MockPublicEmployerRepository>()
-                .As<IPagedRepository<EmployerRecord>>()
-                .Keyed<IPagedRepository<EmployerRecord>>("Public")
-                .SingleInstance();
             builder.Register(g => new MockGovNotify()).As<IGovNotifyAPI>().SingleInstance();
             builder.Register(g => new MockClassQueue(QueueNames.SendNotifyEmail)).Keyed<IQueue>(QueueNames.SendNotifyEmail).SingleInstance();
             builder.Register(g => new MockClassQueue(QueueNames.ExecuteWebJob)).Keyed<IQueue>(QueueNames.ExecuteWebJob).SingleInstance();
-
-            builder.Register(c => Mock.Of<LogEventQueue>()).SingleInstance();
-            builder.Register(c => Mock.Of<LogRecordQueue>()).SingleInstance();
-
-            builder.RegisterType<PinInThePostService>().As<PinInThePostService>().SingleInstance();
-
+            
             // UI Presentation
             builder.Register(
                     c =>
@@ -330,33 +233,17 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
 
             builder.RegisterInstance(new NullLoggerFactory()).As<ILoggerFactory>().SingleInstance();
             builder.RegisterGeneric(typeof(Logger<>)).As(typeof(ILogger<>)).SingleInstance();
-
             builder.RegisterType<AuditLogger>().As<AuditLogger>().SingleInstance();
 
             builder.Register(c => new Mock<IAdminService>().Object).As<IAdminService>().InstancePerLifetimeScope();
-            builder.Register(c => Mock.Of<IObfuscator>()).As<IObfuscator>().SingleInstance();
             builder.Register(c => Mock.Of<IEncryptionHandler>()).As<IEncryptionHandler>().SingleInstance();
-
-            //Register WebTracker
-            builder.Register(c => Mock.Of<IWebTracker>()).As<IWebTracker>().InstancePerLifetimeScope();
-            //Register the global instance of Program.MvcApplication (equavalent in old Global.asax.cs)
+            
+            //Register the global instance of Program.MvcApplication (equivalent in old Global.asax.cs)
             //Specify WithAttributeFiltering for the consumer - required to resolve with Keyed attributes
             builder.RegisterType<MvcApplication>().As<IMvcApplication>().SingleInstance().WithAttributeFiltering();
 
-            //Register all BaseControllers - this is required to ensure KeyFilter is resolved in constructors
-            builder.RegisterAssemblyTypes(typeof(BaseController).Assembly)
-                .Where(t => t.IsAssignableTo<BaseController>())
-                .InstancePerLifetimeScope()
-                .WithAttributeFiltering();
-            
-            //Register all Controllers - this is required to ensure KeyFilter is resolved in constructors
-            builder.RegisterAssemblyTypes(typeof(AccountCreationController).Assembly)
-                .Where(t => t.IsAssignableTo<AccountCreationController>())
-                .InstancePerLifetimeScope()
-                .WithAttributeFiltering();
-            
             //Register all controllers - this is required to ensure KeyFilter is resolved in constructors
-            builder.RegisterType<AdminUnconfirmedPinsController>().InstancePerLifetimeScope();
+            builder.RegisterType<AccountCreationController>().InstancePerLifetimeScope();
 
             builder.Register(c => new MockCache()).As<IDistributedCache>().SingleInstance();
             builder.RegisterType<HttpCache>().As<IHttpCache>().SingleInstance();
@@ -389,7 +276,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
         }
         
        
-                /// <summary>
+        /// <summary>
         ///     Registers an InMemory SQLRespository and populates with entities
         /// </summary>
         /// <param name="builder"></param>
@@ -414,7 +301,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
 
             optionsBuilder.ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning));
 
-            // show more detailed EF errors i.e. ReturnId value instead of '{ReturnId}' n the logs etc...
+            // show more detailed EF errors i.e. ReturnId value instead of '{ReturnId}' in the logs etc...
             optionsBuilder.EnableSensitiveDataLogging();
 
             var dbContext = new GpgDatabaseContext(optionsBuilder.Options);
@@ -439,7 +326,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             return dbContext;
         }
         
-                /// <summary>
+        /// <summary>
         ///     Adds a new mock url to the action method of an existing or new Mock URl helper of the controller
         /// </summary>
         /// <param name="controller">The source controller to add the mock setup</param>
@@ -474,7 +361,48 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             controller.Url = mockUrlHelper.Object;
         }
 
+
+        private static Mock<ClaimsPrincipal> CreateMockPrincipal(long userId = 0, params object[] dbObjects)
+        {
+            //Mock UserId as claim
+            var claims = new List<Claim>();
+            if (userId < 0)
+            {
+                User user = dbObjects.OfType<User>().FirstOrDefault();
+                if (user == null)
+                {
+                    foreach (object item in dbObjects)
+                    {
+                        var enumerable = item as IEnumerable<object>;
+                        if (enumerable != null)
+                        {
+                            user = enumerable.OfType<User>().FirstOrDefault();
+                            if (user != null)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                userId = user.UserId;
+            }
+
+            if (userId != 0)
+            {
+                claims.Add(new Claim("sub", userId.ToString()));
+            }
+
+            var mockPrincipal = new Mock<ClaimsPrincipal>();
+            mockPrincipal.Setup(m => m.Claims).Returns(claims);
+            mockPrincipal.Setup(m => m.Identity.IsAuthenticated).Returns(userId > 0);
+            if (userId > 0)
+            {
+                mockPrincipal.Setup(m => m.Identity.Name).Returns(userId.ToString());
+            }
+
+            return mockPrincipal;
+        }
     }
-    
     
 }
