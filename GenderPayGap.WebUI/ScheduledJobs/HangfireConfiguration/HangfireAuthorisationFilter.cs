5a620fc7ee1a8f02004d9e50e02c1ccfb34ac033
James Griffiths <james.griffiths@softwire.com>
2020-04-21 18:06:04 +0100

    #41111 Configure Hangfire

---
+++ GenderPayGap.WebUI/ScheduledJobs/HangfireConfiguration/HangfireAuthorisationFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Hangfire.Dashboard;
+
+namespace GenderPayGap.WebUI.ScheduledJobs.HangfireConfiguration
+{
+    public class HangfireAuthorisationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+
+            var claims = httpContext.User.Claims;
+            Claim roleClaim = claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+
+            bool isAdministrator = roleClaim != null && roleClaim.Value == "GPGadmin";
+
+            return isAdministrator;
+        }
+    }
+}
