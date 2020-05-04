139dbd57573bd893ad9ad4a482df11f24f3dc834
Raam <raam.chauhan@softwire.com>
2020-05-04 15:13:19 +0100

    #41078 Create empty RawSqlUpdates migration

---
+++ GenderPayGap.Database/Migrations/20200504141221_RawSqlUpdates.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GenderPayGap.Database.Migrations
+{
+    public partial class RawSqlUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
