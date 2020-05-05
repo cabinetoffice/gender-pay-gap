7dbae1a482ceafa10dac85dac48f712ea6bafd4e
Raam <raam.chauhan@softwire.com>
2020-05-05 13:57:38 +0100

    #41078 Remove SQL from GpgDatabaseContext and associated code

--- GenderPayGap.Database/GpgDatabaseContext.cs
+++ GenderPayGap.Database/GpgDatabaseContext.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace GenderPayGap.Database
 {
-    public partial class GpgDatabaseContext : DbContext, IDbContext
+    public partial class GpgDatabaseContext : IDbContext
     {
 
         public static string ConnectionString = @"Server=(localdb)\ProjectsV13;Database=GpgDatabase;Trusted_Connection=True;";
@@ -121,73 +121,7 @@ namespace GenderPayGap.Database
         {
             return base.Set<TEntity>();
         }
-
-        /// <summary>
-        ///     https://social.msdn.microsoft.com/Forums/en-US/c369c1f9-828c-480a-b1e3-14677b64a3c0/how-to-update-large-quantity-of-data-in-database-using-c
-        /// </summary>
-        public void UpdateChangesInBulk<TEntity>(IEnumerable<TEntity> listOfOrganisations) where TEntity : class
-        {
-            var dataTableOfOrganisations = new DataTable("MyDataTableOfOrganisations");
-            dataTableOfOrganisations = ConvertToDataTable(listOfOrganisations);
-            using (var conn = new SqlConnection(Global.DatabaseConnectionString))
-            {
-                using (var command = new SqlCommand(string.Empty, conn))
-                {
-                    try
-                    {
-                        conn.Open();
-
-                        var tempTableName = "#TempBulkUpdateTable";
-
-                        // Make sure the temp table doesn't exist
-                        command.CommandText = $" IF OBJECT_ID('tempdb..{tempTableName}') IS NOT NULL DROP Table {tempTableName}; ";
-                        command.ExecuteNonQuery();
-
-                        // Create the temp table on database
-                        command.CommandText = $" CREATE TABLE {tempTableName} (                     "
-                                              + "     OrganisationId bigint not null,                 "
-                                              + "     SecurityCode nvarchar(max) null,                "
-                                              + "     SecurityCodeExpiryDateTime datetime2(7) null,   "
-                                              + "     SecurityCodeCreatedDateTime datetime2(7) null   "
-                                              + " ); ";
-                        command.ExecuteNonQuery();
-
-                        // Bulk insert into temp table
-                        using (var bulkcopy = new SqlBulkCopy(conn))
-                        {
-                            bulkcopy.BulkCopyTimeout = 660;
-                            bulkcopy.DestinationTableName = tempTableName;
-                            bulkcopy.WriteToServer(dataTableOfOrganisations);
-                            bulkcopy.Close();
-                        }
-
-                        // Updating destination table
-                        command.CommandTimeout = 300;
-                        command.CommandText = " Update Organisations                                                "
-                                              + " set SecurityCode = tmp.SecurityCode,                                 "
-                                              + "     SecurityCodeExpiryDateTime = tmp.SecurityCodeExpiryDateTime,     "
-                                              + "     SecurityCodeCreatedDateTime = tmp.SecurityCodeCreatedDateTime    "
-                                              + " from Organisations orgs                                              "
-                                              + $" inner join {tempTableName} tmp on tmp.OrganisationId = orgs.OrganisationId ";
-                        command.ExecuteNonQuery();
-
-                        // Dropping the temp table
-                        command.CommandText = $" IF OBJECT_ID('tempdb..{tempTableName}') IS NOT NULL DROP Table {tempTableName}; ";
-                        command.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        string mesage = ex.Message;
-                        // Handle exception properly
-                    }
-                    finally
-                    {
-                        conn.Close();
-                    }
-                }
-            }
-        }
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -199,39 +133,7 @@ namespace GenderPayGap.Database
             //Use lazy loading for related virtual items
             optionsBuilder.UseLazyLoadingProxies();
         }
-
-        /// <summary>
-        ///     https://stackoverflow.com/questions/564366/convert-generic-list-enumerable-to-datatable
-        /// </summary>
-        /// <param name="listOfOrganisationsToConvert"></param>
-        /// <returns></returns>
-        private DataTable ConvertToDataTable<TEntity>(IEnumerable<TEntity> listOfOrganisationsToConvert) where TEntity : class
-        {
-            var table = new DataTable();
-
-            table.Columns.Add("OrganisationId", typeof(int));
-            table.Columns.Add("SecurityCode", typeof(string));
-            table.Columns.Add("SecurityCodeExpiryDateTime", typeof(DateTime));
-            table.Columns.Add("SecurityCodeCreatedDateTime", typeof(DateTime));
-
-            foreach (TEntity item in listOfOrganisationsToConvert)
-            {
-                var itemObject = (object) item;
-                var orgObject = itemObject as Organisation;
-
-                if (orgObject != null)
-                {
-                    table.Rows.Add(
-                        orgObject.OrganisationId,
-                        orgObject.SecurityCode,
-                        orgObject.SecurityCodeExpiryDateTime,
-                        orgObject.SecurityCodeCreatedDateTime);
-                }
-            }
-
-            return table;
-        }
-
+        
     }
 
 }
