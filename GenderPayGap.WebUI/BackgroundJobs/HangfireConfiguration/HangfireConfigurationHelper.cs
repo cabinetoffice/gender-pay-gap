85ad291b5c1dc180fea72b408bae7fe30c903c39
James Griffiths <james.griffiths@softwire.com>
2020-05-06 12:45:40 +0100

    #41111 Don't run Hangfire server on Azure staging slots

--- GenderPayGap.WebUI/BackgroundJobs/HangfireConfiguration/HangfireConfigurationHelper.cs
+++ GenderPayGap.WebUI/BackgroundJobs/HangfireConfiguration/HangfireConfigurationHelper.cs
@@ -1,5 +1,7 @@
 ﻿using Autofac;
 using GenderPayGap.Core;
+using GenderPayGap.Extensions;
+using GenderPayGap.Extensions.AspNetCore;
 using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,7 +14,11 @@ namespace GenderPayGap.WebUI.BackgroundJobs.HangfireConfiguration
         public static void ConfigureServices(IServiceCollection services)
         {
             services.AddHangfire(x => x.UseSqlServerStorage(Global.DatabaseConnectionString));
-            services.AddHangfireServer();
+
+            if (HangfireEnabledOnThisServer())
+            {
+                services.AddHangfireServer();
+            }
         }
 
         public static void ConfigureIOC(IContainer container)
@@ -26,7 +32,17 @@ namespace GenderPayGap.WebUI.BackgroundJobs.HangfireConfiguration
                 "/admin/hangfire",
                 new DashboardOptions {Authorization = new[] {new HangfireAuthorisationFilter()}, AppPath = "/admin"});
 
-            BackgroundJobsApi.InitialiseScheduledJobs();
+            if (HangfireEnabledOnThisServer())
+            {
+                BackgroundJobsApi.InitialiseScheduledJobs();
+            }
+        }
+
+        private static bool HangfireEnabledOnThisServer()
+        {
+            int webjobsStoppedSetting = Config.GetAppSetting("WEBJOBS_STOPPED").ToInt32(0);
+            bool webJobsAreEnabled = (webjobsStoppedSetting == 0);
+            return webJobsAreEnabled;
         }
 
     }
