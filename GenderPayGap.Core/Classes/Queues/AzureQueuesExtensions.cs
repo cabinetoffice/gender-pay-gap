6a5cb10ade5e7012d4770b613221a9461fefd751
Aida <aida.younesi@softwire.com>
2020-05-07 09:03:15 +0100

    #40582 Remove LogEventLogger

--- GenderPayGap.Core/Classes/Queues/AzureQueuesExtensions.cs
+++ GenderPayGap.Core/Classes/Queues/AzureQueuesExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using Autofac;
-using GenderPayGap.Core.Classes;
 using GenderPayGap.Core.Classes.Queues;
 using GenderPayGap.Core.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace GenderPayGap.Infrastructure.AzureQueues.Extensions
 {
@@ -34,45 +30,6 @@ namespace GenderPayGap.Infrastructure.AzureQueues.Extensions
                             : new AzureQueue(azureConnectionString, queueName))
                 .Keyed<IQueue>(queueName)
                 .SingleInstance();
-        }
-
-        /// <summary>
-        ///     Adds the LogEvent queue as logging provider to the application
-        /// </summary>
-        /// <param name="factory"></param>
-        public static ILoggerFactory UseLogEventQueueLogger(this ILoggerFactory factory, IServiceProvider serviceProvider)
-        {
-            // Resolve filter options
-            var filterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
-
-            // Resolve the keyed queue from autofac
-            var lifetimeScope = serviceProvider.GetService<ILifetimeScope>();
-            var logEventQueue = lifetimeScope.Resolve<LogEventQueue>();
-
-            // Create the logging provider
-            factory.AddProvider(new LogEventLoggerProvider(logEventQueue, AppDomain.CurrentDomain.FriendlyName, filterOptions));
-
-            return factory;
-        }
-
-        /// <summary>
-        ///     Adds an azure queue logger to a LoggingBuilder
-        /// </summary>
-        /// <param name="builder"></param>
-        public static ILoggingBuilder AddAzureQueueLogger(this ILoggingBuilder builder)
-        {
-            // Build the current service provider
-            ServiceProvider serviceProvider = builder.Services.BuildServiceProvider();
-
-            // Resolve filter options
-            var filterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
-
-            // Resolve the keyed queue from autofac
-            var lifetimeScope = serviceProvider.GetService<IContainer>();
-            var logEventQueue = lifetimeScope.Resolve<LogEventQueue>();
-
-            // Register the logging provider
-            return builder.AddProvider(new LogEventLoggerProvider(logEventQueue, AppDomain.CurrentDomain.FriendlyName, filterOptions));
         }
 
     }
