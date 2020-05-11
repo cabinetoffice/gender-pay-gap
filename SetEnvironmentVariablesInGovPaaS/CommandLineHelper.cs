283a88becc7cc97c513fbd6333090b471d32cb49
James Griffiths <james.griffiths@softwire.com>
2020-05-11 13:45:09 +0100

    #41117 Create helper app to set PaaS env-vars based on env-vars in Azure Pipelines

---
+++ SetEnvironmentVariablesInGovPaaS/CommandLineHelper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SetEnvironmentVariablesInGovPaaS
+{
+    public static class CommandLineHelper
+    {
+
+        public static void RunCommandAndPrintOutputToConsole(string command, string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+
+        public static List<string> RunCommandAndGetOutput(string command, string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            var output = new List<string>();
+
+            process.Start();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                string line = process.StandardOutput.ReadLine();
+                output.Add(line);
+            }
+
+            return output;
+        }
+
+    }
+}
