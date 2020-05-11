00253000ef879bba987b04c88a6d22d3c06f58f8
James Griffiths <james.griffiths@softwire.com>
2020-05-11 22:22:06 +0100

    #41007 Move classes for cached objects into separate files and new folder

---
+++ GenderPayGap.WebUI/Search/CachedObjects/SearchCachedOrganisation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Core;
+
+namespace GenderPayGap.WebUI.Search.CachedObjects {
+
+    internal class SearchCachedOrganisation
+    {
+        public long OrganisationId { get; set; }
+        public string OrganisationName { get; set; }
+        public List<string> OrganisationNames { get; set; } // All names (current and previous)
+        public string CompanyNumber { get; set; }
+        public string EmployerReference { get; set; }
+        public OrganisationStatuses Status { get; set; }
+    }
+}
