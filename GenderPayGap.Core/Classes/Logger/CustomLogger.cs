89149e7e49dd27d4d1aa1ac93aaf7fc1a83432e6
James Griffiths <james.griffiths@softwire.com>
2020-05-12 10:15:01 +0100

    #40582 Prevent serialisation problems when logging C# Exception objects

--- GenderPayGap.Core/Classes/Logger/CustomLogger.cs
+++ GenderPayGap.Core/Classes/Logger/CustomLogger.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Serilog;
 
 namespace GenderPayGap.Core.Classes.Logger
@@ -8,43 +9,63 @@ namespace GenderPayGap.Core.Classes.Logger
 
         public static void Debug(string message, object values = null)
         {
+            values = SanitisedValues(values);
             Log.Debug(GetLogMessage(message, values), values);
         }
 
         public static void Information(string message, object values = null)
         {
+            values = SanitisedValues(values);
             Log.Information(GetLogMessage(message, values), values);
         }
 
         public static void Warning(string message, object values = null)
         {
+            values = SanitisedValues(values);
             Log.Warning(GetLogMessage(message, values), values);
         }
 
         public static void Error(string message, object values = null)
         {
+            values = SanitisedValues(values);
             Log.Error(GetLogMessage(message, values), values);
         }
 
         public static void Fatal(string message, object values = null)
         {
+            values = SanitisedValues(values);
             Log.Fatal(GetLogMessage(message, values), values);
         }
 
-        private static string GetLogMessage(string message, object values = null)
+        private static object SanitisedValues(object values)
         {
+            if (values is Exception exception)
+            {
+                values = new
+                {
+                    exception.Message,
+                    exception.StackTrace,
+                };
+            }
+
             try
             {
                 // The logger doesn't use JsonConvert but it is doing something similar
                 // We try to convert values to JSON to see if it will throw an exception
                 JsonConvert.SerializeObject(values);
-                return values == null ? message : message + ". Log: {@Values}";
             }
             catch (JsonSerializationException ex)
             {
                 Error("SERILOG ERROR: Can't serialize values");
                 throw;
             }
+
+            return values;
+        }
+
+        private static string GetLogMessage(string message, object values = null)
+        {
+            return values == null ? message : message + ". Log: {@Values}";
         }
 
     }
