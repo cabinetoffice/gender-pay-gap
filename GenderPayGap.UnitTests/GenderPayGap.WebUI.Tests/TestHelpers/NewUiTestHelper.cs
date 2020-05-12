42a982e5141ad57891daea96892052f76e3d5957
James Griffiths <james.griffiths@softwire.com>
2020-05-12 10:26:56 +0100

    #40582 Remove another unnecessary interface

--- GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/TestHelpers/NewUiTestHelper.cs
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/TestHelpers/NewUiTestHelper.cs
@@ -163,7 +163,7 @@ namespace GenderPayGap.WebUI.Tests.TestHelpers
             
             builder.Register(c => Mock.Of<ISearchRepository<SicCodeSearchModel>>()).As<ISearchRepository<SicCodeSearchModel>>();
             builder.RegisterType<UserRepository>().As<IUserRepository>().SingleInstance();
-            builder.RegisterType<RegistrationRepository>().As<IRegistrationRepository>().SingleInstance();
+            builder.RegisterType<RegistrationRepository>().As<RegistrationRepository>().SingleInstance();
 
             // BL Services
             builder.RegisterInstance(Config.Configuration);
