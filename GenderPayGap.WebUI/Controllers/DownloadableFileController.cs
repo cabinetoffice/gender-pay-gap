a09706497f00c531a1df0903d71c5ab4ddf82318
James Griffiths <james.griffiths@softwire.com>
2020-05-15 18:28:26 +0100

    #40582 Remove unused code (admin log file downloads)

--- GenderPayGap.WebUI/Controllers/DownloadableFileController.cs
+++ GenderPayGap.WebUI/Controllers/DownloadableFileController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using GenderPayGap.BusinessLogic.Services;
-using GenderPayGap.Core;
 using GenderPayGap.Core.Classes.Logger;
 using GenderPayGap.Core.Filters;
 using GenderPayGap.Core.Interfaces;
-using GenderPayGap.Core.Interfaces.Downloadable;
 using GenderPayGap.Core.Models.Downloadable;
 using GenderPayGap.Extensions.AspNetCore;
 using GenderPayGap.WebUI.Classes;
@@ -69,45 +66,6 @@ namespace GenderPayGap.WebUI.Controllers.Administration
         }
 
         #endregion
-
-        [HttpGet("admin/WebsiteLogs")]
-        public async Task<IActionResult> WebsiteLogs(string fp)
-        {
-            IEnumerable<IDownloadableItem> downloadViewModelToReturn = await FetchDownloadablesFromSubfolderAsync(fp, "GenderPayGap.WebUI");
-            return View("WebsiteLogs", downloadViewModelToReturn);
-        }
-
-        [HttpGet("admin/WebjobLogs")]
-        public async Task<IActionResult> WebjobLogs(string fp)
-        {
-            IEnumerable<IDownloadableItem> downloadViewModelToReturn =
-                await FetchDownloadablesFromSubfolderAsync(fp, "GenderPayGap.WebJob");
-            return View("WebjobLogs", downloadViewModelToReturn);
-        }
-
-        [HttpGet("admin/IdentityLogs")]
-        public async Task<IActionResult> IdentityLogs(string fp)
-        {
-            IEnumerable<IDownloadableItem> downloadViewModelToReturn =
-                await FetchDownloadablesFromSubfolderAsync(fp, "GenderPayGap.IdentityServer4");
-            return View("IdentityLogs", downloadViewModelToReturn);
-        }
-
-        private async Task<IEnumerable<IDownloadableItem>> FetchDownloadablesFromSubfolderAsync(string fp, string subfolderName)
-        {
-            //var logsPathToProcess = string.IsNullOrEmpty(fp)
-            //    ? Path.Combine(Global.LogPath, subfolderName)
-            //    : fp;
-
-            // Storage explorer, we DO want to change
-            string logsPathToProcess = string.IsNullOrWhiteSpace(fp)
-                ? Path.Combine(Global.LogPath, subfolderName).Replace("\\", "/")
-                : fp;
-
-            IEnumerable<IDownloadableItem> listOfDownloadableItems =
-                await _downloadableFileBusinessLogic.GetListOfDownloadableItemsFromPathAsync(logsPathToProcess);
-            return listOfDownloadableItems;
-        }
 
     }
 }
