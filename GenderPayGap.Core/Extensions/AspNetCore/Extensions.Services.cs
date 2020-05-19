ea45878c2afcbe6d372a2fe0a3ea5e3639c734f7
James Griffiths <james.griffiths@softwire.com>
2020-05-19 19:16:04 +0100

    #41321 Update dependencies

--- GenderPayGap.Core/Extensions/AspNetCore/Extensions.Services.cs
+++ GenderPayGap.Core/Extensions/AspNetCore/Extensions.Services.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.WindowsAzure.Storage;
+using Microsoft.Azure.Storage;
 
 namespace GenderPayGap.Extensions.AspNetCore
 {
