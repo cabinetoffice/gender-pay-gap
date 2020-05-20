5e141ebb2f6d52cac5fb88c52f4391fbb8f1d89c
James Griffiths <james.griffiths@softwire.com>
2020-05-20 14:41:26 +0100

    #41582 Extract some common functionality from Purge Jobs and split into smaller methods

--- GenderPayGap.WebUI/BackgroundJobs/ScheduledJobs/PurgeOrganisationsJob.cs
+++ GenderPayGap.WebUI/BackgroundJobs/ScheduledJobs/PurgeOrganisationsJob.cs
@@ -10,7 +10,6 @@ using GenderPayGap.Core.Models;
 using GenderPayGap.Database;
 using GenderPayGap.Extensions;
 using GenderPayGap.Extensions.AspNetCore;
-using Microsoft.EntityFrameworkCore;
 
 namespace GenderPayGap.WebUI.BackgroundJobs.ScheduledJobs
 {
@@ -29,92 +28,90 @@ namespace GenderPayGap.WebUI.BackgroundJobs.ScheduledJobs
 
 
         //Remove any unverified users their addresses, UserOrgs, Org and addresses and archive to zip
-        public async Task PurgeOrganisations()
+        public void PurgeOrganisations()
         {
-            string runId = JobHelpers.CreateRunId();
-            DateTime startTime = DateTime.Now;
-            JobHelpers.LogFunctionStart(runId, nameof(PurgeOrganisations), startTime);
-            try
-            {
-                DateTime deadline = VirtualDateTime.Now.AddDays(0 - Global.PurgeUnusedOrganisationDays);
-                List<Organisation> orgs = await dataRepository.GetAll<Organisation>()
-                    .Where(
-                        o => o.Created < deadline
-                             && !o.Returns.Any()
-                             && !o.OrganisationScopes.Any(
-                                 sc => sc.ScopeStatus == ScopeStatuses.InScope || sc.ScopeStatus == ScopeStatuses.OutOfScope)
-                             && !o.UserOrganisations.Any(
-                                 uo => uo.Method == RegistrationMethods.Manual || uo.PINConfirmedDate != null || uo.PINSentDate > deadline)
-                             && !o.OrganisationAddresses.Any(a => a.CreatedByUserId == -1))
-                    .ToListAsync();
+            JobHelpers.RunAndLogJob(() => PurgeOrganisationsAction().Wait(), nameof(PurgeOrganisations));
+        }
 
-                if (orgs.Any())
+        private async Task PurgeOrganisationsAction()
+        {
+            DateTime deadline = VirtualDateTime.Now.AddDays(0 - Global.PurgeUnusedOrganisationDays);
+
+            List<Organisation> organisationsToPurge = GetOrganisationsToPurge(deadline);
+
+            foreach (Organisation org in organisationsToPurge)
+            {
+                await PurgeOrganisation(org);
+            }
+
+            if (organisationsToPurge.Count > 0)
+            {
+                CustomLogger.Information(
+                    $"Executed {nameof(PurgeOrganisations)} successfully: {organisationsToPurge.Count} organisations deleted",
+                    new {Environment = Config.EnvironmentName});
+            }
+        }
+
+        private List<Organisation> GetOrganisationsToPurge(DateTime deadline)
+        {
+            return dataRepository.GetAll<Organisation>()
+                .Where(
+                    o => o.Created < deadline
+                         && !o.Returns.Any()
+                         && !o.OrganisationScopes.Any(sc => sc.ScopeStatus == ScopeStatuses.InScope || sc.ScopeStatus == ScopeStatuses.OutOfScope)
+                         && !o.UserOrganisations.Any(
+                             uo => uo.Method == RegistrationMethods.Manual || uo.PINConfirmedDate != null || uo.PINSentDate > deadline)
+                         && !o.OrganisationAddresses.Any(a => a.CreatedByUserId == -1))
+                .ToList();
+        }
+
+        private async Task PurgeOrganisation(Organisation org)
+        {
+            EmployerSearchModel searchRecord = org.ToEmployerSearchResult(true);
+
+            auditLogger.AuditChangeToOrganisation(
+                AuditedAction.PurgeOrganisation,
+                org,
+                new
                 {
-                    var count = 0;
-                    foreach (Organisation org in orgs)
+                    org.OrganisationId,
+                    Address = org.GetLatestAddress()?.GetAddressString(),
+                    org.EmployerReference,
+                    org.CompanyNumber,
+                    org.OrganisationName,
+                    org.SectorType,
+                    org.Status,
+                    SicCodes = org.GetSicSectionIdsString(),
+                    SicSource = org.GetSicSource(),
+                    org.DateOfCessation
+                },
+                null);
+
+            await dataRepository.BeginTransactionAsync(
+                async () =>
+                {
+                    try
                     {
-                        EmployerSearchModel searchRecord = org.ToEmployerSearchResult(true);
+                        org.UserOrganisations.ForEach(uo => dataRepository.Delete(uo));
+                        await dataRepository.SaveChangesAsync();
 
-                        auditLogger.AuditChangeToOrganisation(
-                            AuditedAction.PurgeOrganisation,
-                            org,
-                            new
-                            {
-                                org.OrganisationId,
-                                Address = org.GetLatestAddress()?.GetAddressString(),
-                                org.EmployerReference,
-                                org.CompanyNumber,
-                                org.OrganisationName,
-                                org.SectorType,
-                                org.Status,
-                                SicCodes = org.GetSicSectionIdsString(),
-                                SicSource = org.GetSicSource(),
-                                org.DateOfCessation
-                            },
-                            null);
+                        dataRepository.Delete(org);
+                        await dataRepository.SaveChangesAsync();
 
-                        await dataRepository.BeginTransactionAsync(
-                            async () =>
-                            {
-                                try
-                                {
-                                    org.UserOrganisations.ForEach(uo => dataRepository.Delete(uo));
-                                    await dataRepository.SaveChangesAsync();
-
-                                    dataRepository.Delete(org);
-                                    await dataRepository.SaveChangesAsync();
-
-                                    dataRepository.CommitTransaction();
-                                }
-                                catch (Exception ex)
-                                {
-                                    dataRepository.RollbackTransaction();
-                                    CustomLogger.Error(
-                                        $"{nameof(PurgeOrganisations)}: Failed to purge organisation {org.OrganisationId} '{org.OrganisationName}' "
-                                        + $"ERROR: {ex.Message}:{ex.GetDetailsText()}",
-                                        new {Error = ex});
-                                }
-                            });
-                        //Remove this organisation from the search index
-                        await Global.SearchRepository.RemoveFromIndexAsync(new[] {searchRecord});
-
-                        count++;
+                        dataRepository.CommitTransaction();
                     }
+                    catch (Exception ex)
+                    {
+                        dataRepository.RollbackTransaction();
+                        CustomLogger.Error(
+                            $"{nameof(PurgeOrganisations)}: Failed to purge organisation {org.OrganisationId} '{org.OrganisationName}' "
+                            + $"ERROR: {ex.Message}:{ex.GetDetailsText()}",
+                            new {Error = ex});
+                    }
+                });
 
-                    CustomLogger.Information(
-                        $"Executed {nameof(PurgeOrganisations)} successfully: {count} organisations deleted",
-                        new {runId, Environment = Config.EnvironmentName});
-                }
-
-                JobHelpers.LogFunctionEnd(runId, nameof(PurgeOrganisations), startTime);
-            }
-            catch (Exception ex)
-            {
-                JobHelpers.LogFunctionError(runId, nameof(PurgeOrganisations), startTime, ex);
-
-                //Rethrow the error
-                throw;
-            }
+            //Remove this organisation from the search index
+            await Global.SearchRepository.RemoveFromIndexAsync(new[] {searchRecord});
         }
 
     }
