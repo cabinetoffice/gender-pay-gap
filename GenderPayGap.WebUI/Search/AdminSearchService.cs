1dd9c4d44cddd8f6b131c61fe1f45b850880c43f
Raam <raam.chauhan@softwire.com>
2020-05-21 14:26:38 +0100

    #41016 Add ranking for users

--- GenderPayGap.WebUI/Search/AdminSearchService.cs
+++ GenderPayGap.WebUI/Search/AdminSearchService.cs
@@ -16,6 +16,14 @@ namespace GenderPayGap.WebUI.Search
 
     }
     
+    internal class RankedAdminSearchUser
+    {
+        public AdminSearchResultUserViewModel AdminSearchResult { get; set; }
+        public List<RankedName> Values { get; set; }
+        public RankedName TopValue { get; set; }
+
+    }
+    
     public class AdminSearchService
     {
 
@@ -33,7 +41,7 @@ namespace GenderPayGap.WebUI.Search
             var results = new AdminSearchResultsViewModel
             {
                 OrganisationResults = SearchOrganisations(query, searchTerms, queryContainsPunctuation),
-                UserResults = SearchUsers(searchTerms),
+                UserResults = SearchUsers(query, searchTerms, queryContainsPunctuation),
                 SearchCacheUpdatedSecondsAgo = (int) VirtualDateTime.Now.Subtract(timeDetailsLoaded).TotalSeconds,
             };
             return results;
@@ -49,9 +57,9 @@ namespace GenderPayGap.WebUI.Search
 
             List<SearchCachedOrganisation> matchingOrganisations = GetMatchingOrganisations(allOrganisations, searchTerms, query, queryContainsPunctuation);
 
-            List<RankedAdminSearchOrganisation> organisationsWithRankings = CalculateRankings(matchingOrganisations, searchTerms, query, queryContainsPunctuation);
+            List<RankedAdminSearchOrganisation> organisationsWithRankings = CalculateOrganisationRankings(matchingOrganisations, searchTerms, query, queryContainsPunctuation);
 
-            List<RankedAdminSearchOrganisation> rankedOrganisations = OrderByRank(organisationsWithRankings);
+            List<RankedAdminSearchOrganisation> rankedOrganisations = OrderOrganisationsByRank(organisationsWithRankings);
 
             List<AdminSearchResultOrganisationViewModel> results = ConvertOrganisationsToSearchResults(rankedOrganisations);
 
@@ -78,7 +86,7 @@ namespace GenderPayGap.WebUI.Search
         }
 
 
-        private static List<RankedAdminSearchOrganisation> CalculateRankings(List<SearchCachedOrganisation> matchingOrganisations,
+        private static List<RankedAdminSearchOrganisation> CalculateOrganisationRankings(List<SearchCachedOrganisation> matchingOrganisations,
             List<string> searchTerms,
             string query,
             bool queryContainsPunctuation)
@@ -131,7 +139,7 @@ namespace GenderPayGap.WebUI.Search
             return rankedAdminSearchOrganisation;
         }
         
-        private List<RankedAdminSearchOrganisation> OrderByRank(List<RankedAdminSearchOrganisation> organisationsWithRankings)
+        private List<RankedAdminSearchOrganisation> OrderOrganisationsByRank(List<RankedAdminSearchOrganisation> organisationsWithRankings)
         {
             return organisationsWithRankings
                 .RankHelperOrderByListOfDoubles(org => org.TopName.Ranks)
@@ -152,41 +160,116 @@ namespace GenderPayGap.WebUI.Search
 
         #region Search Users
 
-        private List<AdminSearchResultUserViewModel> SearchUsers(List<string> searchTerms)
+        private List<AdminSearchResultUserViewModel> SearchUsers(string query, List<string> searchTerms, bool queryContainsPunctuation)
         {
             List<SearchCachedUser> allUsers = SearchRepository.CachedUsers;
 
             List<SearchCachedUser> matchingUsers = GetMatchingUsers(allUsers, searchTerms);
 
-            List<SearchCachedUser> matchingUsersOrderedByName =
-                matchingUsers.OrderBy(u => u.FullName.OriginalValue).ToList();
+            List<RankedAdminSearchUser> usersWithRankings = CalculateUserRankings(matchingUsers, searchTerms, query, queryContainsPunctuation);
 
-            List<AdminSearchResultUserViewModel> matchingUsersWithHighlightedMatches =
-                ConvertType(matchingUsersOrderedByName);
+            List<RankedAdminSearchUser> rankedUsers = OrderUsersByRank(usersWithRankings);
 
-            return matchingUsersWithHighlightedMatches;
+            List<AdminSearchResultUserViewModel> results = ConvertUsersToSearchResults(rankedUsers);
+            
+            return results;
         }
-
+        
         private List<SearchCachedUser> GetMatchingUsers(List<SearchCachedUser> allUsers, List<string> searchTerms)
         {
             return allUsers
                 .Where(user => user.FullName.Matches(searchTerms) || user.EmailAddress.Matches(searchTerms))
                 .ToList();
         }
+        
+        private static List<RankedAdminSearchUser> CalculateUserRankings(List<SearchCachedUser> matchingUsers, List<string> searchTerms, string query, bool queryContainsPunctuation)
+        {
+            return matchingUsers
+                .Select(user => CalculateRankForUser(user, searchTerms, query, queryContainsPunctuation))
+                .ToList();
+        }
 
-        private List<AdminSearchResultUserViewModel> ConvertType(
-            List<SearchCachedUser> users
+        private static RankedAdminSearchUser CalculateRankForUser(SearchCachedUser user, List<string> searchTerms, string query, bool queryContainsPunctuation)
+        {
+            var rankedAdminSearchUser = new RankedAdminSearchUser
+            {
+                Values = new List<RankedName>()
+            };
+            
+            rankedAdminSearchUser.Values.Add(CalculateRankForUserValue(user.FullName, searchTerms, query, queryContainsPunctuation));
+            rankedAdminSearchUser.Values.Add(CalculateRankForUserValue(user.EmailAddress, searchTerms, query, queryContainsPunctuation));
+
+            rankedAdminSearchUser.TopValue = rankedAdminSearchUser.Values
+                .RankHelperOrderByListOfDoubles(value => value.Ranks)
+                .First();
+            
+            rankedAdminSearchUser.AdminSearchResult = new AdminSearchResultUserViewModel
+            {
+                UserFullName = user.FullName.OriginalValue,
+                UserEmailAddress = user.EmailAddress.OriginalValue,
+                UserId = user.UserId,
+                Status = user.Status
+            };
+
+            return rankedAdminSearchUser;
+        }
+
+        private static RankedName CalculateRankForUserValue(SearchReadyValue value, List<string> searchTerms, string query, bool queryContainsPunctuation)
+        {
+            var rankValues = new List<double>();
+            
+            rankValues.Add(RankValueHelper.CalculateRankValueForPrefixMatch(value, query));
+            
+            for (int searchTermIndex = 0; searchTermIndex < searchTerms.Count; searchTermIndex++)
+            {
+                string searchTerm = searchTerms[searchTermIndex];
+
+                List<string> words = queryContainsPunctuation ? value.LowercaseWordsWithPunctuation : value.LowercaseWords;
+                for (int wordIndex = 0; wordIndex < words.Count; wordIndex++)
+                {
+                    string word = words[wordIndex];
+
+                    rankValues.Add(
+                        CalculateRankValueForUserWordMatch(word, query, searchTerm, searchTermIndex, wordIndex));
+                }
+            }
+
+            return new RankedName {Name = value.OriginalValue, Ranks = rankValues.OrderByDescending(r => r).Take(5).ToList()};
+        }
+
+        public static double CalculateRankValueForUserWordMatch(string word,
+            string query,
+            string searchTerm,
+            int searchTermIndex,
+            int wordIndex
+            )
+        {
+            if (word.StartsWith(searchTerm))
+            {
+                double prefixAmount = (double) query.Length / (double) word.Length;
+                
+                double startOfSearchness = ((double) 4) / (searchTermIndex + wordIndex  + 4);
+
+                return prefixAmount * startOfSearchness;
+            }
+
+            return 0;   
+        }
+        
+        private List<RankedAdminSearchUser> OrderUsersByRank(List<RankedAdminSearchUser> usersWithRankings)
+        {
+            return usersWithRankings
+                .RankHelperOrderByListOfDoubles(user => user.TopValue.Ranks)
+                .ThenBy(user => user.Values[0].Name)
+                .ToList();
+        }
+        
+        private List<AdminSearchResultUserViewModel> ConvertUsersToSearchResults(
+            List<RankedAdminSearchUser> users
         )
         {
             return users
-                .Select(
-                    user => new AdminSearchResultUserViewModel
-                    {
-                        UserId = user.UserId,
-                        UserFullName = user.FullName.OriginalValue,
-                        UserEmailAddress = user.EmailAddress.OriginalValue,
-                        Status = user.Status
-                    })
+                .Select(user => user.AdminSearchResult)
                 .ToList();
         }
 
