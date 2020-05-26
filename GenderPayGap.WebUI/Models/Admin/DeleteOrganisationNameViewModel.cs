32b12bc08587cfbc80afe97656841198910c9379
James Griffiths <james.griffiths@softwire.com>
2020-05-26 13:08:04 +0100

    #41624 Admin delete organisation previous name

---
+++ GenderPayGap.WebUI/Models/Admin/DeleteOrganisationNameViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GenderPayGap.Database;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class DeleteOrganisationNameViewModel : GovUkViewModel
+    {
+
+        // Not mapped, only used for displaying information in the views
+        public Database.Organisation Organisation { get; set; }
+        public OrganisationName OrganisationNameToBeDeleted { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
+        public string Reason { get; set; }
+
+    }
+}
