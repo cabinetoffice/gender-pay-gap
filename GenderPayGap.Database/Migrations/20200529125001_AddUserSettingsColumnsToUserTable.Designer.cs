20bc94c78f6de6700358b5b4293ae9f53e1e7e4a
James Griffiths <james.griffiths@softwire.com>
2020-05-29 17:02:32 +0100

    #41737 Add UserSettings columns to User table

--- GenderPayGap.Database/Migrations/20200529125001_AddUserSettingsColumnsToUserTable.Designer.cs
+++ GenderPayGap.Database/Migrations/20200529125001_AddUserSettingsColumnsToUserTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using GenderPayGap.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace GenderPayGap.Database.Migrations
 {
     [DbContext(typeof(GpgDatabaseContext))]
-    partial class GpgDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200529125001_AddUserSettingsColumnsToUserTable")]
+    partial class AddUserSettingsColumnsToUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
