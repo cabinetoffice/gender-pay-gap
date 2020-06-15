da59a2215718b8d40e8b732c5797fcf823395d9b
James Griffiths <james.griffiths@softwire.com>
2020-06-15 13:01:50 +0100

    Reduce noise in error logs by not checking if obviously-invalid postcodes are valid

--- GenderPayGap.Core/Api/PostcodesIoApi.cs
+++ GenderPayGap.Core/Api/PostcodesIoApi.cs
@@ -10,6 +10,14 @@ namespace GenderPayGap.Core.Api
 
         public static async Task<bool> IsValidPostcode(string postcode)
         {
+            if (string.IsNullOrWhiteSpace(postcode))
+            {
+                // We used to see quite a lot of error in the dependency logs saying "GET /postcodes//validate returned a 404"
+                // This isn't wrong, but it makes the error logs a bit noisy
+                // To make this a bit less noisy, we can reject any null / empty postcodes as being invalid
+                return false;
+            }
+
             try
             {
                 using (var httpClient = new HttpClient())
