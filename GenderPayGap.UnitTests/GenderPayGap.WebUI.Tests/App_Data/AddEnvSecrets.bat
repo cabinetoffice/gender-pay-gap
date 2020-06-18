b3f231c8f3940017750513d8da2a1b46580d24ed
James Griffiths <james.griffiths@softwire.com>
2020-06-18 12:16:51 +0100

    #41128 Remove the use of the Azure key vault

--- GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/App_Data/AddEnvSecrets.bat
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/App_Data/AddEnvSecrets.bat
@@ -1,11 +1,7 @@
 @echo off
 REM To see which values are going to be USED here look in Azure --> AppServices --> application settings --> AppSettingName --> Value
 REM To see where they are going to be READ FROM look in VSTS --> Pipelines --> Builds --> (select one build i.e. Auto-run selenium on PREPROD) --> Edit --> Variables --> Pipeline variables --> Name/Value 
-set ClientId=%~1
-set ClientSecret=%~2
 SET ASPNETCORE_ENVIRONMENT=%~3
 set AdminApiKey=%~5
-@echo ClientId=%ClientId%
-@echo ClientSecret=%ClientSecret%
 @echo ASPNETCORE_ENVIRONMENT=%ASPNETCORE_ENVIRONMENT%
 @echo AdminApiKey=%AdminApiKey%
