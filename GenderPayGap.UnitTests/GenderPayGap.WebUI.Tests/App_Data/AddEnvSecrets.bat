b5df266a253a123f8cb7aeed2eb5bd1265c8ff94
James Griffiths <james.griffiths@softwire.com>
2020-06-18 14:03:54 +0100

    #41128 Remove unused AddEnvSecrets.bat

--- GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/App_Data/AddEnvSecrets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM To see which values are going to be USED here look in Azure --> AppServices --> application settings --> AppSettingName --> Value
-REM To see where they are going to be READ FROM look in VSTS --> Pipelines --> Builds --> (select one build i.e. Auto-run selenium on PREPROD) --> Edit --> Variables --> Pipeline variables --> Name/Value 
-SET ASPNETCORE_ENVIRONMENT=%~3
-set AdminApiKey=%~5
-@echo ASPNETCORE_ENVIRONMENT=%ASPNETCORE_ENVIRONMENT%
-@echo AdminApiKey=%AdminApiKey%
