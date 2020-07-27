74f5b79b38866be7caf748ac3d9f9ded37e6e31d
James Griffiths <james.griffiths@softwire.com>
2020-07-27 16:37:21 +0100

    GPG-220 Create a health-check HTTP endpoint that checks a few dependencies and returns a view
    GPG-220 Health check

---
+++ GenderPayGap.WebUI/Controllers/HealthCheckController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.WebUI.ExternalServices.FileRepositories;
+using GenderPayGap.WebUI.Search;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+        private readonly IFileRepository fileRepository;
+        private const string HealthCheckFileName = "HealthCheckFile.txt";
+
+        public HealthCheckController(
+            IDataRepository dataRepository,
+            IFileRepository fileRepository)
+        {
+            this.dataRepository = dataRepository;
+            this.fileRepository = fileRepository;
+        }
+
+
+        [HttpGet("health-check")]
+        public IActionResult HealthCheck()
+        {
+            CheckDatabaseConnection();
+            CheckFileConnection();
+            CheckSearchRepositoryIsLoaded();
+
+            return View("HealthCheckSuccessful");
+        }
+
+        private void CheckDatabaseConnection()
+        {
+            Organisation anOrganisation = dataRepository.GetAll<Organisation>().FirstOrDefault();
+
+            if (anOrganisation == null)
+            {
+                throw new Exception("Could not load an organisation from the database");
+            }
+        }
+
+        private void CheckFileConnection()
+        {
+            string guid = Guid.NewGuid().ToString("N");
+
+            fileRepository.Write(HealthCheckFileName, guid);
+
+            string fileContents = fileRepository.Read(HealthCheckFileName);
+
+            if (fileContents != guid)
+            {
+                throw new Exception("Could not read or write a file");
+            }
+        }
+
+        private void CheckSearchRepositoryIsLoaded()
+        {
+            if (SearchRepository.CachedOrganisations == null ||
+                SearchRepository.CachedUsers == null)
+            {
+                throw new Exception("SearchRepository was not loaded");
+            }
+        }
+
+    }
+}
