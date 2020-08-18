4338608d71bfb65f56b53de2a4ec0e0aa489d7f9
James Griffiths <james.griffiths@softwire.com>
2020-08-18 18:36:16 +0100

    GPG-349 Add new-style custom error pages for User Retired and Email Not Verified

--- GenderPayGap.WebUI/ErrorHandling/CustomErrorPageExceptions.cs
+++ GenderPayGap.WebUI/ErrorHandling/CustomErrorPageExceptions.cs
@@ -9,4 +9,19 @@ namespace GenderPayGap.WebUI.ErrorHandling
         public abstract int StatusCode { get; }
     }
 
+
+    public class UserAccountRetiredException : CustomErrorPageException
+    {
+        public override string ViewName => "../Errors/UserAccountRetired";
+        public override int StatusCode => 403;
+    }
+
+    public class EmailNotVerifiedException : CustomErrorPageException
+    {
+        public override string ViewName => "../Errors/EmailNotVerified";
+        public override int StatusCode => 403;
+        public string EmailAddress { get; set; }
+        public DateTime? EmailVerifySendDate { get; set; }
+    }
+
 }
