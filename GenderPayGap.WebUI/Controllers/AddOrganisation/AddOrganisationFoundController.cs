f55385a04e8f9c2714423439179621ce70f06b14
James Griffiths <james.griffiths@softwire.com>
2020-08-20 16:14:49 +0100

    GPG-347 Add stub pages for "Found" and "Manually add name"

---
+++ GenderPayGap.WebUI/Controllers/AddOrganisation/AddOrganisationFoundController.cs
@@ -0,0 +1,33 @@
+﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.WebUI.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.AddOrganisation
+{
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Route("add-organisation")]
+    public class AddOrganisationFoundController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+
+        public AddOrganisationFoundController(IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
+
+
+        [HttpGet("found")]
+        public IActionResult Found()
+        {
+            ControllerHelper.Throw404IfFeatureDisabled(FeatureFlag.NewAddOrganisationJourney);
+
+            ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+
+            return View("Found");
+        }
+
+    }
+}
---
+++ GenderPayGap.WebUI/Controllers/AddOrganisation/AddOrganisationManualController.cs
@@ -0,0 +1,33 @@
+﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.WebUI.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.AddOrganisation
+{
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Route("add-organisation")]
+    public class AddOrganisationManualController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+
+        public AddOrganisationManualController(IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
+
+
+        [HttpGet("manual")]
+        public IActionResult Manual()
+        {
+            ControllerHelper.Throw404IfFeatureDisabled(FeatureFlag.NewAddOrganisationJourney);
+
+            ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+
+            return View("Manual");
+        }
+
+    }
+}
