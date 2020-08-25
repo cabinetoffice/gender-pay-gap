ba6cf821b517f3a9bdcee8e8ac18b52475cc1329
James Griffiths <james.griffiths@softwire.com>
2020-08-26 00:00:17 +0100

    GPG-429 Move PrivacyPolicy into separate controller

--- GenderPayGap.WebUI/Controllers/HomeController.cs
+++ GenderPayGap.WebUI/Controllers/HomeController.cs
@@ -43,49 +43,6 @@ namespace GenderPayGap.WebUI.Controllers
         {
             return View(nameof(ReportConcerns));
         }
-
-        #region PrivacyPolicy
-
-        [HttpGet("~/privacy-policy")]
-        public IActionResult PrivacyPolicy()
-        {
-            return View("PrivacyPolicy", null);
-        }
-
-        [PreventDuplicatePost]
-        [ValidateAntiForgeryToken]
-        [HttpPost("~/privacy-policy")]
-        public async Task<IActionResult> PrivacyPolicyPost()
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("PrivacyPolicy");
-            }
-
-            //Ensure user has completed the registration process
-            User currentUser;
-            IActionResult checkResult = CheckUserRegisteredOk(out currentUser);
-            if (checkResult == null)
-            {
-                return checkResult;
-            }
-
-            if (!IsImpersonatingUser && !CurrentUser.IsAdministrator())
-            {
-                // check if the user has accepted the privacy statement
-                DateTime? hasReadPrivacy = currentUser.AcceptedPrivacyStatement;
-                if (hasReadPrivacy == null || hasReadPrivacy.ToDateTime() < Global.PrivacyChangedDate)
-                {
-                    currentUser.AcceptedPrivacyStatement = VirtualDateTime.Now;
-                    await DataRepository.SaveChangesAsync();
-                }
-            }
-
-            return RedirectToAction(nameof(OrganisationController.ManageOrganisations), "Organisation");
-        }
-
-        #endregion
-        
         #region Accessibility Statement
         
         [HttpGet("~/accessibility-statement")]
