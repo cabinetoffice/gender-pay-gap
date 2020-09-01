e957c229252590c6806e4f5a008369a333c40d24
Dulcie <dulcie.jackson@softwire.com>
2020-09-01 11:33:38 +0100

    GPG-360 Adds methods to get users by last login date

--- GenderPayGap.WebUI/BackgroundJobs/ScheduledJobs/SendIntentionToRetireAccountEmailsJob.cs
+++ GenderPayGap.WebUI/BackgroundJobs/ScheduledJobs/SendIntentionToRetireAccountEmailsJob.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.Database.Models;
 using GenderPayGap.Extensions;
 
 namespace GenderPayGap.WebUI.BackgroundJobs.ScheduledJobs
@@ -24,7 +26,7 @@ namespace GenderPayGap.WebUI.BackgroundJobs.ScheduledJobs
 
             try
             {
-                // TODO Add clever stuff here
+                SendReminderEmails();
 
                 JobHelpers.LogFunctionEnd(runId, nameof(SendIntentionToRetireAccountEmails), startTime);
             }
@@ -35,6 +37,48 @@ namespace GenderPayGap.WebUI.BackgroundJobs.ScheduledJobs
                 //Rethrow the error
                 throw;
             }
+        }
+
+        public void SendReminderEmails()
+        {
+            DateTime threeYearsAgo = VirtualDateTime.Now.AddYears(-3);
+            DateTime threeYearsAgoMinusThirtyDays = threeYearsAgo.AddDays(30);
+            DateTime threeYearsAgoMinusSevenDays = threeYearsAgo.AddDays(7);
+
+            List<User> usersToSendThirtyDayReminders = dataRepository.GetAll<User>()
+                .Where(u => u.LoginDate == threeYearsAgoMinusThirtyDays)
+                .ToList();
+
+            List<User> usersToSendSevenDayReminders = dataRepository.GetAll<User>()
+                .Where(u => u.LoginDate == threeYearsAgoMinusSevenDays)
+                .ToList();
+
+            foreach (User user in usersToSendThirtyDayReminders)
+            {
+                // TODO: Send 30 day reminder email
+            }
+
+            foreach (User user in usersToSendSevenDayReminders)
+            {
+                // TODO: Send 7 day reminder email
+            }
+
+            dataRepository.SaveChangesAsync().Wait();
+        }
+
+        public void RetireUsers()
+        {
+            DateTime threeYearsAgo = VirtualDateTime.Now.AddYears(-3);
+
+            List<User> usersToRetire = dataRepository.GetAll<User>()
+                .Where(u => u.LoginDate < threeYearsAgo)
+                .ToList();
+
+            foreach (User user in usersToRetire)
+            {
+                // TODO: Retire user
+            }
+
         }
     }
 }
