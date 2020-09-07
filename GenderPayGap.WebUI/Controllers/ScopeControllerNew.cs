fc27b56c2a7caa7306aa519c5ff25b8bd8b04eee
Dulcie <dulcie.jackson@softwire.com>
2020-09-07 15:12:02 +0100

    GPG-388 Creates new scope controller with minimal dependencies

---
+++ GenderPayGap.WebUI/Controllers/ScopeControllerNew.cs
@@ -0,0 +1,25 @@
+﻿using GenderPayGap.Core.Interfaces;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Microsoft.AspNetCore.Components.Route("scope")]
+    public class ScopeControllerNew : Controller
+    {
+
+        private readonly EmailSendingService emailSendingService;
+        private readonly IDataRepository dataRepository;
+        
+        public ScopeControllerNew(EmailSendingService emailSendingService, IDataRepository dataRepository)
+        {
+            this.emailSendingService = emailSendingService;
+            this.dataRepository = dataRepository;
+        }
+
+    }
+}
