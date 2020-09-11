3a4636f0f285977960e7afd6b569e8922837c9cf
James Griffiths <james.griffiths@softwire.com>
2020-09-11 10:51:18 +0100

    GPG-368 Review mark-ups (a couple of typos)

--- GenderPayGap.WebUI/Models/AddOrganisation/AddOrganisationChooseSectorViewModel.cs
+++ GenderPayGap.WebUI/Models/AddOrganisation/AddOrganisationChooseSectorViewModel.cs
@@ -7,7 +7,7 @@ namespace GenderPayGap.WebUI.Models.AddOrganisation
     {
 
         // This is a bool? (nullable) because it makes for better "Back" links
-        //   e.g. in Views/AddOrganisation/Search.cshtml, we crate a new AddOrganisationChooseSectorViewModel,
+        //   e.g. in Views/AddOrganisation/Search.cshtml, we create a new AddOrganisationChooseSectorViewModel,
         //   but we only want to pass the Sector back to the previous page (we haven't specified a value for Validate)
         public bool? Validate { get; set; }
 
