7fd6a89a593745d3387ccd7e04491ca487a16893
James Griffiths <james.griffiths@softwire.com>
2020-09-15 17:48:09 +0100

    GPG-372 Add Organisation - Add validation messages to Manual Confirm page

--- GenderPayGap.WebUI/Controllers/AddOrganisation/AddOrganisationManualConfirmController.cs
+++ GenderPayGap.WebUI/Controllers/AddOrganisation/AddOrganisationManualConfirmController.cs
@@ -5,6 +5,7 @@ using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Models.AddOrganisation;
+using GovUkDesignSystem;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,22 +31,46 @@ namespace GenderPayGap.WebUI.Controllers.AddOrganisation
 
             ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
 
-            if (viewModel.Validate == true)
+            ValidateManuallyEnteredOrganisationDetails(viewModel);
+
+            PopulateViewModel(viewModel);
+
+            return View("ManualConfirm", viewModel);
+        }
+
+        private static void ValidateManuallyEnteredOrganisationDetails(AddOrganisationManualViewModel viewModel)
+        {
+            if (string.IsNullOrWhiteSpace(viewModel.OrganisationName))
             {
-                viewModel.Validate = null; // Required to prevent the next page immediately trying to validate the form
-                return RedirectToAction("ManualSicCodes", "AddOrganisationManualSicCodes", viewModel);
+                viewModel.AddErrorFor(m => m.OrganisationName, "Enter the name of the organisation");
             }
 
+            if (string.IsNullOrWhiteSpace(viewModel.Address1))
+            {
+                viewModel.AddErrorFor(m => m.Address1, "Enter the registered address of the organisation");
+            }
+
+            if (!viewModel.IsUkAddress.HasValue)
+            {
+                viewModel.AddErrorFor(m => m.IsUkAddress, "Select if this organisation's address is a UK address");
+            }
+
+            if (!viewModel.Sector.HasValue)
+            {
+                viewModel.AddErrorFor(m => m.Sector, "Choose which type of organisation you would like to add");
+            }
+        }
+
+        private void PopulateViewModel(AddOrganisationManualViewModel viewModel)
+        {
             if (viewModel.SicCodes == null)
             {
                 viewModel.SicCodes = new List<int>();
             }
+
             viewModel.SelectedSicCodes = dataRepository.GetAll<SicCode>()
                 .Where(sc => viewModel.SicCodes.Contains(sc.SicCodeId))
                 .ToList();
-
-            viewModel.Editing = true;
-            return View("ManualConfirm", viewModel);
         }
 
     }
