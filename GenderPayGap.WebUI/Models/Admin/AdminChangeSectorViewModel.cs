30c9ca31d3445ae4c92c9e82ce9d3413c25f5d35
Dulcie <dulcie.jackson@softwire.com>
2020-09-17 14:58:00 +0100

    GPG-182 Adds page to select new sector

---
+++ GenderPayGap.WebUI/Models/Admin/AdminChangeSectorViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Core;
+using GenderPayGap.Database;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class AdminChangeSectorViewModel : GovUkViewModel
+    {
+
+        public ChangeOrganisationSectorViewModelActions Action { get; set; }
+
+        public SectorTypes? NewSector { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a reason for this change.")]
+        public string Reason { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public Database.Organisation Organisation { get; set; }
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public List<InactiveUserOrganisation> InactiveUserOrganisations { get; set; }
+
+    }
+
+    public enum ChangeOrganisationSectorViewModelActions
+    {
+
+        Unknown,
+        OfferNewSectorAndReason,
+        ConfirmSectorChange
+
+    }
+}
