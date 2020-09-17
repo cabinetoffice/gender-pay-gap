4b477faa76aa9a9bdd01313274b8bf9d94b6fcd3
Dulcie <dulcie.jackson@softwire.com>
2020-09-17 15:05:01 +0100

    GPG-182 Refactors sector history to use audit log

---
+++ GenderPayGap.WebUI/Services/AuditLogModels/AdminChangeSectorAuditLogDetails.cs
@@ -0,0 +1,13 @@
+﻿using GenderPayGap.Core;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class AdminChangeSectorAuditLogDetails
+    {
+
+        public SectorTypes OldSector { get; set; }
+        public SectorTypes NewSector { get; set; }
+        public string Reason { get; set; }
+
+    }
+}
