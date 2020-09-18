5184e17ba34d482807b6bdfd7a660fb344a37eea
James Griffiths <james.griffiths@softwire.com>
2020-09-18 10:45:13 +0100

    GPG-429 Move static pages (Contact Us, etc.) to new StaticPagesController (not based on BaseController)

---
+++ GenderPayGap.WebUI/Controllers/StaticPagesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return new OkResult(); // OK = 200
+        }
+
+        [HttpGet("~/contact-us")]
+        public IActionResult ContactUs()
+        {
+            return View("ContactUs");
+        }
+
+        [HttpGet("report-concerns")]
+        public IActionResult ReportConcerns()
+        {
+            return View("ReportConcerns");
+        }
+
+        [HttpGet("accessibility-statement")]
+        public IActionResult AccessibilityStatement()
+        {
+            return View("AccessibilityStatement");
+        }
+        
+    }
+}
