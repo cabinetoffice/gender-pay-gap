432efd0428f7f20109bf18346e1f849dd6ba64c6
James Griffiths <james.griffiths@softwire.com>
2020-09-21 17:15:07 +0100

    GPG-386 Report Person Responsible In Your organisation page

---
+++ GenderPayGap.WebUI/Models/Report/ReportResponsiblePersonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GovUkDesignSystem;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Report
+{
+    public class ReportResponsiblePersonViewModel : GovUkViewModel
+    {
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public Database.Organisation Organisation { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public int ReportingYear { get; set; }
+
+        public ReportPagesAction Action { get; set; }
+
+        public string ResponsiblePersonFirstName { get; set; }
+        public string ResponsiblePersonLastName { get; set; }
+        public string ResponsiblePersonJobTitle { get; set; }
+
+    }
+}
