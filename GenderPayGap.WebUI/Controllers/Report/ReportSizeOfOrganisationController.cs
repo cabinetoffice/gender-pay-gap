ecee62d361e0e3714cb1bdb6c12f09b0da0daf5b
James Griffiths <james.griffiths@softwire.com>
2020-09-21 17:14:56 +0100

    GPG-386 Report Size Of Organisation page

---
+++ GenderPayGap.WebUI/Controllers/Report/ReportSizeOfOrganisationController.cs
@@ -0,0 +1,126 @@
+﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.Database.Models;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.Report;
+using GenderPayGap.WebUI.Services;
+using GovUkDesignSystem.Parsers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Report
+{
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Route("account/organisations")]
+    public class ReportSizeOfOrganisationController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+        private readonly DraftReturnService draftReturnService;
+
+        public ReportSizeOfOrganisationController(
+            IDataRepository dataRepository,
+            DraftReturnService draftReturnService)
+        {
+            this.dataRepository = dataRepository;
+            this.draftReturnService = draftReturnService;
+        }
+
+
+        [HttpGet("{encryptedOrganisationId}/reporting-year-{reportingYear}/report/size-of-organisation")]
+        public IActionResult ReportSizeOfOrganisationGet(string encryptedOrganisationId, int reportingYear)
+        {
+            ControllerHelper.Throw404IfFeatureDisabled(FeatureFlag.NewReportingJourney);
+
+            long organisationId = ControllerHelper.DecryptOrganisationIdOrThrow404(encryptedOrganisationId);
+            ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+            ControllerHelper.ThrowIfUserDoesNotHavePermissionsForGivenOrganisation(User, dataRepository, organisationId);
+            ControllerHelper.ThrowIfReportingYearIsOutsideOfRange(reportingYear);
+
+            var viewModel = new ReportSizeOfOrganisationViewModel();
+            PopulateViewModelWithOrganisationAndReportingYear(viewModel, organisationId, reportingYear);
+            SetValuesFromDraftReturnOrSubmittedReturn(viewModel, organisationId, reportingYear);
+
+            return View("ReportSizeOfOrganisation", viewModel);
+        }
+
+        private void PopulateViewModelWithOrganisationAndReportingYear(ReportSizeOfOrganisationViewModel viewModel, long organisationId, int reportingYear)
+        {
+            viewModel.Organisation = dataRepository.Get<Organisation>(organisationId);
+            viewModel.ReportingYear = reportingYear;
+        }
+
+        private void SetValuesFromDraftReturnOrSubmittedReturn(ReportSizeOfOrganisationViewModel viewModel, long organisationId, int reportingYear)
+        {
+            DraftReturn draftReturn = draftReturnService.GetDraftReturn(organisationId, reportingYear);
+            if (draftReturn != null)
+            {
+                SetValuesFromDraftReturn(viewModel, draftReturn);
+                return;
+            }
+
+            Organisation organisation = dataRepository.Get<Organisation>(organisationId);
+            Return submittedReturn = organisation.GetReturn(reportingYear);
+            if (submittedReturn != null)
+            {
+                SetValuesFromSubmittedReturn(viewModel, submittedReturn);
+                return;
+            }
+        }
+
+        private void SetValuesFromDraftReturn(ReportSizeOfOrganisationViewModel viewModel, DraftReturn draftReturn)
+        {
+            viewModel.SetSizeOfOrganisation(draftReturn.OrganisationSize);
+        }
+
+        private void SetValuesFromSubmittedReturn(ReportSizeOfOrganisationViewModel viewModel, Return submittedReturn)
+        {
+            viewModel.SetSizeOfOrganisation(submittedReturn.OrganisationSize);
+        }
+
+
+        [HttpPost("{encryptedOrganisationId}/reporting-year-{reportingYear}/report/size-of-organisation")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ReportSizeOfOrganisationPost(string encryptedOrganisationId, int reportingYear, ReportSizeOfOrganisationViewModel viewModel)
+        {
+            ControllerHelper.Throw404IfFeatureDisabled(FeatureFlag.NewReportingJourney);
+
+            long organisationId = ControllerHelper.DecryptOrganisationIdOrThrow404(encryptedOrganisationId);
+            ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+            ControllerHelper.ThrowIfUserDoesNotHavePermissionsForGivenOrganisation(User, dataRepository, organisationId);
+            ControllerHelper.ThrowIfReportingYearIsOutsideOfRange(reportingYear);
+
+            ValidateUserInput(viewModel);
+
+            if (viewModel.HasAnyErrors())
+            {
+                PopulateViewModelWithOrganisationAndReportingYear(viewModel, organisationId, reportingYear);
+                return View("ReportSizeOfOrganisation", viewModel);
+            }
+
+            SaveChangesToDraftReturn(viewModel, organisationId, reportingYear);
+
+            string nextPageUrl = viewModel.Action == ReportPagesAction.Save
+                ? Url.Action("ReportSizeOfOrganisationGet", "ReportSizeOfOrganisation", new {encryptedOrganisationId = encryptedOrganisationId, reportingYear = reportingYear})
+                : Url.Action("ReportOverview", "ReportOverview", new {encryptedOrganisationId = encryptedOrganisationId, reportingYear = reportingYear});
+            StatusMessageHelper.SetStatusMessage(Response, "Saved changes to size of organisation", nextPageUrl);
+            return LocalRedirect(nextPageUrl);
+        }
+
+        private void ValidateUserInput(ReportSizeOfOrganisationViewModel viewModel)
+        {
+            viewModel.ParseAndValidateParameters(Request, m => m.SizeOfOrganisation);
+        }
+
+        private void SaveChangesToDraftReturn(ReportSizeOfOrganisationViewModel viewModel, long organisationId, int reportingYear)
+        {
+            DraftReturn draftReturn = draftReturnService.GetOrCreateDraftReturn(organisationId, reportingYear);
+
+            draftReturn.OrganisationSize = viewModel.GetSizeOfOrganisation();
+
+            draftReturnService.SaveDraftReturnOrDeleteIfNotRelevent(draftReturn);
+        }
+
+    }
+}
---
+++ GenderPayGap.WebUI/Models/Report/ReportSizeOfOrganisationViewModel.cs
@@ -0,0 +1,90 @@
+﻿using GenderPayGap.Core;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Report
+{
+    public class ReportSizeOfOrganisationViewModel : GovUkViewModel
+    {
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public Database.Organisation Organisation { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public int ReportingYear { get; set; }
+
+        public ReportPagesAction Action { get; set; }
+
+        public ReportSizeOfOrganisation? SizeOfOrganisation { get; set; }
+
+        public OrganisationSizes? GetSizeOfOrganisation()
+        {
+            switch (SizeOfOrganisation)
+            {
+                case ReportSizeOfOrganisation.Employees0To249:
+                    return OrganisationSizes.Employees0To249;
+                case ReportSizeOfOrganisation.Employees250To499:
+                    return OrganisationSizes.Employees250To499;
+                case ReportSizeOfOrganisation.Employees500To999:
+                    return OrganisationSizes.Employees500To999;
+                case ReportSizeOfOrganisation.Employees1000To4999:
+                    return OrganisationSizes.Employees1000To4999;
+                case ReportSizeOfOrganisation.Employees5000To19999:
+                    return OrganisationSizes.Employees5000To19999;
+                case ReportSizeOfOrganisation.Employees20000OrMore:
+                    return OrganisationSizes.Employees20000OrMore;
+                case null:
+                default:
+                    return null;
+            }
+        }
+
+        public void SetSizeOfOrganisation(OrganisationSizes? organisationSize)
+        {
+            switch (organisationSize)
+            {
+                case OrganisationSizes.Employees0To249:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees0To249;
+                    break;
+                case OrganisationSizes.Employees250To499:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees250To499;
+                    break;
+                case OrganisationSizes.Employees500To999:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees500To999;
+                    break;
+                case OrganisationSizes.Employees1000To4999:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees1000To4999;
+                    break;
+                case OrganisationSizes.Employees5000To19999:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees5000To19999;
+                    break;
+                case OrganisationSizes.Employees20000OrMore:
+                    SizeOfOrganisation = ReportSizeOfOrganisation.Employees20000OrMore;
+                    break;
+                case OrganisationSizes.NotProvided:
+                case null:
+                default:
+                    SizeOfOrganisation = null;
+                    break;
+            }
+        }
+
+    }
+
+    public enum ReportSizeOfOrganisation
+    {
+        [GovUkRadioCheckboxLabelText(Text = "Less than 250 and I wish to report voluntarily")]
+        Employees0To249,
+        [GovUkRadioCheckboxLabelText(Text = "250 to 499")]
+        Employees250To499,
+        [GovUkRadioCheckboxLabelText(Text = "500 to 999")]
+        Employees500To999,
+        [GovUkRadioCheckboxLabelText(Text = "1000 to 4999")]
+        Employees1000To4999,
+        [GovUkRadioCheckboxLabelText(Text = "5000 to 19,999")]
+        Employees5000To19999,
+        [GovUkRadioCheckboxLabelText(Text = "20,000 or more")]
+        Employees20000OrMore
+    }
+}
