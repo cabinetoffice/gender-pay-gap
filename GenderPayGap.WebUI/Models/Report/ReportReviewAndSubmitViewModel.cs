684a6610c4bb7c168ef2360c3beee866957821a5
James Griffiths <james.griffiths@softwire.com>
2020-09-23 10:42:52 +0100

    GPG-386 Report Review/Submit & Late Submission pages (excluding save)

---
+++ GenderPayGap.WebUI/Models/Report/ReportReviewAndSubmitViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GenderPayGap.Database.Models;
+using GovUkDesignSystem;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Report
+{
+    public class ReportReviewAndSubmitViewModel : GovUkViewModel
+    {
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public Database.Organisation Organisation { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public int ReportingYear { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public DraftReturn DraftReturn { get; set; }
+
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public bool WillBeLateSubmission { get; set; }
+
+    }
+}
--- GenderPayGap.WebUI/Services/DraftReturnService.cs
+++ GenderPayGap.WebUI/Services/DraftReturnService.cs
@@ -1,7 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using GenderPayGap.Core;
+using GenderPayGap.Core.Classes;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.Database.Models;
+using GenderPayGap.Extensions;
+using GenderPayGap.WebUI.Helpers;
 
 namespace GenderPayGap.WebUI.Services
 {
@@ -63,10 +68,10 @@ namespace GenderPayGap.WebUI.Services
                 draftReturn.DiffMeanHourlyPayPercent == null
                 && draftReturn.DiffMedianHourlyPercent == null
 
-                && draftReturn.DiffMeanBonusPercent == null
-                && draftReturn.DiffMedianBonusPercent == null
                 && draftReturn.MaleMedianBonusPayPercent == null
                 && draftReturn.FemaleMedianBonusPayPercent == null
+                && draftReturn.DiffMeanBonusPercent == null
+                && draftReturn.DiffMedianBonusPercent == null
 
                 && draftReturn.MaleLowerPayBand == null
                 && draftReturn.FemaleLowerPayBand == null
@@ -121,7 +126,91 @@ namespace GenderPayGap.WebUI.Services
                 ;
         }
 
+        public bool DraftReturnExistsAndIsComplete(long organisationId, int reportingYear)
+        {
+            DraftReturn draftReturn = GetDraftReturn(organisationId, reportingYear);
 
+            return HourlyPaySectionIsComplete(draftReturn)
+                   && BonusPaySectionIsComplete(draftReturn)
+                   && EmployeesByPayQuartileSectionIsComplete(draftReturn)
+                   && ResponsiblePersonSectionIsComplete(draftReturn)
+                   && OrganisationSizeSectionIsComplete(draftReturn)
+                   && WebsiteLinkSectionIsComplete(draftReturn);
+        }
+
+        private static bool HourlyPaySectionIsComplete(DraftReturn draftReturn)
+        {
+            return draftReturn.DiffMeanHourlyPayPercent != null
+                   && draftReturn.DiffMedianHourlyPercent != null;
+        }
+
+        private static bool BonusPaySectionIsComplete(DraftReturn draftReturn)
+        {
+            bool noBonusesPaid =
+                draftReturn.MaleMedianBonusPayPercent == 0
+                && draftReturn.FemaleMedianBonusPayPercent == 0;
+
+            bool allValuesComplete =
+                draftReturn.MaleMedianBonusPayPercent != null
+                && draftReturn.FemaleMedianBonusPayPercent != null
+                && draftReturn.DiffMeanBonusPercent != null
+                && draftReturn.DiffMedianBonusPercent != null;
+
+            return noBonusesPaid || allValuesComplete;
+        }
+
+        private static bool EmployeesByPayQuartileSectionIsComplete(DraftReturn draftReturn)
+        {
+            return draftReturn.MaleLowerPayBand != null
+                   && draftReturn.FemaleLowerPayBand != null
+                   && draftReturn.MaleMiddlePayBand != null
+                   && draftReturn.FemaleMiddlePayBand != null
+                   && draftReturn.MaleUpperPayBand != null
+                   && draftReturn.FemaleUpperPayBand != null
+                   && draftReturn.MaleUpperQuartilePayBand != null
+                   && draftReturn.FemaleUpperQuartilePayBand != null;
+        }
+
+        private static bool ResponsiblePersonSectionIsComplete(DraftReturn draftReturn)
+        {
+            return !string.IsNullOrWhiteSpace(draftReturn.FirstName)
+                   && !string.IsNullOrWhiteSpace(draftReturn.LastName)
+                   && !string.IsNullOrWhiteSpace(draftReturn.JobTitle);
+        }
+
+        private static bool OrganisationSizeSectionIsComplete(DraftReturn draftReturn)
+        {
+            return draftReturn.OrganisationSize != null;
+        }
+
+        private static bool WebsiteLinkSectionIsComplete(DraftReturn draftReturn)
+        {
+            // The website link is optional, so we should allow it to be missing
+            bool linkIsMissing = string.IsNullOrEmpty(draftReturn.CompanyLinkToGPGInfo);
+            bool linkIsValid = UriSanitiser.IsValidHttpOrHttpsLink(draftReturn.CompanyLinkToGPGInfo);
+
+            return linkIsMissing || linkIsValid;
+        }
+
+        public bool DraftReturnWouldBeLateIfSubmittedNow(DraftReturn draftReturn)
+        {
+            Organisation organisation = dataRepository.Get<Organisation>(draftReturn.OrganisationId);
+            int reportingYear = draftReturn.SnapshotYear;
+
+            DateTime snapshotDate = organisation.SectorType.GetAccountingStartDate(reportingYear);
+
+            bool isLate = VirtualDateTime.Now > snapshotDate.AddYears(1);
+            bool isMandatory = draftReturn.OrganisationSize != OrganisationSizes.Employees0To249;
+            bool isInScope = organisation.GetScopeForYear(reportingYear).IsInScopeVariant();
+            bool yearIsNotExcluded = !Global.ReportingStartYearsToExcludeFromLateFlagEnforcement.Contains(reportingYear);
+
+            // TODO: Add logic to take into account whether this is:
+            // - the first submission this year,
+            // - an edit
+            //   - whether the previous submission was late
+
+            return isLate && isMandatory && isInScope && yearIsNotExcluded;
+        }
 
     }
 }
