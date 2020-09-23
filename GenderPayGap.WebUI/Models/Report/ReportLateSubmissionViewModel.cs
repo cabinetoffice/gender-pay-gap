1bc8a3c0dfa950be6b3f26556464f8e507a0cc50
James Griffiths <james.griffiths@softwire.com>
2020-09-23 23:53:45 +0100

    GPG-386 Rename Ehrc property on ReportLateSubmissionViewModel

--- GenderPayGap.WebUI/Models/Report/ReportLateSubmissionViewModel.cs
+++ GenderPayGap.WebUI/Models/Report/ReportLateSubmissionViewModel.cs
@@ -18,13 +18,13 @@ namespace GenderPayGap.WebUI.Models.Report
         public DateTime DeadlineDate { get; set; }
 
         [GovUkValidateRequired(ErrorMessageIfMissing = "Select yes if you have received a letter about a breach of the regulations from the Equality and Human Rights Commission.")]
-        public ReportLateSubmissionContactFromEhrc? ContactFromEhrc { get; set; }
+        public ReportLateSubmissionReceivedLetterFromEhrc? ReceivedLetterFromEhrc { get; set; }
         [GovUkValidateRequired(ErrorMessageIfMissing = "Explain why your organisation is reporting or changing your gender pay gap figures after the deadline.")]
         public string Reason { get; set; }
 
     }
 
-    public enum ReportLateSubmissionContactFromEhrc
+    public enum ReportLateSubmissionReceivedLetterFromEhrc
     {
         Yes,
         No
