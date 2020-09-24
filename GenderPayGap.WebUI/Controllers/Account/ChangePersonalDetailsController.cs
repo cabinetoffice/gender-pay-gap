ddc260c7de7468bfef350b84d07c92d03e9d4bb8
Dulcie <dulcie.jackson@softwire.com>
2020-09-24 14:45:02 +0100

    GPG-420 Validates and checks new personal details and redirects

--- GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
+++ GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
@@ -1,5 +1,6 @@
-using GenderPayGap.Core.Interfaces;
+﻿using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
+using GenderPayGap.WebUI.Classes;
 using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Models.Account;
 using GovUkDesignSystem.Parsers;
@@ -39,9 +40,36 @@ namespace GenderPayGap.WebUI.Controllers.Account
         // The user submits some new personal details
         // We validate it (e.g. that all fields are filled in)
         [HttpPost("change-personal-details")]
+        [PreventDuplicatePost]
+        [ValidateAntiForgeryToken]
         public IActionResult ChangePersonalDetailsPost(ChangePersonalDetailsViewModel viewModel)
         {
-            return null;
+            // We don't validate contact number, since this is manually checked and we'll accept anything here
+            viewModel.ParseAndValidateParameters(Request, m => m.FirstName);
+            viewModel.ParseAndValidateParameters(Request, m => m.LastName);
+            viewModel.ParseAndValidateParameters(Request, m => m.JobTitle);
+
+            if (viewModel.HasAnyErrors())
+            {
+                return View("ChangePersonalDetails", viewModel);
+            }
+            
+            // Get the user db entry
+            User currentUser = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
+            
+            // Update the user's information
+            currentUser.Firstname = viewModel.FirstName;
+            currentUser.Lastname = viewModel.LastName;
+            currentUser.JobTitle = viewModel.JobTitle;
+            currentUser.ContactPhoneNumber = viewModel.ContactPhoneNumber;
+
+            // Save updates
+            dataRepository.SaveChangesAsync().Wait();
+            
+            // TODO: Add success notification to show on ManageAccount page after redirect
+
+            // Return user to the Manage Account page
+            return RedirectToAction("ManageAccount", "ManageAccount", new { Area = "Account" });
         }
 
     }
