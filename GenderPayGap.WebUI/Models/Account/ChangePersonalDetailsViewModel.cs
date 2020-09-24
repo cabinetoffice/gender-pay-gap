36b5cb284ff4f9563b2fec31e1c8f26b5178f100
Dulcie <dulcie.jackson@softwire.com>
2020-09-24 10:29:28 +0100

    GPG-420 Creates new Model, View and Controller

---
+++ GenderPayGap.WebUI/Models/Account/ChangePersonalDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Account
+{
+    public class ChangePersonalDetailsViewModel : GovUkViewModel
+    {
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your first name")]
+        public string FirstName { get; set; }
+        
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your last name")]
+        public string LastName { get; set; }
+        
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your job title")]
+        public string JobTitle { get; set; }
+
+    }
+}
