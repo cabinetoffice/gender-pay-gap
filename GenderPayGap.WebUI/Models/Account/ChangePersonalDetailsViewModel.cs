d6c1a0bffe083ead7af5ab1214f0b4f399e973b0
Dulcie <dulcie.jackson@softwire.com>
2020-09-24 14:45:02 +0100

    GPG-420 Validates and checks new personal details and redirects

--- GenderPayGap.WebUI/Models/Account/ChangePersonalDetailsViewModel.cs
+++ GenderPayGap.WebUI/Models/Account/ChangePersonalDetailsViewModel.cs
@@ -6,13 +6,13 @@ namespace GenderPayGap.WebUI.Models.Account
 {
     public class ChangePersonalDetailsViewModel : GovUkViewModel
     {
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your first name")]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "You need to enter your first name")]
         public string FirstName { get; set; }
         
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your last name")]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "You need to enter your last name")]
         public string LastName { get; set; }
         
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your job title")]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "You need to enter your job title")]
         public string JobTitle { get; set; }
 
         public string ContactPhoneNumber { get; set; }
