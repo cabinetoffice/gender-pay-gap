1e26a7c83f816616114eef75f07da92ae6a3c6f1
Dulcie <dulcie.jackson@softwire.com>
2020-09-24 14:13:18 +0100

    GPG-420 Adds GET endpoint functionality and form inputs

--- GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
+++ GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
@@ -1,4 +1,8 @@
-﻿using GenderPayGap.WebUI.Models.Account;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.Account;
+using GovUkDesignSystem.Parsers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GenderPayGap.WebUI.Controllers.Account
@@ -6,6 +10,14 @@ namespace GenderPayGap.WebUI.Controllers.Account
     [Route("manage-account")]
     public class ChangePersonalDetailsController : Controller
     {
+        
+        private readonly IDataRepository dataRepository;
+
+        public ChangePersonalDetailsController(
+            IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
 
         // The user asks to change their personal details
         // We direct them to a page that asks for their updated personal details
@@ -13,6 +25,14 @@ namespace GenderPayGap.WebUI.Controllers.Account
         public IActionResult ChangePersonalDetailsGet()
         {
             var viewModel = new ChangePersonalDetailsViewModel();
+
+            User currentUser = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
+
+            viewModel.FirstName = currentUser.Firstname;
+            viewModel.LastName = currentUser.Lastname;
+            viewModel.JobTitle = currentUser.JobTitle;
+            viewModel.ContactPhoneNumber = currentUser.ContactPhoneNumber;
+            
             return View("ChangePersonalDetails", viewModel);
         }
         
