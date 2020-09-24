e60d4b176b812104756186d835d5a28e35c87e31
Dulcie <dulcie.jackson@softwire.com>
2020-09-24 10:29:28 +0100

    GPG-420 Creates new Model, View and Controller

---
+++ GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
@@ -0,0 +1,28 @@
+﻿using GenderPayGap.WebUI.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Account
+{
+    [Route("manage-account")]
+    public class ChangePersonalDetailsController : Controller
+    {
+
+        // The user asks to change their personal details
+        // We direct them to a page that asks for their updated personal details
+        [HttpGet("change-personal-details")]
+        public IActionResult ChangePersonalDetailsGet()
+        {
+            var viewModel = new ChangePersonalDetailsViewModel();
+            return View("ChangePersonalDetails", viewModel);
+        }
+        
+        // The user submits some new personal details
+        // We validate it (e.g. that all fields are filled in)
+        [HttpPost("change-personal-details")]
+        public IActionResult ChangePersonalDetailsPost(ChangePersonalDetailsViewModel viewModel)
+        {
+            return null;
+        }
+
+    }
+}
