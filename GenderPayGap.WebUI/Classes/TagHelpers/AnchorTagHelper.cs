94430743c7fda7d9aba3d3ffd730b3264e25ac25
James Griffiths <james.griffiths@softwire.com>
2020-09-28 18:11:24 +0100

    GPG-449 Add custom tracking rel manually, rather than through AnchorTagHelper (ATH causes too many problems)

--- GenderPayGap.WebUI/Classes/TagHelpers/AnchorTagHelper.cs
+++ GenderPayGap.WebUI/Classes/TagHelpers/AnchorTagHelper.cs
@@ -27,29 +27,6 @@ namespace GenderPayGap.WebUI.Classes.TagHelpers
                 rels.AddRange(output.Attributes["rel"].Value.ToStringOrEmpty().SplitI(";, "));
             }
 
-            #region Google Analytics Tracking
-
-            //Check if link should be tracked
-            //TODO check what these fields mean
-            bool isTracked = output.Attributes.ContainsName("data-track-category")
-                             || output.Attributes.ContainsName("data-track-action")
-                             || output.Attributes.ContainsName("data-track-label")
-                             || output.Attributes.ContainsName("data-track-options");
-
-            //Add the track relationship
-            if (isTracked)
-            {
-                rels.Add("track");
-            }
-
-            //Ensure we have at least a category when tracking
-            if (!output.Attributes.ContainsName("data-track-category") && rels.Contains("track"))
-            {
-                throw new ArgumentNullException("data-track-category", "You must specify a category for tracking");
-            }
-
-            #endregion
-
             //Save the relationship attribute changes
             if (rels.Any())
             {
