5ca972465e848b543842a4376f61525556990817
Dulcie <dulcie.jackson@softwire.com>
2020-09-28 15:13:00 +0100

    GPG-410 Adds Organisation and OrganisationScope builders

---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/Builders/OrganisationBuilder.cs
@@ -0,0 +1,55 @@
+﻿using GenderPayGap.Core;
+using GenderPayGap.Database;
+
+namespace GenderPayGap.WebUI.Tests.Builders
+{
+    public class OrganisationBuilder
+    {
+
+        private long organisationId = 1;
+        private string organisationName = "Test Organisation Ltd";
+        private string companyNumber = "12345678";
+        private SectorTypes sectorType = SectorTypes.Public;
+
+        public OrganisationBuilder WithOrganisationId(long organisationId)
+        {
+            this.organisationId = organisationId;
+            return this;
+        }
+
+        public OrganisationBuilder WithOrganisationName(string organisationName)
+        {
+            this.organisationName = organisationName;
+            return this;
+        }
+
+        public OrganisationBuilder WithCompanyNumber(string companyNumber)
+        {
+            this.companyNumber = companyNumber;
+            return this;
+        }
+
+        public OrganisationBuilder WithSectorType(SectorTypes sectorType)
+        {
+            this.sectorType = sectorType;
+            return this;
+        }
+
+        private Organisation Build()
+        {
+            return new Organisation
+            {
+                OrganisationId = organisationId,
+                OrganisationName = organisationName,
+                CompanyNumber = companyNumber,
+                SectorType = sectorType
+            };
+        }
+        
+        public static implicit operator Organisation(OrganisationBuilder instance)
+        {
+            return instance.Build();
+        }
+
+    }
+}
---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/Builders/OrganisationScopeBuilder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using GenderPayGap.Core;
+using GenderPayGap.Core.Classes;
+using GenderPayGap.Database;
+
+namespace GenderPayGap.WebUI.Tests.Builders
+{
+    public class OrganisationScopeBuilder
+    {
+
+        private Organisation organisation = new OrganisationBuilder();
+        private bool readGuidance = false;
+        private ScopeStatuses scopeStatus = ScopeStatuses.PresumedInScope;
+        private string reason = "A reason";
+        private ScopeRowStatuses status = ScopeRowStatuses.Active;
+        private int reportingYear = 0;
+
+        public OrganisationScopeBuilder WithOrganisation(Organisation organisation)
+        {
+            this.organisation = organisation;
+            return this;
+        }
+
+        public OrganisationScopeBuilder WithReadGuidance(bool readGuidance)
+        {
+            this.readGuidance = readGuidance;
+            return this;
+        }
+
+        public OrganisationScopeBuilder WithScopeStatus(ScopeStatuses scopeStatus)
+        {
+            this.scopeStatus = scopeStatus;
+            return this;
+        }
+
+        public OrganisationScopeBuilder WithReason(string reason)
+        {
+            this.reason = reason;
+            return this;
+        }
+
+        public OrganisationScopeBuilder WithStatus(ScopeRowStatuses status)
+        {
+            this.status = status;
+            return this;
+        }
+
+        public OrganisationScopeBuilder WithReportingYear(int reportingYear)
+        {
+            this.reportingYear = reportingYear;
+            return this;
+        }
+
+        private OrganisationScope Build()
+        {
+            return new OrganisationScope
+            {
+                Organisation = organisation,
+                OrganisationId = organisation.OrganisationId,
+                ReadGuidance = readGuidance,
+                ScopeStatus = scopeStatus,
+                Reason = reason,
+                Status = status,
+                SnapshotDate = organisation.SectorType.GetAccountingStartDate(reportingYear)
+            };
+        }
+        
+        public static implicit operator OrganisationScope(OrganisationScopeBuilder instance)
+        {
+            return instance.Build();
+        }
+
+    }
+}
