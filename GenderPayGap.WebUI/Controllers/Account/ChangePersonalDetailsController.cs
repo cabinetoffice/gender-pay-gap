5239ab23a0e555f8c214a95ea6e37b5278222416
James Griffiths <james.griffiths@softwire.com>
2020-09-30 12:13:39 +0100

    GPG-420 Audit Log user details changes

--- GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
+++ GenderPayGap.WebUI/Controllers/Account/ChangePersonalDetailsController.cs
@@ -1,9 +1,11 @@
-﻿using GenderPayGap.Core.Interfaces;
+﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.Extensions;
 using GenderPayGap.WebUI.Classes;
 using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Models.Account;
+using GenderPayGap.WebUI.Services;
 using GovUkDesignSystem.Parsers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,11 +16,14 @@ namespace GenderPayGap.WebUI.Controllers.Account
     {
         
         private readonly IDataRepository dataRepository;
+        private readonly AuditLogger auditLogger;
 
         public ChangePersonalDetailsController(
-            IDataRepository dataRepository)
+            IDataRepository dataRepository,
+            AuditLogger auditLogger)
         {
             this.dataRepository = dataRepository;
+            this.auditLogger = auditLogger;
         }
 
         // The user asks to change their personal details
@@ -62,6 +67,7 @@ namespace GenderPayGap.WebUI.Controllers.Account
             // Get the user db entry
             User currentUser = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
             
+            AuditLogChanges(currentUser, viewModel);
             SaveUserDetails(currentUser, viewModel);
 
             string nextPageUrl = Url.Action("ManageAccount", "ManageAccount", new {Area = "Account"});
@@ -69,6 +75,57 @@ namespace GenderPayGap.WebUI.Controllers.Account
 
             // Return user to the Manage Account page
             return LocalRedirect(nextPageUrl);
+        }
+
+        private void AuditLogChanges(User user, ChangePersonalDetailsViewModel viewModel)
+        {
+            if (user.Firstname != viewModel.FirstName ||
+               user.Lastname != viewModel.LastName ||
+               user.ContactFirstName != viewModel.FirstName ||
+               user.ContactLastName != viewModel.LastName)
+            {
+                auditLogger.AuditChangeToUser(
+                    AuditedAction.UserChangeName,
+                    user,
+                    new
+                    {
+                        OldFirstName = user.Firstname,
+                        OldLastName = user.Lastname,
+                        OldContactFirstName = user.ContactFirstName,
+                        OldContactLastName = user.ContactLastName,
+                        NewFirstName = viewModel.FirstName,
+                        NewLastName = viewModel.LastName,
+                    },
+                    user);
+            }
+
+            if (user.JobTitle != viewModel.JobTitle ||
+                user.ContactJobTitle != viewModel.JobTitle)
+            {
+                auditLogger.AuditChangeToUser(
+                    AuditedAction.UserChangeJobTitle,
+                    user,
+                    new
+                    {
+                        OldJobTitle = user.JobTitle,
+                        OldContactJobTitle = user.ContactJobTitle,
+                        NewJobTitle = viewModel.JobTitle,
+                    },
+                    user);
+            }
+
+            if (user.ContactPhoneNumber != viewModel.ContactPhoneNumber)
+            {
+                auditLogger.AuditChangeToUser(
+                    AuditedAction.UserChangePhoneNumber,
+                    user,
+                    new
+                    {
+                        OldContactPhoneNumber = user.ContactPhoneNumber,
+                        NewContactPhoneNumber = viewModel.ContactPhoneNumber,
+                    },
+                    user);
+            }
         }
 
         private void SaveUserDetails(User currentUser, ChangePersonalDetailsViewModel viewModel)
