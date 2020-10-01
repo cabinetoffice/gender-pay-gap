6ad7333bb2a62619663a99ba4fe478ddba394b66
Dulcie <dulcie.jackson@softwire.com>
2020-10-01 15:03:08 +0100

    GPG-419 Adds new page and functionality for change password journey

---
+++ GenderPayGap.WebUI/Models/Account/ChangePasswordNewViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Account
+{
+    public class ChangePasswordNewViewModel : GovUkViewModel
+    {
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter your current password")]
+        public string CurrentPassword { get; set; }
+        
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please enter a new password")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "New password must be at least 8 characters long")]
+        public string NewPassword { get; set; }
+        
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Please confirm your new password")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
