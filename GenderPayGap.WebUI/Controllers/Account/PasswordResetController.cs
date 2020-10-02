7429fbee40059f229e607f526780b7a4f36b316b
Dulcie <dulcie.jackson@softwire.com>
2020-10-02 10:38:28 +0100

    GPG-424 Creates new PasswordResetController

---
+++ GenderPayGap.WebUI/Controllers/Account/PasswordResetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Account
+{
+    public class PasswordResetController : Controller
+    {
+
+        [HttpGet("password-reset")]
+        public IActionResult PasswordResetGet()
+        {
+            
+        }
+
+    }
+}
