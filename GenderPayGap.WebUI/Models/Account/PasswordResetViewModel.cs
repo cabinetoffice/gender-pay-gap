c6d8cf66be4924ecce143a4cf441b3c184c12886
Dulcie <dulcie.jackson@softwire.com>
2020-10-02 17:15:50 +0100

    GPG-424 Adds new password reset page with validation

---
+++ GenderPayGap.WebUI/Models/Account/PasswordResetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Account
+{
+    public class PasswordResetViewModel : GovUkViewModel
+    {
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter your email address")]
+        public string EmailAddress { get; set; }
+
+    }
+}
