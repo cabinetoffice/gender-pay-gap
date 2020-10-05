e1c90abc69905f72babe2119e0e4f64d3682a6d2
James Griffiths <james.griffiths@softwire.com>
2020-10-05 18:03:11 +0100

    GPG-497 Set cookie settings for login cookie

--- GenderPayGap.WebUI/Helpers/LoginHelper.cs
+++ GenderPayGap.WebUI/Helpers/LoginHelper.cs
@@ -51,26 +51,22 @@ namespace GenderPayGap.WebUI.Helpers
 
             var authProperties = new AuthenticationProperties
             {
-                //AllowRefresh = <bool>,
+                AllowRefresh = true,
                 // Refreshing the authentication session should be allowed.
 
-                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                ExpiresUtc = DateTimeOffset.Now.AddDays(7),
                 // The time at which the authentication ticket expires. A 
                 // value set here overrides the ExpireTimeSpan option of 
                 // CookieAuthenticationOptions set with AddCookie.
 
-                //IsPersistent = true,
+                IsPersistent = true,
                 // Whether the authentication session is persisted across 
                 // multiple requests. When used with cookies, controls
                 // whether the cookie's lifetime is absolute (matching the
                 // lifetime of the authentication ticket) or session-based.
 
-                //IssuedUtc = <DateTimeOffset>,
+                IssuedUtc = DateTimeOffset.Now,
                 // The time at which the authentication ticket was issued.
-
-                //RedirectUri = <string>
-                // The full path or absolute URI to be used as an http 
-                // redirect response value.
             };
 
             httpContext
