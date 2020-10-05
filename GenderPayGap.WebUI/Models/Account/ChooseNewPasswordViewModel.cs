24250a1c917994387491f94e8b23cbd43d11b760
Dulcie <dulcie.jackson@softwire.com>
2020-10-05 17:40:59 +0100

    GPG-424 Adds model, view and controller get method to reset password
    GPG-424 Adds functions to decrypt code, get user and validate send date

---
+++ GenderPayGap.WebUI/Models/Account/ChooseNewPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GenderPayGap.Database;
+using GovUkDesignSystem;
+using GovUkDesignSystem.Attributes.ValidationAttributes;
+
+namespace GenderPayGap.WebUI.Models.Account
+{
+    public class ChooseNewPasswordViewModel : GovUkViewModel
+    {
+        public User User { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Enter a new password")]
+        public string NewPassword { get; set; }
+        
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Confirm your new password")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
