f8d6cd965b2e26331e4fa95b87afa7744769e2be
Dulcie <dulcie.jackson@softwire.com>
2020-10-05 14:20:28 +0100

    GPG-424 Adds custom error page and sends password reset email

--- GenderPayGap.WebUI/Controllers/Account/PasswordResetController.cs
+++ GenderPayGap.WebUI/Controllers/Account/PasswordResetController.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using GenderPayGap.Core;
+using GenderPayGap.Core.Classes.Logger;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.Extensions;
+using GenderPayGap.WebUI.BusinessLogic.Abstractions;
 using GenderPayGap.WebUI.Classes;
+using GenderPayGap.WebUI.ErrorHandling;
 using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Models.Account;
+using GenderPayGap.WebUI.Repositories;
+using GenderPayGap.WebUI.Services;
 using GovUkDesignSystem;
 using GovUkDesignSystem.Parsers;
 using Microsoft.AspNetCore.Mvc;
@@ -17,10 +23,17 @@ namespace GenderPayGap.WebUI.Controllers.Account
     {
 
         private readonly IDataRepository dataRepository;
+        private readonly IUserRepository userRepository;
+        private readonly EmailSendingService emailSendingService;
 
-        public PasswordResetController(IDataRepository dataRepository)
+        public PasswordResetController(
+            IDataRepository dataRepository,
+            IUserRepository userRepository,
+            EmailSendingService emailSendingService)
         {
             this.dataRepository = dataRepository;
+            this.userRepository = userRepository;
+            this.emailSendingService = emailSendingService;
         }
 
         [HttpGet("password-reset")]
@@ -42,7 +55,7 @@ namespace GenderPayGap.WebUI.Controllers.Account
             }
 
             // Find user associated with email address
-            User userForPasswordReset = dataRepository.GetAll<User>().FirstOrDefault(u => u.EmailAddress == viewModel.EmailAddress);
+            User userForPasswordReset = userRepository.FindByEmailAsync(viewModel.EmailAddress, UserStatuses.Active).Result;
 
             if (userForPasswordReset.IsNull())
             {
@@ -51,13 +64,28 @@ namespace GenderPayGap.WebUI.Controllers.Account
                 return View("PasswordReset", viewModel); 
             }
 
-            // If password reset email has been sent (and password hasn't been changed) within the last 10 minutes
+            // If password reset email has been sent (and password hasn't been changed) within the last 10 minutes, show an error page
             if (userForPasswordReset.ResetSendDate.HasValue && (userForPasswordReset.ResetSendDate.Value - DateTime.Now).TotalMinutes < 10)
             {
-                // TODO: Redirect to custom error page
+                throw new UserRecentlySentPasswordResetEmailWithoutChangingPassword();
             }
             
+            SendPasswordResetEmail(userForPasswordReset);
+
             return null;
+        }
+
+        private void SendPasswordResetEmail(User userForPasswordReset)
+        {
+            // Send a password reset link to the user's email address
+            string resetCode = Encryption.EncryptQuerystring(userForPasswordReset.UserId + ":" + VirtualDateTime.Now.ToSmallDateTime());
+            // TODO: Update this action to an updated page
+            string resetUrl = Url.Action("NewPassword", "Register", new { code = resetCode }, "https");
+            emailSendingService.SendResetPasswordVerificationEmail(userForPasswordReset.EmailAddress, resetUrl);
+
+            CustomLogger.Information(
+                "Password reset sent",
+                $"User ID: {userForPasswordReset.UserId}, Email:{userForPasswordReset.EmailAddress}");
         }
         
     }
