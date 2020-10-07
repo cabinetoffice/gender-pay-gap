88ec27fc9c1c586c10be675b1829348ed101b460
Dulcie <dulcie.jackson@softwire.com>
2020-10-07 16:21:10 +0100

    GPG-424 Adds password reset code to user

---
+++ GenderPayGap.Database/Migrations/20201007151941_AddPasswordResetCodeToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GenderPayGap.Database.Migrations
+{
+    public partial class AddPasswordResetCodeToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetCode",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCode",
+                table: "Users");
+        }
+    }
+}
