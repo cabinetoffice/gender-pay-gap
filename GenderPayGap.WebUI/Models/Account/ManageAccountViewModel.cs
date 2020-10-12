cb1e683f618f1723512c7c62d64a72dc2eae3fb5
Dulcie <dulcie.jackson@softwire.com>
2020-10-12 15:32:23 +0100

    GPG-423 Adds new model, view and controller for ManageAccount page

---
+++ GenderPayGap.WebUI/Models/Account/ManageAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GenderPayGap.Database;
+using GovUkDesignSystem;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Account
+{
+    public class ManageAccountViewModel : GovUkViewModel
+    {
+
+        [BindNever]
+        public User User { get; set; }
+
+    }
+}
