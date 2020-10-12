41e6ae65d1450c224868215e01b409cd04d0648d
Dulcie <dulcie.jackson@softwire.com>
2020-10-12 16:29:10 +0100

    GPG-423 Starts adding content to new Manage Account page

--- GenderPayGap.WebUI/Controllers/Account/ManageAccountController.cs
+++ GenderPayGap.WebUI/Controllers/Account/ManageAccountController.cs
@@ -1,6 +1,5 @@
 ﻿using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
-using GenderPayGap.WebUI.ErrorHandling;
 using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Models.Account;
 using Microsoft.AspNetCore.Authorization;
@@ -18,25 +17,18 @@ namespace GenderPayGap.WebUI.Controllers.Account
         {
             this.dataRepository = dataRepository;
         }
-
-        // The user asks to close their account
-        // We direct them to a page that asks for confirmation
+        
         [HttpGet("manage-account-new")]
         public IActionResult ManageAccountGet()
         {
-            // Admin impersonating a user shouldn't be able to view the manage account page
-            if (LoginHelper.IsUserBeingImpersonated(User))
-            {
-                throw new PageNotFoundException();
-            }
-            
             // Get the current user
             User currentUser = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
 
             // Set properties on view model
             var viewModel = new ManageAccountViewModel
             {
-                User = currentUser
+                User = currentUser,
+                IsUserBeingImpersonated = LoginHelper.IsUserBeingImpersonated(User)
             };
 
             // Return the Manage Account page
