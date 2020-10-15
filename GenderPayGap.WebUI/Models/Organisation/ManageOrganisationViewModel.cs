97e936a592fd8201881acd3b1134e05119517fc1
Dulcie <dulcie.jackson@softwire.com>
2020-10-15 17:12:24 +0100

    GPG-387 Adds Manage Organisation controller method, model and view

---
+++ GenderPayGap.WebUI/Models/Organisation/ManageOrganisationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Database;
+using GovUkDesignSystem;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Organisation
+{
+    public class ManageOrganisationViewModel : GovUkViewModel
+    {
+
+        [BindNever]
+        public Database.Organisation Organisation { get; set; }
+        
+        [BindNever]
+        public List<int> YearsWithDraftReturns { get; set; }
+
+    }
+}
