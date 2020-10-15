fd297d5799f1593b370098f18a9536cd9ad76cd5
Dulcie <dulcie.jackson@softwire.com>
2020-10-15 12:22:39 +0100

    GPG-387 Adds new model and controller and ManageOrganisations view

---
+++ GenderPayGap.WebUI/Controllers/ManageOrganisations/ManageOrganisationsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.Organisation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.ManageOrganisations
+{
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Route("manage-organisations-new")]
+    public class ManageOrganisationsController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+
+        public ManageOrganisationsController(IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
+
+        [HttpGet]
+        public IActionResult ManageOrganisationsGet()
+        {
+            User user = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
+
+            var viewModel = new ManageOrganisationsViewModel
+            {
+                UserOrganisations = user.UserOrganisations.OrderBy(uo => uo.Organisation.OrganisationName)
+            };
+
+            return View("ManageOrganisations", viewModel);
+
+        }
+
+    }
+}
