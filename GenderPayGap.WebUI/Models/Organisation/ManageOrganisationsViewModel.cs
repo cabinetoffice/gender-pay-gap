c9705b07c053096ec43ab77889b3f54f0e5e6da4
Dulcie <dulcie.jackson@softwire.com>
2020-10-15 12:22:39 +0100

    GPG-387 Adds new model and controller and ManageOrganisations view

---
+++ GenderPayGap.WebUI/Models/Organisation/ManageOrganisationsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using GenderPayGap.Database;
+using GovUkDesignSystem;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Organisation
+{
+    public class ManageOrganisationsViewModel : GovUkViewModel
+
+    {
+
+        [BindNever]
+        public IOrderedEnumerable<UserOrganisation> UserOrganisations { get; set; }
+
+    }
+}
