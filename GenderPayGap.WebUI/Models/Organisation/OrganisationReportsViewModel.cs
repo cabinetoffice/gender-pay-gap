d166dcab08eceded6dc3d18cd49b94afb097b410
Dulcie <dulcie.jackson@softwire.com>
2020-10-16 13:52:19 +0100

    GPG-387 Refactors reports table into separate file

---
+++ GenderPayGap.WebUI/Models/Organisation/OrganisationReportsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.Organisation
+{
+    public class OrganisationReportsViewModel
+    {
+
+        [BindNever]
+        public Database.Organisation Organisation { get; set; }
+        
+        [BindNever]
+        public List<int> YearsWithDraftReturns { get; set; }
+
+    }
+}
