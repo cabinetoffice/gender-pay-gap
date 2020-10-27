60635aa40c07540c2990a3232054fe02b8e8a420
James Griffiths <james.griffiths@softwire.com>
2020-10-27 17:18:37 +0000

    GPG-164 Admin page to re-add retired submissions (will only be used once, then removed)

---
+++ GenderPayGap.WebUI/Models/Admin/AdminReAddRetiredSubmissionsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GovUkDesignSystem;
+using Microsoft.AspNetCore.Http;
+
+namespace GenderPayGap.WebUI.Models.Admin
+{
+    public class AdminReAddRetiredSubmissionsViewModel : GovUkViewModel
+    {
+
+        public IFormFile File { get; set; }
+
+    }
+}
