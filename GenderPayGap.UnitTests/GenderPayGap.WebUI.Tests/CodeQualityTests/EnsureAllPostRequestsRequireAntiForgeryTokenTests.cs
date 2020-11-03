973ca688fa38edb9cef9d50f8df1c6c9530c66da
James Griffiths <james.griffiths@softwire.com>
2020-11-03 12:53:22 +0000

    GPG-546 Add a unit test to check that all POST requests require a CSRF token

---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CodeQualityTests/EnsureAllPostRequestsRequireAntiForgeryTokenTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace GenderPayGap.WebUI.Tests.CodeQualityTests
+{
+    [TestFixture]
+    public class EnsureAllPostRequestsRequireAntiForgeryTokenTests
+    {
+
+        [Test]
+        public void EnsureAllPostRequestsRequireAntiForgeryToken()
+        {
+            Assembly assembly = typeof(GenderPayGap.WebUI.Program).Assembly;
+
+            var methodsMissingAttribute = new List<MethodInfo>();
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(Controller).IsAssignableFrom(type))
+                {
+                    foreach (MethodInfo method in type.GetMethods())
+                    {
+                        if (method.GetCustomAttributes<HttpPostAttribute>().Any())
+                        {
+                            if (!method.GetCustomAttributes<ValidateAntiForgeryTokenAttribute>().Any())
+                            {
+                                methodsMissingAttribute.Add(method);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+
+            if (methodsMissingAttribute.Any())
+            {
+                Assert.Fail(
+                    $"All POST methods must use a ValidateAntiForgeryToken\n"
+                    + $"The following methods are missing the ValidateAntiForgeryToken attribute\n"
+                    + $"- {string.Join("\n- ", methodsMissingAttribute.Select(c => $"Controller [{c.DeclaringType.Name}] Method [{c.Name}]"))}\n");
+            }
+
+        }
+
+    }
+}
