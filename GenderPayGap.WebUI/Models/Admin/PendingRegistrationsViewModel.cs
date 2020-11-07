c342ce21c3c3bb5e64f1fbac6a9324ac04d9ccda
Dulcie <dulcie.jackson@softwire.com>
2020-11-07 10:51:03 +0000

    GPG-425 Updates ReviewRequest to new design system

--- GenderPayGap.WebUI/Models/Admin/PendingRegistrationsViewModel.cs
+++ GenderPayGap.WebUI/Models/Admin/PendingRegistrationsViewModel.cs
@@ -9,12 +9,6 @@ namespace GenderPayGap.WebUI.Models.Admin
         public List<UserOrganisation> PublicSectorUserOrganisations { get; set; }
         public List<UserOrganisation> NonUkAddressUserOrganisations { get; set; }
         public List<UserOrganisation> ManuallyRegisteredUserOrganisations { get; set; }
-        
-        public List<UserOrganisation> NewUsersForExistingOrganisations { get; set; }
-        
-        public List<UserOrganisation> NewCompaniesHouseOrganisations { get; set; }
-        
-        public List<UserOrganisation> NewManuallyRegisteredOrganisations { get; set; }
 
     }
 }
