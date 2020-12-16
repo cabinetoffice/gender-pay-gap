ebc1d1c2b2285bca1aebfdbd061158c3358407cf
Dulcie <dulcie.jackson@softwire.com>
2020-12-16 11:08:04 +0000

    GPG-606 Finishes migration from MySQL to PostgreSQL syntax

--- LoadTests/SET_UP_TEST_ENTITIES.sql
+++ LoadTests/SET_UP_TEST_ENTITIES.sql
@@ -1,91 +1,93 @@
 DO $$
 
-DECLARE
-   STARTING_ID INTEGER := 300000000;
-   NUM_OF_USERS INTEGER := 20000;
-   INDEX INTEGER := 1;
+    DECLARE
+        STARTING_ID INTEGER := 300000000;
+        NUM_OF_USERS INTEGER := 20000;
+        INDEX INTEGER := 1;
 
 -- Remove all test entities
-BEGIN
-    DELETE FROM Users WHERE UserId > STARTING_ID AND UserId <= STARTING_ID + NUM_OF_USERS;
-    DELETE FROM Organisations WHERE OrganisationId > STARTING_ID AND OrganisationId <= STARTING_ID + 2 * NUM_OF_USERS;
-    DELETE FROM UserOrganisations WHERE OrganisationId > STARTING_ID AND OrganisationId <= STARTING_ID + 2 * NUM_OF_USERS;
-    DELETE FROM OrganisationScopes WHERE OrganisationScopeId > STARTING_ID AND OrganisationScopeId <= STARTING_ID + 2* NUM_OF_USERS;
-    DELETE FROM OrganisationNames WHERE OrganisationId > STARTING_ID AND OrganisationId <= STARTING_ID + 2 * NUM_OF_USERS;
-    DELETE FROM OrganisationSicCodes WHERE OrganisationId > STARTING_ID AND OrganisationId <= STARTING_ID + 2 * NUM_OF_USERS;
-    DELETE FROM OrganisationAddresses WHERE AddressId > STARTING_ID AND AddressId <= STARTING_ID + 2 * NUM_OF_USERS;
-    DELETE FROM Returns WHERE OrganisationId > STARTING_ID AND OrganisationId <= STARTING_ID + NUM_OF_USERS;
+    BEGIN
+        DELETE FROM "UserStatus" WHERE "ByUserId" = STARTING_ID + INDEX;
+        DELETE FROM "Users" WHERE "Users"."UserId" > STARTING_ID AND "Users"."UserId" <= STARTING_ID + NUM_OF_USERS;
+        DELETE FROM "Organisations" WHERE "Organisations"."OrganisationId" > STARTING_ID AND "Organisations"."OrganisationId" <= STARTING_ID + 2 * NUM_OF_USERS;
+        DELETE FROM "UserOrganisations" WHERE "UserOrganisations"."OrganisationId" > STARTING_ID AND "UserOrganisations"."OrganisationId" <= STARTING_ID + 2 * NUM_OF_USERS;
+        DELETE FROM "OrganisationScopes" WHERE "OrganisationScopes"."OrganisationScopeId" > STARTING_ID AND "OrganisationScopes"."OrganisationScopeId" <= STARTING_ID + 2* NUM_OF_USERS;
+        DELETE FROM "OrganisationNames" WHERE "OrganisationNames"."OrganisationId" > STARTING_ID AND "OrganisationNames"."OrganisationId" <= STARTING_ID + 2 * NUM_OF_USERS;
+        DELETE FROM "OrganisationSicCodes" WHERE "OrganisationSicCodes"."OrganisationId" > STARTING_ID AND "OrganisationSicCodes"."OrganisationId" <= STARTING_ID + 2 * NUM_OF_USERS;
+        DELETE FROM "OrganisationAddresses" WHERE "OrganisationAddresses"."AddressId" > STARTING_ID AND "OrganisationAddresses"."AddressId" <= STARTING_ID + 2 * NUM_OF_USERS;
+        DELETE FROM "Returns" WHERE "Returns"."OrganisationId" > STARTING_ID AND "Returns"."OrganisationId" <= STARTING_ID + NUM_OF_USERS;
 
-    -- Delete users created using registration journey
-    DELETE FROM Users WHERE Firstname = 'Test' AND Lastname = 'Example' AND JobTitle = 'Tester';
+        -- Delete users created using registration journey
+        DELETE FROM "UserStatus" WHERE "ByUserId" IN (SELECT "UserId" FROM "Users" WHERE "Users"."Firstname" = 'Test' AND "Users"."Lastname" = 'Example' AND "Users"."JobTitle" = 'Tester');
+        DELETE FROM "Users" WHERE "Users"."Firstname" = 'Test' AND "Users"."Lastname" = 'Example' AND "Users"."JobTitle" = 'Tester';
 
-    WHILE INDEX <= NUM_OF_USERS LOOP
-        -- Create test users
-        INSERT INTO Users
-        ("UserId", "FirstName", "LastName", "JobTitle", "StatusId", "StatusDate", "LoginAttempts", "LoginDate", "ResetAttempts", "VerifyAttempts", "Created", "Modified", "EmailAddress", "PasswordHash", "Salt", "HashingAlgorithm", "EmailVerifiedDate")
-        VALUES
-        (STARTING_ID + INDEX, 'test', 'test', 'test', 3, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', 0, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'loadtest'+ CAST(INDEX AS VARCHAR(16)) +'@example.com', 'EsbXTRJaMnDBhGEerRu1eqbMoInXkOz4P5rNZyq1VKU=','9jjDqTsUzGqk/+Rl8JeR4A==', 2, '01/10/2019 12:26:44');
+        WHILE INDEX <= NUM_OF_USERS LOOP
+                -- Create test users
+                INSERT INTO "Users"
+                ("UserId", "Firstname", "Lastname", "JobTitle", "StatusId", "StatusDate", "LoginAttempts", "LoginDate", "ResetAttempts", "VerifyAttempts", "Created", "Modified", "EmailAddress", "PasswordHash", "Salt", "HashingAlgorithm", "EmailVerifiedDate", "SendUpdates", "AllowContact")
+                VALUES
+                (STARTING_ID + INDEX, 'test', 'test', 'test', 3, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', 0, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'loadtest' || CAST(INDEX AS VARCHAR(16)) || '@example.com', 'EsbXTRJaMnDBhGEerRu1eqbMoInXkOz4P5rNZyq1VKU=','9jjDqTsUzGqk/+Rl8JeR4A==', 2, '01/10/2019 12:26:44', false, false);
 
-        -- Create test organisations that are linked to test users
-        INSERT INTO Organisations
-        ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
-        VALUES
-        (STARTING_ID + INDEX, '99999' + CAST(INDEX AS VARCHAR(16)), 'test_' + CAST(INDEX AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', 1, 'ABCDE' + CAST(INDEX AS VARCHAR(16)));
+                -- Create test organisations that are linked to test users
+                INSERT INTO "Organisations"
+                ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
+                VALUES
+                (STARTING_ID + INDEX, '99999' || CAST(INDEX AS VARCHAR(16)), 'test_' || CAST(INDEX AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', true, 'ABCDE' || CAST(INDEX AS VARCHAR(16)));
 
-        INSERT INTO OrganisationNames
-        ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
-        VALUES
-        (STARTING_ID + INDEX, STARTING_ID + INDEX, 'test_' + CAST(INDEX AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
+                INSERT INTO "OrganisationNames"
+                ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
+                VALUES
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 'test_' || CAST(INDEX AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
 
-        INSERT INTO OrganisationScopes
-        ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
-        VALUES
-        (STARTING_ID + INDEX, STARTING_ID + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
+                INSERT INTO "OrganisationScopes"
+                ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
+                VALUES
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
 
-        INSERT INTO OrganisationAddresses
-        ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Modified", "Source", "IsUkAddress")
-        VALUES
-        (STARTING_ID + INDEX, STARTING_ID + INDEX, STARTING_ID + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', '05/04/2017 00:00:00', 'User', 'True');
+                INSERT INTO "OrganisationAddresses"
+                ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Source", "IsUkAddress")
+                VALUES
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, STARTING_ID + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'User', 'True');
 
-        INSERT INTO OrganisationSicCodes
-        ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
-        VALUES
-        (STARTING_ID + INDEX, STARTING_ID + INDEX, 2200, '01/10/2019 12:26:44', 'User');
+                INSERT INTO "OrganisationSicCodes"
+                ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
+                VALUES
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 2200, '01/10/2019 12:26:44', 'User');
 
-        INSERT INTO UserOrganisations
-        ("UserId", "OrganisationId", "ConfirmAttempts", "Created", "Modified", "PINConfirmedDate")
-        VALUES
-        (STARTING_ID + INDEX, STARTING_ID + INDEX, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', '01/10/2019 12:26:44')
+                INSERT INTO "UserOrganisations"
+                ("UserId", "OrganisationId", "ConfirmAttempts", "Created", "Modified", "PINConfirmedDate")
+                VALUES
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', '01/10/2019 12:26:44');
 
-        -- Create test organisations that are NOT linked to test users
-        INSERT INTO Organisations
-        ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
-        VALUES
-        (STARTING_ID + NUM_OF_USERS + INDEX, '99999' + CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'test_' + CAST(INDEX + NUM_OF_USERS  AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', 1, 'ABCDE' + CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)));
+                -- Create test organisations that are NOT linked to test users
+                INSERT INTO "Organisations"
+                ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
+                VALUES
+                (STARTING_ID + NUM_OF_USERS + INDEX, '99999' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'test_' || CAST(INDEX + NUM_OF_USERS  AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', true, 'ABCDE' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)));
 
-        INSERT INTO OrganisationNames
-        ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
-        VALUES
-        (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test_' + CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
+                INSERT INTO "OrganisationNames"
+                ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
+                VALUES
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test_' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
 
-        INSERT INTO OrganisationScopes
-        ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
-        VALUES
-        (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
+                INSERT INTO "OrganisationScopes"
+                ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
+                VALUES
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
 
-        INSERT INTO OrganisationAddresses
-        ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Modified", "Source", "IsUkAddress")
-        VALUES
-        (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', '05/04/2017 00:00:00', 'User', 'True');
+                INSERT INTO "OrganisationAddresses"
+                ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Source", "IsUkAddress")
+                VALUES
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'User', 'True');
 
-        INSERT INTO OrganisationSicCodes
-        ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
-        VALUES
-        (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 2200, '01/10/2019 12:26:44', 'User');
+                INSERT INTO "OrganisationSicCodes"
+                ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
+                VALUES
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 2200, '01/10/2019 12:26:44', 'User');
 
-        SET INDEX = INDEX + 1;
-    END LOOP
-END $$
+                INDEX := INDEX + 1;
+            END LOOP;
 
-select * from "Organisations" where "Organisations"."OrganisationId" >= @STARTING_ID;
-select * from "Users" where "Users"."UserId" >= @STARTING_ID;
+        perform * from "Organisations" where "Organisations"."OrganisationId" >= STARTING_ID;
+        perform * from "Users" where "Users"."UserId" >= STARTING_ID;
+    END $$
