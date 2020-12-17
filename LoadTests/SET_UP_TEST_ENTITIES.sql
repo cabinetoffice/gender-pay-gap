76d494e91182e3a8ea126286be605eb9a007ff0d
Dulcie <dulcie.jackson@softwire.com>
2020-12-17 09:24:12 +0000

    GPG-606 Updates dates and capitalises perform command

--- LoadTests/SET_UP_TEST_ENTITIES.sql
+++ LoadTests/SET_UP_TEST_ENTITIES.sql
@@ -26,68 +26,68 @@ DO $$
                 INSERT INTO "Users"
                 ("UserId", "Firstname", "Lastname", "JobTitle", "StatusId", "StatusDate", "LoginAttempts", "LoginDate", "ResetAttempts", "VerifyAttempts", "Created", "Modified", "EmailAddress", "PasswordHash", "Salt", "HashingAlgorithm", "EmailVerifiedDate", "SendUpdates", "AllowContact")
                 VALUES
-                (STARTING_ID + INDEX, 'test', 'test', 'test', 3, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', 0, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'loadtest' || CAST(INDEX AS VARCHAR(16)) || '@example.com', 'EsbXTRJaMnDBhGEerRu1eqbMoInXkOz4P5rNZyq1VKU=','9jjDqTsUzGqk/+Rl8JeR4A==', 2, '01/10/2019 12:26:44', false, false);
+                (STARTING_ID + INDEX, 'test', 'test', 'test', 3, '01/10/2020 12:26:44', 0, '01/10/2020 12:26:44', 0, 0, '01/10/2020 12:26:44', '01/10/2020 12:26:44', 'loadtest' || CAST(INDEX AS VARCHAR(16)) || '@example.com', 'EsbXTRJaMnDBhGEerRu1eqbMoInXkOz4P5rNZyq1VKU=','9jjDqTsUzGqk/+Rl8JeR4A==', 2, '01/10/2020 12:26:44', false, false);
 
                 -- Create test organisations that are linked to test users
                 INSERT INTO "Organisations"
                 ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
                 VALUES
-                (STARTING_ID + INDEX, '99999' || CAST(INDEX AS VARCHAR(16)), 'test_' || CAST(INDEX AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', true, 'ABCDE' || CAST(INDEX AS VARCHAR(16)));
+                (STARTING_ID + INDEX, '99999' || CAST(INDEX AS VARCHAR(16)), 'test_' || CAST(INDEX AS VARCHAR(16)), 1, 3, '01/10/2020 12:26:44', 'PIN Confirmed', '01/10/2020 12:26:44', '01/10/2020 12:26:44', true, 'ABCDE' || CAST(INDEX AS VARCHAR(16)));
 
                 INSERT INTO "OrganisationNames"
                 ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
                 VALUES
-                (STARTING_ID + INDEX, STARTING_ID + INDEX, 'test_' || CAST(INDEX AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 'test_' || CAST(INDEX AS VARCHAR(16)), 'User', '01/10/2020 12:26:44');
 
                 INSERT INTO "OrganisationScopes"
                 ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
                 VALUES
-                (STARTING_ID + INDEX, STARTING_ID + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 1, '01/10/2020 12:26:44', 0, '01/10/2020 12:26:44', '05/04/2019 00:00:00', 3);
 
                 INSERT INTO "OrganisationAddresses"
                 ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Source", "IsUkAddress")
                 VALUES
-                (STARTING_ID + INDEX, STARTING_ID + INDEX, STARTING_ID + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'User', 'True');
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, STARTING_ID + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2020 12:26:44', '01/10/2020 12:26:44', 'User', 'True');
 
                 INSERT INTO "OrganisationSicCodes"
                 ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
                 VALUES
-                (STARTING_ID + INDEX, STARTING_ID + INDEX, 2200, '01/10/2019 12:26:44', 'User');
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 2200, '01/10/2020 12:26:44', 'User');
 
                 INSERT INTO "UserOrganisations"
                 ("UserId", "OrganisationId", "ConfirmAttempts", "Created", "Modified", "PINConfirmedDate")
                 VALUES
-                (STARTING_ID + INDEX, STARTING_ID + INDEX, 0, '01/10/2019 12:26:44', '01/10/2019 12:26:44', '01/10/2019 12:26:44');
+                (STARTING_ID + INDEX, STARTING_ID + INDEX, 0, '01/10/2020 12:26:44', '01/10/2020 12:26:44', '01/10/2020 12:26:44');
 
                 -- Create test organisations that are NOT linked to test users
                 INSERT INTO "Organisations"
                 ("OrganisationId", "CompanyNumber", "OrganisationName", "SectorTypeId", "StatusId", "StatusDate", "StatusDetails", "Created", "Modified", "OptedOutFromCompaniesHouseUpdate", "EmployerReference")
                 VALUES
-                (STARTING_ID + NUM_OF_USERS + INDEX, '99999' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'test_' || CAST(INDEX + NUM_OF_USERS  AS VARCHAR(16)), 1, 3, '01/10/2019 12:26:44', 'PIN Confirmed', '01/10/2019 12:26:44', '01/10/2019 12:26:44', true, 'ABCDE' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)));
+                (STARTING_ID + NUM_OF_USERS + INDEX, '99999' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'test_' || CAST(INDEX + NUM_OF_USERS  AS VARCHAR(16)), 1, 3, '01/10/2020 12:26:44', 'PIN Confirmed', '01/10/2020 12:26:44', '01/10/2020 12:26:44', true, 'ABCDE' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)));
 
                 INSERT INTO "OrganisationNames"
                 ("OrganisationNameId", "OrganisationId", "Name", "Source", "Created")
                 VALUES
-                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test_' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'User', '01/10/2019 12:26:44');
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test_' || CAST(INDEX + NUM_OF_USERS AS VARCHAR(16)), 'User', '01/10/2020 12:26:44');
 
                 INSERT INTO "OrganisationScopes"
                 ("OrganisationScopeId", "OrganisationId", "ScopeStatusId", "ScopeStatusDate", "RegisterStatusId", "RegisterStatusDate", "SnapshotDate", "StatusId")
                 VALUES
-                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 1, '01/10/2019 12:26:44', 0, '01/10/2019 12:26:44', '05/04/2017 00:00:00', 3);
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 1, '01/10/2020 12:26:44', 0, '01/10/2020 12:26:44', '05/04/2019 00:00:00', 3);
 
                 INSERT INTO "OrganisationAddresses"
                 ("AddressId", "OrganisationId", "CreatedByUserId", "Address1", "Country", "PostCode", "StatusId", "StatusDate", "Created", "Source", "IsUkAddress")
                 VALUES
-                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2019 12:26:44', '01/10/2019 12:26:44', 'User', 'True');
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 'test street', 'United Kingdom', 'NWN NWN', 3, '01/10/2020 12:26:44', '01/10/2020 12:26:44', 'User', 'True');
 
                 INSERT INTO "OrganisationSicCodes"
                 ("OrganisationSicCodeId", "OrganisationId", "SicCodeId", "Created", "Source")
                 VALUES
-                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 2200, '01/10/2019 12:26:44', 'User');
+                (STARTING_ID + NUM_OF_USERS + INDEX, STARTING_ID + NUM_OF_USERS + INDEX, 2200, '01/10/2020 12:26:44', 'User');
 
                 INDEX := INDEX + 1;
             END LOOP;
 
-        perform * from "Organisations" where "Organisations"."OrganisationId" >= STARTING_ID;
-        perform * from "Users" where "Users"."UserId" >= STARTING_ID;
+        PERFORM * from "Organisations" where "Organisations"."OrganisationId" >= STARTING_ID;
+        PERFORM * from "Users" where "Users"."UserId" >= STARTING_ID;
     END $$
