d675ba9a83799bfd3ece116e3d8282fadfd96504
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-01-12 15:09:47 +0000

    GPG-556 Change Redirect to equivalent new RedirectResult

--- GenderPayGap.WebUI/Controllers/SubmitController.DraftComplete.cs
+++ GenderPayGap.WebUI/Controllers/SubmitController.DraftComplete.cs
@@ -47,7 +47,7 @@ namespace GenderPayGap.WebUI.Controllers.Submission
         {
             string doneUrl = Global.DoneUrl ?? Url.Action("Index", "Viewing", null, "https");
 
-            return Redirect(doneUrl);
+            return new RedirectResult(doneUrl);
         }
 
         #endregion
