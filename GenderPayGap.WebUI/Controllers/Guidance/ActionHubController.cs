10808ba1e9c3a7f6951ccb3f5fb9bf10ad8110b0
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-01-22 16:26:34 +0000

    GPG-629 Removed the unused ActionHubController.New and views

---
+++ GenderPayGap.WebUI/Controllers/Guidance/ActionHubController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Guidance
+{
+    [Route("actions-to-close-the-gap")]
+    public class ActionHubController : Controller
+    {
+        // GET: ActionHub
+        public IActionResult Overview()
+        {
+            return View("Pages/Overview");
+        }
+
+        [HttpGet("effective-actions")]
+        public IActionResult Effective()
+        {
+            return View("Pages/Effective");
+        }
+
+        [HttpGet("promising-actions")]
+        public IActionResult Promising()
+        {
+            return View("Pages/Promising");
+        }
+
+        [HttpGet("actions-with-mixed-results")]
+        public IActionResult MixedResult()
+        {
+            return View("Pages/MixedResult");
+        }
+    }
+}
