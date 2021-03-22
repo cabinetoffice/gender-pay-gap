5eafc69911c1362c4499a87fde380f9348a662c9
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-03-22 14:30:07 +0000

    GPG-651 Refactor manage employer breadcrumbs into partial

---
+++ GenderPayGap.WebUI/Models/Shared/Patterns/ManageOrganisationBreadcrumbs.cs
@@ -0,0 +1,11 @@
+﻿namespace GenderPayGap.WebUI.Models.Shared.Patterns
+{
+    public class ManageOrganisationBreadcrumbs
+    {
+        public string OrganisationName { get; set; }
+
+        public string EncryptedOrganisationId { get; set; }
+
+        public string PageText { get; set; }
+    }
+}
