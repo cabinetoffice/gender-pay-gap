04175d2f5c8bb0336b34f336f04d0ddb87c421be
Malina Slevoaca <Malina.Slevoaca@softwire.com>
2021-03-30 19:27:14 +0300

    GPG-735 Added database integrity checks for the submission form data

---
+++ GenderPayGap.WebUI/Helpers/IntegrationChecksHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenderPayGap.WebUI.Helpers
+{
+    public static class IntegrationChecksHelper
+    {
+        public static Boolean SumNotEqualTo100(decimal a, decimal b)
+        {
+            return a + b != 100;
+        }
+
+        public static Boolean FigureGreaterThan100OrLessThan(decimal? a, decimal minValue)
+        {
+            return a > 100 || a < minValue;
+        }
+
+        
+        public static Boolean HasMoreThanOneDecimalPlace(decimal? number)
+        {
+            if (!number.HasValue)
+            {
+                return false;
+            }
+
+            return Decimal.Round(number.Value, 1) != number.Value;
+        }
+    }
+}
