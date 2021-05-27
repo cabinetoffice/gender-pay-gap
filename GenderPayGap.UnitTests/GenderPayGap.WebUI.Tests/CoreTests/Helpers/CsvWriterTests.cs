9b1e36d2220837d31aa8200589185aad8118b58b
Malina Slevoaca <Malina.Slevoaca@softwire.com>
2021-05-27 10:55:41 +0300

    GPG-752 Added tests

---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CoreTests/Helpers/CsvWriterTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using GenderPayGap.Core.Helpers;
+using NUnit.Framework;
+
+namespace GenderPayGap.Core.Tests.Helpers
+{
+    [TestFixture]
+    public class CsvWriterTests
+    {
+
+        [TestCase('-')]
+        [TestCase('+')]
+        [TestCase('@')]
+        [TestCase('=')]
+        public void CsvWriter_Sanitizes_Strings_That_Start_With(char character)
+        {
+            // Arrange
+            var value = character + "test";
+            var expectedCsvRow = "\t" + character + "test\r\n";
+
+            // Act
+            var actualCsvRow = CsvWriter.Write(WriteValue(value));
+
+            // Assert
+            Assert.AreEqual(expectedCsvRow, actualCsvRow);
+        }
+
+        [Test]
+        public void CsvWriter_Does_Not_Sanitize_Negative_Values()
+        {
+            // Arrange
+            var value = -10.2;
+            var expectedCsvRow = "-10.2\r\n";
+
+            // Act
+            var actualCsvRow = CsvWriter.Write(WriteValue(value));
+
+            // Assert
+            Assert.AreEqual(expectedCsvRow, actualCsvRow);
+        }
+
+        [Test]
+        public void CsvWriter_Does_Not_Sanitize_Strings_That_Do_Not_Start_With_Injection_Character()
+        {
+            // Arrange
+            var value = "Test - string that doesn't start with an injection character";
+            var expectedCsvRow = "Test - string that doesn't start with an injection character\r\n";
+
+            // Act
+            var actualCsvRow = CsvWriter.Write(WriteValue(value));
+
+            // Assert
+            Assert.AreEqual(expectedCsvRow, actualCsvRow);
+        }
+
+        private Func<MemoryStream, StreamReader, StreamWriter, CsvHelper.CsvWriter, string> WriteValue<T>(T value)
+        {
+            return (memoryStream, streamReader, streamWriter, csvWriter) =>
+            {
+                csvWriter.WriteField(value);
+                csvWriter.NextRecord();
+                streamWriter.Flush();
+                memoryStream.Position = 0;
+                return streamReader.ReadToEnd();
+            };
+        }
+
+    }
+}
