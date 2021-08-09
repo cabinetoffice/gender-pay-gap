02786b4417db8087db254b8947a9a438ef49a2c8
Malina Slevoaca <Malina.Slevoaca@softwire.com>
2021-08-09 09:30:26 +0300

    GPG-792 Added tests

---
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CoreTests/Helpers/ReportingYearsHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GenderPayGap.Core.Classes;
+using GenderPayGap.Core.Helpers;
+using NUnit.Framework;
+using GenderPayGap.Extensions;
+
+namespace GenderPayGap.Core.Tests.Helpers
+{
+    [TestFixture]
+    public class ReportingYearsHelperTests
+    {
+
+        private static List<int> reportingStartYearsWithFurloughScheme = Global.ReportingStartYearsWithFurloughScheme;
+
+        private static List<int> reportingStartYearsWithoutFurloughScheme =
+            ReportingYearsHelper.GetReportingYears()
+                .Except(reportingStartYearsWithFurloughScheme)
+                .ToList();
+
+        [TestCaseSource(nameof(reportingStartYearsWithFurloughScheme))]
+        public void IsReportingYearWithFurloughScheme_Returns_True_Given_A_Year_With_Furlough_Scheme(int year)
+        {
+            // Arrange
+            var accountingDate = SectorTypes.Private.GetAccountingStartDate(year);
+
+            // Act
+            var actualResult = ReportingYearsHelper.IsReportingYearWithFurloughScheme(accountingDate);
+
+            // Assert
+            Assert.AreEqual(true, actualResult);
+        }
+
+        [TestCaseSource(nameof(reportingStartYearsWithoutFurloughScheme))]
+        public void IsReportingYearWithFurloughScheme_Returns_False_Given_A_Year_Without_Furlough_Scheme(int year)
+        {
+            // Arrange
+            var accountingDate = SectorTypes.Private.GetAccountingStartDate(year);
+
+            // Act
+            var actualResult = ReportingYearsHelper.IsReportingYearWithFurloughScheme(accountingDate);
+
+            // Assert
+            Assert.AreEqual(false, actualResult);
+        }
+
+    }
+}
