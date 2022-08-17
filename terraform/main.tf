d2a49b8ea5740d51d716021b0aa3b88d24dc53bc
CodCot <codrin.cotarlan@softwire.com>
2022-08-17 16:45:14 +0100

    GPG-868: Replace postgre module with resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,29 +14,15 @@ module "example" {
 
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = ["subnet-0b9845b5d6e0541be", "subnet-04e8deb14870ac711"]
-
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-module "postgresql-rds" {
-  source  = "registry.terraform.io/azavea/postgresql-rds/aws"
-  version = "3.0.0"
-  engine_version = "14.2"
-  # insert the 9 required variables here
-  alarm_actions = []
-  database_identifier = "uniqueidforthisparticulardatabase"
-  database_name = "gpgdevdb"
-  database_password = "local_gpg_database"
-  database_username = "gpg_user"
-  database_port = 5432
-  insufficient_data_actions = []
-  ok_actions = []
-  subnet_group = aws_db_subnet_group.default.name
-  vpc_id = "vpc-0e9491851e4208470"
-  parameter_group = "testparamgroup"
+resource "aws_db_instance" "gpg-dev-db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "14"
+  identifier           =  "gpg-dev-db"
+  instance_class       = "db.t3.medium"
+  name                 = "gpgdevdb"
+  username             = "testuser"
+  password             = "testpassword"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
 }
