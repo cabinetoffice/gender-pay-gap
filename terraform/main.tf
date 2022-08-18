322dedb8f2b2989932fc63017abd5146f43f1b0b
CodCot <codrin.cotarlan@softwire.com>
2022-08-18 16:50:37 +0100

    GPG-868: Add publicly accessible flag to postgres

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,18 @@
 // this does not include the state S3 bucket as this must be created in order for terraform to init
 
 resource "aws_db_instance" "gpg-dev-db" {
-  allocated_storage       = 100
-  engine                  = "postgres"
-  engine_version          = "11"
-  instance_class          = "db.t3.small"
-  identifier              = "gpg-dev-db"
-  db_name                 = "gpg-dev-db"
-  username                = "postgres"
-  password                = "postgres"
-  port                    = 5432
-  backup_retention_period = 5
-  backup_window           = "04:00-05:00"
-  storage_encrypted       = true
+  allocated_storage           = 100
+  engine                      = "postgres"
+  engine_version              = "14"
+  instance_class              = "db.t3.small"
+  identifier                  = "gpg-dev-db"
+  db_name                     = "gpgDevDb"
+  username                    = "postgres"
+  password                    = "postgres"
+  port                        = 5432
+  backup_retention_period     = 5
+  backup_window               = "04:00-05:00"
+  storage_encrypted           = true
+  publicly_accessible         = true
+  allow_major_version_upgrade = true
 }
