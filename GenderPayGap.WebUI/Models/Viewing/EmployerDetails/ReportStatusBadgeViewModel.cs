dcff1c0c8d6104e675b57e9a93c4348b688bc982
Aaron Spencer <aaronspencer@softwire.com>
2022-09-21 13:49:23 +0100

    GPG-905: WIP: desktop table.

--- GenderPayGap.WebUI/Models/Viewing/EmployerDetails/ReportStatusBadgeViewModel.cs
+++ GenderPayGap.WebUI/Models/Viewing/EmployerDetails/ReportStatusBadgeViewModel.cs
@@ -4,8 +4,8 @@
     public class ReportStatusBadgeViewModel
     {
         public ReportStatusBadgeType ReportStatus { get; set; }
-
         public string DateText { get; set; }
+        public bool Desktop { get; set; }
 
     }
 
