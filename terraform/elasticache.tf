b8b839b175ebf73f99f3279c469808d4e5255141
CodCot <codrin.cotarlan@softwire.com>
2022-10-11 14:11:14 +0100

    fix missing id

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "redis-cluster" {
   num_cache_nodes      = 1
   parameter_group_name = "default.redis6.x"
   port                 = var.cache_port
-  security_group_ids = [aws_security_group.elasticache_security_group]
+  security_group_ids = [aws_security_group.elasticache_security_group.id]
   subnet_group_name    = module.vpc.elasticache_subnet_group_name
   
   log_delivery_configuration {
