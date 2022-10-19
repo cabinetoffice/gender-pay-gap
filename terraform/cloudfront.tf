7961f7455a20fd09f1e3bd0758b51eb4bc35d852
Ladun Omideyi <ladun.omideyi@softwire.com>
2022-10-19 22:05:56 +0100

    GPG-926: add custom origin block

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,9 +1,15 @@
-﻿resource "aws_cloudfront_distribution" "gpg-load-balancer-origin" {
+﻿resource "aws_cloudfront_distribution" "gpg-distribution" {
   provider            = aws.us-east-1   
   
   origin {
-    domain_name = aws_elastic_beanstalk_environment.gpg-elb-environment.load_balancers[0]
+    domain_name = aws_elastic_beanstalk_environment.gpg-elb-environment.load_balancers
     origin_id   = var.cloudfront_origin_id
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
     
     origin_shield {
       enabled              = true
@@ -34,29 +40,6 @@
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/images/*.png"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = var.cloudfront_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
   }
 
   price_class = "PriceClass_200"
