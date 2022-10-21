b1cee0c51712821ff8ad08d69e674f41de104ab6
Ladun Omideyi <ladun.omideyi@softwire.com>
2022-10-21 15:58:15 +0100

    GPG-926: change origin policy protocol to https only

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -8,7 +8,7 @@ resource "aws_cloudfront_distribution" "gpg-distribution" {
     custom_origin_config {
       http_port              = "80"
       https_port             = "443"
-      origin_protocol_policy = "match-viewer"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
 
