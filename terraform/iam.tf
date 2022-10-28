6573c71bf12faa584388ac6402b84b3a8f6193f7
Ladun Omideyi <ladun.omideyi@softwire.com>
2022-10-28 11:44:17 +0100

    GPG-NONE: add new policies

---
+++ terraform/iam.tf
@@ -0,0 +1,16 @@
+﻿resource "aws_iam_role" "aws-elasticbeanstalk-ec2-role" {
+  name                = "aws-elasticbeanstalk-ec2-role-${var.env}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonRDSFullAccess", "arn:aws:iam::aws:policy/AmazonElastiCacheFullAccess", "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier","arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker","arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy", "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier", "arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy"]
+}
