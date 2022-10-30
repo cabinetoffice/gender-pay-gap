01c1c1727946a8f64c01cb8ca6529077993d9603
Ladun Omideyi <ladun.omideyi@softwire.com>
2022-10-30 09:30:55 +0000

    GPG-926: Add instance role

--- terraform/elasticbeanstalk.tf
+++ terraform/elasticbeanstalk.tf
@@ -11,6 +11,15 @@ locals {
   elb_health_check_path        = "/health-check"
   elb_matcher_http_code        = 200
 }
+// Instance profile 
+resource "aws_iam_instance_profile" "elastic_beanstalk" {
+  name = "${local.env_prefix}-elastic-beanstalk"
+  role = data.aws_iam_role.elastic-beanstalk_instance_profile
+}
+
+data "aws_iam_role" "elastic-beanstalk_instance_profile" {
+  name = "AWSServiceRoleForElasticBeanstalk"
+}
 
 // Load balancer id
 data "aws_instance" "elb_primary_instance" {
