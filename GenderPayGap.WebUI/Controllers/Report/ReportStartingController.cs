2b0f50cce63fcafde9c849ba5f209cefc51daa1e
Aaron Spencer <aaronspencer@softwire.com>
2022-11-14 17:19:56 +0000

    GPG-918:  WIP: content added, controller added etc

---
+++ GenderPayGap.WebUI/Controllers/Report/ReportStartingController.cs
@@ -0,0 +1,66 @@
+﻿using GenderPayGap.Core;
+using GenderPayGap.Core.Classes;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.Database.Models;
+using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.Report;
+using GenderPayGap.WebUI.Models.ReportStarting;
+using GenderPayGap.WebUI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Report
+{
+    [Authorize(Roles = LoginRoles.GpgEmployer)]
+    [Route("account/organisations")]
+    public class ReportStartingController: Controller
+    {
+        
+        private readonly IDataRepository dataRepository;
+        private readonly DraftReturnService draftReturnService;
+
+        public ReportStartingController(
+            IDataRepository dataRepository,
+            DraftReturnService draftReturnService)
+        {
+            this.dataRepository = dataRepository;
+            this.draftReturnService = draftReturnService;
+        }
+        
+        #region GET
+        
+        [HttpGet("{encryptedOrganisationId}/reporting-year-{reportingYear}/report/start")]
+        public IActionResult ReportFiguresGet(string encryptedOrganisationId, int reportingYear)
+        {
+             long organisationId = ControllerHelper.DecryptOrganisationIdOrThrow404(encryptedOrganisationId);
+             
+             ControllerHelper.ThrowIfUserAccountRetiredOrEmailNotVerified(User, dataRepository);
+             ControllerHelper.ThrowIfUserDoesNotHavePermissionsForGivenOrganisation(User, dataRepository, organisationId);
+             ControllerHelper.ThrowIfReportingYearIsOutsideOfRange(reportingYear);
+
+             var viewModel = new ReportStartingViewModel();
+
+             DraftReturn draftReturn = draftReturnService.GetDraftReturn(organisationId, reportingYear);
+             PopulateViewModel(viewModel, organisationId, reportingYear, draftReturn != null);
+
+             //Return submittedReturn = viewModel.Organisation.GetReturn(reportingYear);
+             //ReportFiguresHelper.SetFigures(viewModel, draftReturn, submittedReturn);
+
+             return View("ReportStarting", viewModel);
+        }
+
+        private void PopulateViewModel(ReportStartingViewModel viewModel, long organisationId, int reportingYear, bool hasDraftReturn)
+         {
+             Organisation organisation = dataRepository.Get<Organisation>(organisationId);
+
+             viewModel.Organisation = organisation;
+             viewModel.ReportingYear = reportingYear;
+             viewModel.IsEditingSubmittedReturn = organisation.HasSubmittedReturn(reportingYear);
+             viewModel.IsEditingForTheFirstTime = !viewModel.IsEditingSubmittedReturn && !hasDraftReturn;
+             viewModel.SnapshotDate = organisation.SectorType.GetAccountingStartDate(reportingYear);
+         }
+
+        #endregion
+    }
+}
---
+++ GenderPayGap.WebUI/Models/ReportStarting/ReportStartingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GenderPayGap.WebUI.Models.ReportStarting
+{
+    public class ReportStartingViewModel
+    {
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public Database.Organisation Organisation { get; set; }
+        [BindNever /* Output Only - only used for sending data from the Controller to the View */]
+        public int ReportingYear { get; set; }
+        
+        public bool IsEditingSubmittedReturn { get; set; }
+        public bool IsEditingForTheFirstTime { get; set; }
+        
+        public DateTime SnapshotDate { get; set; }
+
+    }
+}
