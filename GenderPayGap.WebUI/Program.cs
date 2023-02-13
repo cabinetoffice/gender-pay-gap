696541c9f04e77168290b154dcee979971f1d161
Alistair Knight <alistair@nts-graphics.co.uk>
2023-02-13 13:40:19 +0000

    Add Sentry.io

--- GenderPayGap.WebUI/Program.cs
+++ GenderPayGap.WebUI/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using Sentry;
 
 namespace GenderPayGap.WebUI
 {
@@ -18,26 +19,43 @@ namespace GenderPayGap.WebUI
 
         public static void Main(string[] args)
         {
-            Console.Title = "GenderPayGap.WebUI";
 
-            //Add a handler for unhandled exceptions
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            //Culture is required so UK dates can be parsed correctly
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
-            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+            // Add sentry logging
+            using (SentrySdk.Init(o =>
+                {
+                    o.Dsn = "https://7aa7b734d6b24c549432c396a5c42465@o4504651047501824.ingest.sentry.io/4504672991641600";
+                    // When configuring for the first time, to see what the SDK is doing:
+                    o.Debug = true;
+                    // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+                    // We recommend adjusting this value in production.
+                    o.TracesSampleRate = 1.0;
+                    // Enable Global Mode if running in a client app
+                    o.IsGlobalModeEnabled = true;
+                }))
+            {
+                // App code goes here. Dispose the SDK before exiting to flush events.
+                Console.Title = "GenderPayGap.WebUI";
 
-            //Create the web host
-            IWebHost host = BuildWebHost(args);
+                // Add a handler for unhandled exceptions
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            //Set the minumum threads 
-            Console.WriteLine(Extensions.AspNetCore.Extensions.SetThreadCount());
+                // Culture is required so UK dates can be parsed correctly
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
+                Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
 
-            //Show thread availability
-            Console.WriteLine(Extensions.AspNetCore.Extensions.GetThreadCount());
+                // Create the web host
+                IWebHost host = BuildWebHost(args);
 
-            //Run the webhost
-            host.Run();
+                // Set the minumum threads 
+                Console.WriteLine(Extensions.AspNetCore.Extensions.SetThreadCount());
+
+                // Show thread availability
+                Console.WriteLine(Extensions.AspNetCore.Extensions.GetThreadCount());
+
+                // Run the webhost
+                host.Run();
+            }
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
