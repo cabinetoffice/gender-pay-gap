05f92fa33ea6f84fdaf337aa175ae1b232010e6e
James Griffiths <james@jwgsoftware.com>
2023-03-27 17:10:44 +0100

    Viewing Controller: Simplify code to remove use of ReturnViewModel

--- GenderPayGap.WebUI/Controllers/ViewingController.cs
+++ GenderPayGap.WebUI/Controllers/ViewingController.cs
@@ -8,7 +8,6 @@ using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Core.Models.HttpResultModels;
 using GenderPayGap.Database;
 using GenderPayGap.Extensions;
-using GenderPayGap.WebUI.BusinessLogic.Models.Submit;
 using GenderPayGap.WebUI.BusinessLogic.Services;
 using GenderPayGap.WebUI.Classes.Presentation;
 using GenderPayGap.WebUI.ErrorHandling;
@@ -176,8 +175,6 @@ namespace GenderPayGap.WebUI.Controllers
 
             #region Load latest submission 
 
-            ReturnViewModel model;
-
             try
             {
                 Return foundReturn = foundOrganisation.GetReturn(year);
@@ -187,7 +184,9 @@ namespace GenderPayGap.WebUI.Controllers
                     throw new PageNotFoundException();
                 }
                 
-                model = ConvertSubmissionReportToReturnViewModel(foundReturn);
+                ViewBag.BasketViewModel = new CompareBasketViewModel {CanAddEmployers = true, CanViewCompare = true};
+
+                return View("EmployerDetails/Report", foundReturn);
             }
             catch (Exception ex)
             {
@@ -195,65 +194,6 @@ namespace GenderPayGap.WebUI.Controllers
             }
 
             #endregion
-
-            ViewBag.BasketViewModel = new CompareBasketViewModel {CanAddEmployers = true, CanViewCompare = true};
-
-            return View("EmployerDetails/Report", model);
-        }
-
-        private ReturnViewModel ConvertSubmissionReportToReturnViewModel(Return reportToConvert)
-        {
-            var model = new ReturnViewModel {
-                SectorType = reportToConvert.Organisation.SectorType,
-                ReturnId = reportToConvert.ReturnId,
-                OrganisationId = reportToConvert.OrganisationId,
-                EncryptedOrganisationId = reportToConvert.Organisation.GetEncryptedId(),
-                DiffMeanBonusPercent = reportToConvert.DiffMeanBonusPercent,
-                DiffMeanHourlyPayPercent = reportToConvert.DiffMeanHourlyPayPercent,
-                DiffMedianBonusPercent = reportToConvert.DiffMedianBonusPercent,
-                DiffMedianHourlyPercent = reportToConvert.DiffMedianHourlyPercent,
-                FemaleLowerPayBand = reportToConvert.FemaleLowerPayBand,
-                FemaleMedianBonusPayPercent = reportToConvert.FemaleMedianBonusPayPercent,
-                FemaleMiddlePayBand = reportToConvert.FemaleMiddlePayBand,
-                FemaleUpperPayBand = reportToConvert.FemaleUpperPayBand,
-                FemaleUpperQuartilePayBand = reportToConvert.FemaleUpperQuartilePayBand,
-                MaleLowerPayBand = reportToConvert.MaleLowerPayBand,
-                MaleMedianBonusPayPercent = reportToConvert.MaleMedianBonusPayPercent,
-                MaleMiddlePayBand = reportToConvert.MaleMiddlePayBand,
-                MaleUpperPayBand = reportToConvert.MaleUpperPayBand,
-                MaleUpperQuartilePayBand = reportToConvert.MaleUpperQuartilePayBand,
-                JobTitle = reportToConvert.JobTitle,
-                FirstName = reportToConvert.FirstName,
-                LastName = reportToConvert.LastName,
-                CompanyLinkToGPGInfo = reportToConvert.CompanyLinkToGPGInfo,
-                AccountingDate = reportToConvert.AccountingDate,
-                Address = reportToConvert.Organisation.GetLatestAddress()?.GetAddressString(),
-                LatestAddress = reportToConvert.Organisation.GetLatestAddress()?.GetAddressString(),
-                EHRCResponse = reportToConvert.EHRCResponse.ToString(),
-                IsVoluntarySubmission = reportToConvert.IsVoluntarySubmission(),
-                IsLateSubmission = reportToConvert.IsLateSubmission,
-                OptedOutOfReportingPayQuarters = reportToConvert.OptedOutOfReportingPayQuarters
-            };
-
-            if (model.Address.EqualsI(model.LatestAddress))
-            {
-                model.LatestAddress = null;
-            }
-
-            model.OrganisationName = reportToConvert.Organisation.GetName(reportToConvert.StatusDate)?.Name
-                                     ?? reportToConvert.Organisation.OrganisationName;
-            model.LatestOrganisationName = reportToConvert.Organisation.OrganisationName;
-
-            model.Sector = reportToConvert.Organisation.GetSicSectorsString(reportToConvert.StatusDate);
-            model.LatestSector = reportToConvert.Organisation.GetSicSectorsString();
-
-            model.OrganisationSize = reportToConvert.OrganisationSize;
-            model.Modified = reportToConvert.Modified;
-
-            model.IsInScopeForThisReportYear =
-                reportToConvert.Organisation.GetIsInscope(reportToConvert.AccountingDate.Year);
-
-            return model;
         }
 
         [HttpGet("add-search-results-to-compare")]
