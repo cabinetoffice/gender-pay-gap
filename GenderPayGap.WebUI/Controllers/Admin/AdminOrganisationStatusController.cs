96ba093cd9adeaaa93d936cfd86efbb4c0fd83a1
James Griffiths <james@jwgsoftware.com>
2023-04-27 17:31:35 +0100

    4410: Retired/InScope orgs: Allow admins to mark years as Out Of Scope whilst retiring an organisation

--- GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationStatusController.cs
+++ GenderPayGap.WebUI/Controllers/Admin/AdminOrganisationStatusController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using GenderPayGap.Core;
+using GenderPayGap.Core.Classes;
+using GenderPayGap.Core.Helpers;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
 using GenderPayGap.WebUI.Classes;
@@ -44,12 +46,12 @@ namespace GenderPayGap.WebUI.Controllers
             var viewModel = new AdminChangeOrganisationStatusViewModel();
 
             UpdateAdminChangeStatusViewModelFromOrganisation(viewModel, id);
+            SetInitialValueOfNewScopes(viewModel);
 
             return View("ChangeStatus", viewModel);
         }
 
         [HttpPost("organisation/{id}/status/change")]
-        [PreventDuplicatePost]
         [ValidateAntiForgeryToken]
         public IActionResult ChangeStatusPost(long id, AdminChangeOrganisationStatusViewModel viewModel)
         {
@@ -74,10 +76,75 @@ namespace GenderPayGap.WebUI.Controllers
 
         private void UpdateAdminChangeStatusViewModelFromOrganisation(AdminChangeOrganisationStatusViewModel viewModel, long organisationId)
         {
-            viewModel.Organisation = dataRepository.Get<Organisation>(organisationId);
+            Organisation organisation = dataRepository.Get<Organisation>(organisationId);
+            
+            viewModel.Organisation = organisation;
 
             viewModel.InactiveUserOrganisations = dataRepository.GetAll<InactiveUserOrganisation>()
                 .Where(m => m.OrganisationId == organisationId).ToList();
+
+            if (organisation.Status == OrganisationStatuses.New ||
+                organisation.Status == OrganisationStatuses.Pending ||
+                organisation.Status == OrganisationStatuses.Active)
+            {
+                foreach (int reportingYear in ReportingYearsHelper.GetReportingYears())
+                {
+                    if (!viewModel.Years.Any(y => y.ReportingYear == reportingYear))
+                    {
+                        viewModel.Years.Add(new AdminChangeOrganisationStatusReportingYearViewModel
+                        {
+                            ReportingYear = reportingYear
+                        });
+                    }
+                }
+
+                foreach (AdminChangeOrganisationStatusReportingYearViewModel yearViewModel in viewModel.Years)
+                {
+                    yearViewModel.CurrentScope = organisation.GetScopeStatusForYear(yearViewModel.ReportingYear);
+                    yearViewModel.HasReported = organisation.HasSubmittedReturn(yearViewModel.ReportingYear);
+                }
+
+                viewModel.Years = viewModel.Years.OrderByDescending(y => y.ReportingYear).ToList();
+            }
+        }
+
+        private void SetInitialValueOfNewScopes(AdminChangeOrganisationStatusViewModel viewModel)
+        {
+            // The admin user is Retiring / Deleting this organisation.
+            // This is usually because the organisation has stopped trading.
+            // When an organisation stops trading, the usually DON'T login to the GPG service and mark themselves as Out Of Scope! :-o
+            // So, the admin team will find this a few months later and mark the organisation as Retired.
+            // 
+            // Often, when the admin team do this, they will also want to mark some years as Out Of Scope (typically the previous 1 or 2 years).
+            // To help them, we will guess which years they will want to change and pre-tick the checkboxes next to those years.
+            // But which years should we pre-tick?
+            // 
+            // Here is our best guess at which years they will want to mark as Our Of Scope:
+            // - Work backwards through the years, starting with the current reporting year
+            // - If, for a reporting year, the organisation has NOT reported and is IN Scope, we think the admin team will probably want to mark them as Out Of Scope
+            // - As soon as we find a year where the organisation HAS reported, stop
+            // - As soon as we find a year where the organisation is OUT Of Scope, stop
+            // - In both these cases, we assume the organisation has taken this action, so this was before they stopped trading
+            
+            // Work backwards through the years
+            foreach (AdminChangeOrganisationStatusReportingYearViewModel yearViewModel in viewModel.Years.OrderByDescending(y => y.ReportingYear))
+            {
+                // If the organisation has reported for that year, stop
+                if (yearViewModel.HasReported)
+                {
+                    return;
+                }
+
+                // If the organisation is IN Scope for that year, stop
+                if (!yearViewModel.CurrentScope.IsInScopeVariant())
+                {
+                    return;
+                }
+
+                // Otherwise, pre-tick the "Change to Out Of Scope" checkbox
+                yearViewModel.MarkAsOutOfScope = true;
+                viewModel.AnyGuessedScopeChanges = true;
+            }
         }
 
         private void ChangeStatus(AdminChangeOrganisationStatusViewModel viewModel, long organisationId)
@@ -93,6 +160,15 @@ namespace GenderPayGap.WebUI.Controllers
                 newStatus,
                 currentUser.UserId,
                 viewModel.Reason);
+            
+            // Change scopes
+            foreach (AdminChangeOrganisationStatusReportingYearViewModel yearViewModel in viewModel.Years)
+            {
+                if (yearViewModel.MarkAsOutOfScope)
+                {
+                    organisation.SetScopeForYear(yearViewModel.ReportingYear, ScopeStatuses.OutOfScope, viewModel.Reason);
+                }
+            }
 
             // [In]Activate users of the organisation
             if (newStatus == OrganisationStatuses.Active)
