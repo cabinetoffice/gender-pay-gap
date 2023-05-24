44ff2a44ec110831c1b9a1b21a43f691bb1fbe23
James Griffiths <james@jwgsoftware.com>
2023-05-24 12:04:39 +0100

    4415: Remove unused code: ReturnStatuses: Database migration

---
+++ GenderPayGap.Database/Migrations/20230524110409_Remove ReturnStatus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace GenderPayGap.Database.Migrations
+{
+    public partial class RemoveReturnStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReturnStatus");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReturnStatus",
+                columns: table => new
+                {
+                    ReturnStatusId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ByUserId = table.Column<long>(type: "bigint", nullable: false),
+                    ReturnId = table.Column<long>(type: "bigint", nullable: false),
+                    StatusId = table.Column<byte>(type: "smallint", nullable: false),
+                    StatusDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    StatusDetails = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dbo.ReturnStatus", x => x.ReturnStatusId);
+                    table.ForeignKey(
+                        name: "FK_ReturnStatus_Users_ByUserId",
+                        column: x => x.ByUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_dbo.ReturnStatus_dbo.Returns_ReturnId",
+                        column: x => x.ReturnId,
+                        principalTable: "Returns",
+                        principalColumn: "ReturnId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReturnStatus_ByUserId",
+                table: "ReturnStatus",
+                column: "ByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReturnStatus_ReturnId",
+                table: "ReturnStatus",
+                column: "ReturnId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReturnStatus_StatusDate",
+                table: "ReturnStatus",
+                column: "StatusDate");
+        }
+    }
+}
