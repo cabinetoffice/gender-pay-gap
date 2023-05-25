38900815283901cb78704ba5f085eb19f3b128a2
James Griffiths <james@jwgsoftware.com>
2023-05-25 14:47:09 +0100

    4411: Manage admin users: Add "data migration" admin page to populate User.UserRole for admin users

---
+++ GenderPayGap.WebUI/Controllers/Admin/AdminManageAdminUsersController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GenderPayGap.Core;
+using GenderPayGap.Core.Interfaces;
+using GenderPayGap.Database;
+using GenderPayGap.Extensions;
+using GenderPayGap.WebUI.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers.Admin
+{
+    [Authorize(Roles = LoginRoles.GpgAdmin)]
+    [Route("admin/manage-admin-users")]
+    public class AdminManageAdminUsersController : Controller
+    {
+
+        private readonly IDataRepository dataRepository;
+
+        public AdminManageAdminUsersController(IDataRepository dataRepository)
+        {
+            this.dataRepository = dataRepository;
+        }
+
+
+        [HttpGet("migrate-admin-users")]
+        public IActionResult MigrateAdminUsers()
+        {
+            List<User> adminUsers = dataRepository.GetAll<User>()
+                .AsEnumerable()
+                .Where(u => u.EmailAddress.IsEmailAddress())
+                .Where(u => u.IsAdministrator())
+                .ToList();
+
+            foreach (User adminUser in adminUsers)
+            {
+                adminUser.UserRole = UserRole.Admin;
+            }
+            
+            dataRepository.SaveChanges();
+
+            List<string> adminEmails = adminUsers.Select(u => u.EmailAddress).ToList();
+
+            return Json(adminEmails);
+        }
+
+    }
+}
