2b1c3616889edd83515624e36324d86ef016b41a
James Griffiths <james@jwgsoftware.com>
2023-05-31 15:20:16 +0100

    4411: Manage admin users: Retire admin users

--- GenderPayGap.WebUI/Controllers/Admin/AdminManageAdminUsersController.cs
+++ GenderPayGap.WebUI/Controllers/Admin/AdminManageAdminUsersController.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using GenderPayGap.Core;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
+using GenderPayGap.Extensions;
+using GenderPayGap.WebUI.ErrorHandling;
 using GenderPayGap.WebUI.Helpers;
+using GenderPayGap.WebUI.Models.Admin;
+using GenderPayGap.WebUI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,10 +19,12 @@ namespace GenderPayGap.WebUI.Controllers.Admin
     {
 
         private readonly IDataRepository dataRepository;
+        private readonly AuditLogger auditLogger;
 
-        public AdminManageAdminUsersController(IDataRepository dataRepository)
+        public AdminManageAdminUsersController(IDataRepository dataRepository, AuditLogger auditLogger)
         {
             this.dataRepository = dataRepository;
+            this.auditLogger = auditLogger;
         }
         
         [HttpGet("")]
@@ -33,5 +39,50 @@ namespace GenderPayGap.WebUI.Controllers.Admin
             return View("ViewAdminUsers", adminUsers);
         }
 
+        [HttpGet("retire-user/{userId}")]
+        public IActionResult RetireAdminUserGet(long userId)
+        {
+            User adminUser = dataRepository.Get<User>(userId);
+
+            if (adminUser == null || !adminUser.IsAdministrator())
+            {
+                throw new PageNotFoundException();
+            }
+            
+            var viewModel = new AdminRetireAdminUserViewModel();
+            viewModel.User = adminUser;
+
+            return View("RetireAdminUser", viewModel);
+        }
+        
+        [ValidateAntiForgeryToken]
+        [HttpPost("retire-user/{userId}")]
+        public IActionResult RetireAdminUserPost(long userId, AdminRetireAdminUserViewModel viewModel)
+        {
+            User adminUser = dataRepository.Get<User>(userId);
+            User userMakingTheChange = ControllerHelper.GetGpgUserFromAspNetUser(User, dataRepository);
+
+            if (adminUser == null || !adminUser.IsAdministrator())
+            {
+                throw new PageNotFoundException();
+            }
+            
+            if (!ModelState.IsValid)
+            {
+                viewModel.User = adminUser;
+                return View("RetireAdminUser", viewModel);
+            }
+
+            // Update the status
+            adminUser.SetStatus(
+                UserStatuses.Retired,
+                userMakingTheChange,
+                viewModel.Reason);
+
+            dataRepository.SaveChanges();
+
+            return RedirectToAction("ViewAdminUsers", "AdminManageAdminUsers");
+        }
+        
     }
 }
