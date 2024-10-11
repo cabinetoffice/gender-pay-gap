7b04afe4c966fd82b621f97fee9aaee46ab8bb0b
James Griffiths <james@jwgsoftware.com>
2024-10-11 15:33:41 +0100

    WIP

---
+++ GenderPayGap.WebUI/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using GenderPayGap.Core;
+using GenderPayGap.WebUI.Classes.Presentation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderPayGap.WebUI.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private readonly ICompareViewService compareViewService;
+        
+        public SearchController(ICompareViewService compareViewService)
+        {
+            this.compareViewService = compareViewService;
+        }
+
+        [HttpGet("search")]
+        public IActionResult SearchPage(
+            string employerName = "",
+            List<OrganisationSizes> employerSize = null,
+            List<string> sector = null,
+            bool reportedLate = false,
+            string orderBy = "relevance")
+        {
+            compareViewService.LoadComparedEmployersFromCookie();
+            
+            return View("SearchPage");
+        }
+
+
+    }
+}
