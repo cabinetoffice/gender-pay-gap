0249dab8ddec1afab8f309bf209a92c0cb3a39fa
James Griffiths <james@jwgsoftware.com>
2024-10-22 16:54:47 +0100

    EHD-1384: Minimise use of Obfuscator: Remove unused obfuscated IDs in Homepage search

--- GenderPayGap.WebUI/Models/Search/SuggestOrganisationResult.cs
+++ GenderPayGap.WebUI/Models/Search/SuggestOrganisationResult.cs
@@ -4,7 +4,6 @@
     public class SuggestOrganisationResult
     {
 
-        public string Id { get; set; }
         public string Text { get; set; }
         public string PreviousName { get; set; }
 
