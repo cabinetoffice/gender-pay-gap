6243ce1daa5d9e373b4e437bbf1d8b57b3b0ef2c
James Griffiths <james@jwgsoftware.com>
2024-12-02 17:21:50 +0000

    EHD-457: Upgrade .Net: Misc: Unit tests: In static analysis tests, change minimum number of .cshtml files to 400

--- GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CodeQualityTests/DoNotAllowHtmlDotRawTests.cs
+++ GenderPayGap.UnitTests/GenderPayGap.WebUI.Tests/CodeQualityTests/DoNotAllowHtmlDotRawTests.cs
@@ -34,7 +34,7 @@ namespace GenderPayGap.WebUI.Tests.CodeQualityTests
             FileInfo[] files = new DirectoryInfo(rootCodeFolder).GetFiles(searchPattern, SearchOption.AllDirectories);
 
             // Pre-Act Assert (to check again that we're running the test on the right folder)
-            Assert.Greater(files.Length, 900, "We expect there to be >900 .cs/.cshtml files");
+            Assert.Greater(files.Length, 400, "We expect there to be >400 .cs/.cshtml files");
 
             // Act
             var failedFiles = new List<string>();
