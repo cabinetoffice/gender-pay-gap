f5309c208c4831ed8273ce8df8a78f32b53a3c4d
James Griffiths <james@jwgsoftware.com>
2024-12-02 16:59:01 +0000

    EHD-457: Upgrade .Net: Misc: Remove unused solution files

--- SolutionInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("UK Government Equalities Office")]
-[assembly: AssemblyCopyright("Copyright © UK Government Equalities Office 2017")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
-[assembly: AssemblyDelaySign(false)]
