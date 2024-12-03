b3b5e183a147962b89f3287faf9c6619ae701c27
James Griffiths <james@jwgsoftware.com>
2024-12-03 18:18:39 +0000

    EHD-457: Upgrade .Net: Tidy up Program.cs and remove unused Startup.cs

--- GenderPayGap.WebUI/Program.cs
+++ GenderPayGap.WebUI/Program.cs
@@ -5,7 +5,6 @@ using GenderPayGap.Core.Classes;
 using GenderPayGap.Core.Classes.Logger;
 using GenderPayGap.Core.Interfaces;
 using GenderPayGap.Database;
-using GenderPayGap.Extensions;
 using GenderPayGap.Extensions.AspNetCore;
 using GenderPayGap.WebUI.BackgroundJobs;
 using GenderPayGap.WebUI.BackgroundJobs.HangfireConfiguration;
@@ -17,7 +16,6 @@ using GenderPayGap.WebUI.Helpers;
 using GenderPayGap.WebUI.Repositories;
 using GenderPayGap.WebUI.Search;
 using GenderPayGap.WebUI.Services;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -291,74 +289,6 @@ namespace GenderPayGap.WebUI
                 });
             
             HangfireConfigurationHelper.ConfigureApp(app);
-        }
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            IWebHostBuilder webHostBuilder = WebHost.CreateDefaultBuilder(args);
-            webHostBuilder.ConfigureKestrel(
-                    options =>
-                    {
-                        // options.AddServerHeader = false;
-                        //options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
-                    })
-                .ConfigureAppConfiguration(ConfigureAppConfiguration)
-                .CaptureStartupErrors(true) // Add this line to capture startup errors
-                .UseEnvironment(Config.EnvironmentName) //Set the environment name
-                .UseSetting(
-                    WebHostDefaults.DetailedErrorsKey,
-                    "true"); //When enabled (or when the Environment is set to Development), the app captures detailed exceptions.
-                // .ConfigureServices(
-                //     services => services
-                //         .AddAutofac()); /// This call allows for ConfigureContainer to be supported in Startup with a strongly-typed ContainerBuilder
-            // if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("PORT")))
-            // {
-            //     webHostBuilder.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT")}/");
-            // }
-
-            // webHostBuilder.UseStartup<Startup>();
-
-            // SetupSerilogLogger();
-
-            // if (Global.LogToSentry)
-            // {
-            //     webHostBuilder.UseSentry();
-            // }
-
-            return webHostBuilder.Build();
-        }
-
-        /// <summary>
-        ///     Use the Config extension class for Configuration
-        /// </summary>
-        /// <param name="builderContext"></param>
-        /// <param name="configBuilder"></param>
-        private static void ConfigureAppConfiguration(WebHostBuilderContext builderContext, IConfigurationBuilder configBuilder)
-        {
-            Console.WriteLine($"Environment: {Config.EnvironmentName}");
-
-            //Build the configuration
-            Config.Configuration = Config.Build(configBuilder);
         }
 
     }
